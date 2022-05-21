a2cb92da7fde7468acfaed1ef5d705b1d55c8d40
Yoom Lam <yoom@navapbc.com>
2022-05-20 23:02:36 -0500

    deployed and accessible at dev.lighthouse.va.gov/abd-vro/

---
+++ scripts/cicd-deploy.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+ENV="$(tr '[A-Z]' '[a-z]' <<< $1)"
+
+if [ "${ENV}" != "sandbox" ] && [ "${ENV}" != "dev" ] && [ "${ENV}" != "qa" ] && [ "${ENV}" != "prod" ]
+then
+  echo "Please enter valid environment (dev, sandbox, qa, prod)" && exit 1
+fi
+
+GIT_SHA=$(git rev-parse HEAD)
+if [ -n "$2" ]
+then
+  IMAGE_TAG=$2
+  VERSION=$2
+else
+  IMAGE_TAG=$GIT_SHA
+fi
+
+: ${TEAMNAME:=va-abd-rrd}
+
+# Name must match `{{ .Values.name }}-ghcr` used in api/deployment.yaml
+kubectl create secret docker-registry abd-vro-ghcr -n ${TEAMNAME}-"${ENV}" \
+    --docker-username="${GITHUB_USERNAME}" \
+    --docker-password="${GITHUB_ACCESS_TOKEN}" \
+    --docker-server='https://ghcr.io' \
+    --dry-run=client -o yaml | kubectl apply -f -
+
+# Why is this needed?
+kubectl create secret generic github-access-token \
+              --from-literal=GITHUB_ACCESS_TOKEN="${GITHUB_ACCESS_TOKEN}" \
+              -n ${TEAMNAME}-"${ENV}" \
+              --save-config --dry-run=client -o yaml | kubectl apply -f -
+
+: ${HELM_APP_NAME:=abd-vro}
+: ${VERSION:=0.0.1}
+# --set-string overrides settings in helmchart/values.yaml
+helm upgrade --install $HELM_APP_NAME helmchart \
+              --set-string environment="${ENV}"\
+              --set-string images.app.tag="${IMAGE_TAG}"\
+              --set-string images.dbInit.tag="${IMAGE_TAG}"\
+              --set-string info.version="${VERSION}"\
+              --set-string info.git_hash="${GIT_SHA}" \
+              --set-string info.deploy_env="${ENV}" \
+              --set-string info.github_token="${GITHUB_ACCESS_TOKEN}" \
+              --debug \
+              -f helmchart/"${ENV}".yaml -n ${TEAMNAME}-"${ENV}" --wait
