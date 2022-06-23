6a47a76a917fc08d4e521e7ab4a5de60e0601c04
Anthony Velazquez <anthonyvelazquez@users.noreply.github.com>
2022-06-23 08:26:17 -0400

    Update Commit Message Requirements
    Amida works with a `MCP` prefix for our JIRA tickets. Updating this to match that rather than using `API`

--- scripts/commit-message.sh
+++ scripts/commit-message.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # regex to validate in commit msg
-commit_regex='(^\[API-[0-9]+\])'
-error_msg="Aborting commit. Your commit message is formatted incorrectly ex. [API-1234] message"
+commit_regex='(^\[MCP-[0-9]+\])'
+error_msg="Aborting commit. Your commit message is formatted incorrectly ex. [MCP-1234] message"
 
 if ! grep -iqE "$commit_regex" "$1"; then
     echo "$error_msg" >&2
