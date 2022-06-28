5f8ad6827f55322a59eae29b3995d6de99d7c686
julianbregstone <julian>
2022-06-28 13:32:54 -0400

    [API-1476] change med processor to use keyword substring matching

--- service-assess-data/src/dc7101/lib/continuous_medication.py
+++ service-assess-data/src/dc7101/lib/continuous_medication.py
@@ -21,7 +21,7 @@ def continuous_medication_required(request_body):
   :return: response body indicating success or failure with additional attributes 
   :rtype: dict
   """
-
+  medication_list = []
   continuous_medication_required_calculation = {
         "success": True
   }
@@ -29,9 +29,10 @@ def continuous_medication_required(request_body):
   medication_list = [med["text"] for med in veterans_medication]
   vet_is_taking_htn_medication = False
   for medication in medication_list:
-    if (medication.lower() in hypertension_medications):
-      vet_is_taking_htn_medication = True
-      break
+    for keyword in [x.lower() for x in hypertension_medications]:
+      if (keyword in medication.lower()):
+        vet_is_taking_htn_medication = True
+        medication_list.append(medication)
 
   continuous_medication_required_calculation["continuous_medication_required"] = vet_is_taking_htn_medication
 
