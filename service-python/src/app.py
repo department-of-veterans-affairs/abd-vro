e8a5ba579ee415e2243852c49ad5631033623dae
Anthony Velazquez <anthonyhvelazquez@gmail.com>
2022-06-28 09:33:59 -0400

    Cleanup and stop consuming on exit

--- service-python/src/app.py
+++ service-python/src/app.py
@@ -1,11 +1,14 @@
+import atexit
 from lib.consumer import RabbitMQConsumer
 from config.settings import consumer_config
 
-consumer = RabbitMQConsumer(consumer_config)
 
+consumer = RabbitMQConsumer(consumer_config)
 consumer.setup_queue("generate_pdf", "pdf_generator")
 
-try:
-  consumer.channel.start_consuming()
-except KeyboardInterrupt:
+def exit_handler():
   consumer.channel.stop_consuming()
+
+atexit.register(exit_handler)
+
+consumer.channel.start_consuming()
