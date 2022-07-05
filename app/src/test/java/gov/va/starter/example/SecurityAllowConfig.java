694d2364907c2a99c7055d6e1bc6120cb52459ef
Yoom Lam <yoom@navapbc.com>
2022-07-05 10:11:07 -0500

    move SecurityAllowConfig to test folder so that CodeQL passes

--- app/src/test/java/gov/va/starter/example/SecurityAllowConfig.java
+++ app/src/test/java/gov/va/starter/example/SecurityAllowConfig.java
@@ -17,8 +17,7 @@ public class SecurityAllowConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    // Only disable CSRF for non-production environments
-    // http.csrf().disable();
+    http.csrf().disable();
 
     http.authorizeRequests().antMatchers("/").permitAll();
   }
