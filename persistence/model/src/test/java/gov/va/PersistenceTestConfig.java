dc7586da6b49c8d702d3e4a6792abced401b9c84
julianbregstone <julian>
2022-07-18 11:00:45 -0400

    [API-1472] rm files

--- persistence/model/src/test/java/gov/va/PersistenceTestConfig.java
+++
@@ -1,8 +0,0 @@
-package gov.va;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@SpringBootApplication(scanBasePackages = {"gov.va"})
-@EnableJpaAuditing
-public class PersistenceTestConfig {}
