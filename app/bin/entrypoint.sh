6e935c2431eb4473b1673d98c47d25ca48f7c6ee
julianbregstone <julian>
2022-07-18 11:44:11 -0400

    [API-1478] remove app api

--- app/bin/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# prepare the full URL based on the hostname from AWS
-
-exec java -jar \
-  -Dspring.profiles.active=local \
-  -Djava.security.egd=file:/dev/./urandom \
-  $JAVA_PROFILE \
-  $JAVA_OPTS \
-  starter-java.jar
