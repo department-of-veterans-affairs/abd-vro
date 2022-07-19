d9ed2fc3cc5e054536aae19d151ef4a809b36573
dimitri-amida <dimitri.papaioannou@amida.com>
2022-07-19 16:11:02 -0400

    MCP-1497: merged to develop

--- service/spi/src/main/java/gov/va/vro/service/spi/db/model/Claim.java
+++ service/spi/src/main/java/gov/va/vro/service/spi/db/model/Claim.java
@@ -1,18 +1,25 @@
 package gov.va.vro.service.spi.db.model;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
 
 import javax.validation.constraints.NotNull;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor(onConstructor_ = {@JsonIgnore})
+@EqualsAndHashCode
+@Builder
+@ToString
 public class Claim {
 
   @NotNull private String claimSubmissionId;
 
   // At the moment, this is the only id type
-  @NotNull private String idType = "va.gov-Form526Submission";
+  @Builder.Default @NotNull private String idType = "va.gov-Form526Submission";
 
-  @NotNull private String incomingStatus = "submission";
+  @Builder.Default @NotNull private String incomingStatus = "submission";
 
   @NotNull private String veteranIcn;
 
