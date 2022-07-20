8f01dd074efb191c4d94e30be79092ebf6cd4759
Anthony Velazquez <anthonyhvelazquez@gmail.com>
2022-07-20 16:47:41 -0400

    python test

--- service-python/src/test/test_consumer.py
+++ service-python/src/test/test_consumer.py
@@ -16,6 +16,8 @@ def test_valid_rabbitmq_connection():
 
 def test_valid_queue_created():
     consumer = RabbitMQConsumer(consumer_settings)
-    consumer.setup_queue(consumer_settings["exchange_name"], consumer_settings["queue_name"])
-    queue_exists = consumer.channel.queue_declare(queue=consumer_settings["queue_name"], durable=True, passive=True)
-    assert queue_exists
+    consumer.setup_queues()
+    generate_queue_exists = consumer.channel.queue_declare(queue=consumer_settings["generate_queue_name"], durable=True, passive=True)
+    fetch_queue_exists = consumer.channel.queue_declare(queue=consumer_settings["fetch_queue_name"], durable=True, passive=True)
+    assert generate_queue_exists
+    assert fetch_queue_exists
