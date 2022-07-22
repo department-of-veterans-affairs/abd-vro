60109f43138737212e8b0d8c5471d1e55f23b115
ianwolson <ian@amida.com>
2022-07-22 11:29:00 -0400

    more data type and column name changes

--- db-init/src/main/resources/database/migrations/V2.0__Create_Tables.sql
+++ db-init/src/main/resources/database/migrations/V2.0__Create_Tables.sql
@@ -1,58 +1,61 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE IF NOT EXISTS veteran (
-    veteran_id VARCHAR NOT NULL,
+    id uuid DEFAULT uuid_generate_v4 () NOT NULL,
     icn VARCHAR NOT NULL UNIQUE,
     participant_id VARCHAR NOT NULL,
-    createdAt timestamp NOT NULL,
-    updatedAt timestamp NOT NULL,
-    PRIMARY KEY(veteran_id)
+    created_at timestamp NOT NULL,
+    updated_at timestamp NOT NULL,
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE IF NOT EXISTS claim (
-    claim_id VARCHAR NOT NULL,
-    veteran_id VARCHAR NOT NULL,
+    id uuid DEFAULT uuid_generate_v4 () NOT NULL,
+    veteran_id uuid DEFAULT uuid_generate_v4 () NOT NULL,
     id_type VARCHAR NOT NULL UNIQUE,
     incoming_status VARCHAR,
-    createdAt timestamp NOT NULL,
-    updatedAt timestamp NOT NULL,
+    created_at timestamp NOT NULL,
+    updated_at timestamp NOT NULL,
+    PRIMARY KEY(id),
     CONSTRAINT fk_veteran_id
         FOREIGN KEY (veteran_id)
-            REFERENCES veteran(veteran_id),
-    PRIMARY KEY(claim_id)
+            REFERENCES veteran(id)
 );
 
 CREATE TABLE IF NOT EXISTS contention(
-    contention_id VARCHAR NOT NULL,
-    claim_id VARCHAR NOT NULL,
+    id uuid DEFAULT uuid_generate_v4 () NOT NULL,
+    claim_id uuid DEFAULT uuid_generate_v4 () NOT NULL,
     diagnostic_code VARCHAR NOT NULL UNIQUE,
-    createdAt timestamp NOT NULL,
-    updatedAt timestamp NOT null,
-    PRIMARY KEY(contention_id),
+    created_at timestamp NOT NULL,
+    updated_at timestamp NOT null,
+    PRIMARY KEY(id),
     CONSTRAINT fk_claim_id
         FOREIGN KEY (claim_id)
-            REFERENCES claim(claim_id)
+            REFERENCES claim(id)
 );
 
 CREATE TABLE IF NOT EXISTS assessment_result (
-    assessmentResult_id VARCHAR NOT NULL,
-    contention_id VARCHAR NOT NULL,
+    id uuid DEFAULT uuid_generate_v4 () NOT NULL,
+    contention_id uuid DEFAULT uuid_generate_v4 () NOT NULL,
     evidence_count INTEGER NOT NULL,
-    createdAt timestamp NOT NULL,
-    updatedAt timestamp NOT NULL,
-    PRIMARY KEY(assessmentResult_id),
+    created_at timestamp NOT NULL,
+    updated_at timestamp NOT NULL,
+    PRIMARY KEY(id),
     CONSTRAINT fk_contention_id
         FOREIGN KEY (contention_id)
-            REFERENCES contention(contention_id)
+            REFERENCES contention(id)
 );
 
 CREATE TABLE IF NOT EXISTS evidence_summary_document (
-    esd_id VARCHAR NOT NULL,
-    contention_id VARCHAR NOT NULL,
+    id uuid DEFAULT uuid_generate_v4 () NOT NULL,
+    contention_id uuid DEFAULT uuid_generate_v4 () NOT NULL,
     evidence_count INTEGER NOT NULL,
     document_name VARCHAR NOT NULL,
-    createdAt timestamp NOT NULL,
-    updatedAt timestamp NOT NULL,
-    PRIMARY KEY(esd_id),
+    created_at timestamp NOT NULL,
+    updated_at timestamp NOT NULL,
+    PRIMARY KEY(id),
     CONSTRAINT fk_contention_id
         FOREIGN KEY (contention_id)
-            REFERENCES contention(contention_id)
+            REFERENCES contention(id)
 );
+
