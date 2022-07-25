a588e9178f137660ec67e25ee98ef8eb27e14839
Anthony Velazquez <anthonyhvelazquez@gmail.com>
2022-07-25 12:51:50 -0400

    Consumer Restructure

---
+++ service-python/main_consumer.py
@@ -0,0 +1,52 @@
+from time import sleep
+from pdf_generator.src.lib.queues import pdf_queue_setup
+
+import pika
+import logging
+import os
+import atexit
+
+logging.basicConfig(level=logging.INFO)
+
+CONSUMER_CONFIG = {
+    "host": os.environ.get("RABBITMQ_PLACEHOLDERS_HOST", "localhost"),
+    "port": 5672,
+    "retry_limit": 3,
+}
+
+
+class RabbitMQConsumer:
+
+	def __init__(self, config):
+		self.config = config
+		self.connection = self._create_connection()
+		self.setup_queues()
+
+
+	def __del__(self):
+		self.connection.close()
+
+
+	def _create_connection(self):
+		for i in range(self.config["retry_limit"]):
+			try:
+				parameters = pika.ConnectionParameters(host=self.config["host"], port = self.config["port"])
+				return pika.BlockingConnection(parameters)
+			except:
+				logging.warn(f"RabbitMQ Connection Failed. Retrying in 15s")
+				sleep(15)
+
+
+	def setup_queues(self):
+		channel = self.connection.channel()
+		# PDF Generator Exchange/Queue Setup
+		pdf_queue_setup(channel)
+		self.channel = channel
+
+
+if __name__ == "__main__":
+	consumer = RabbitMQConsumer(CONSUMER_CONFIG)
+
+	atexit.register(consumer.channel.stop_consuming)
+
+	consumer.channel.start_consuming()
---
+++ service-python/pdf_generator/src/lib/queues.py
@@ -0,0 +1,65 @@
+from .pdf_generator import PDFGenerator
+from .redis_client import RedisClient
+from .settings import codes as DIAGNOSTIC_CODE_MAPPING, pdf_options, redis_config, queue_config
+
+import pika
+import json
+import logging
+import base64
+
+EXCHANGE = queue_config["exchange_name"]
+GENERATE_QUEUE = queue_config["generate_queue_name"]
+FETCH_QUEUE = queue_config["fetch_queue_name"]
+
+
+def on_generate_callback(channel, method, properties, body):
+	redis_client = RedisClient(redis_config)
+	pdf_generator = PDFGenerator(pdf_options)
+
+	binding_key = method.routing_key
+	message = json.loads(body)
+	logging.info(f" [x] {binding_key}: Received message: {message}")
+	claim_id = message["claimSubmissionId"]
+	message["veteran_info"] = json.loads(message["veteranInfo"])
+	message["evidence"] = json.loads(message["evidence"])
+	code = message["diagnosticCode"]
+	diagnosis_name = DIAGNOSTIC_CODE_MAPPING[code]
+	variables = pdf_generator.generate_template_variables(diagnosis_name, message)
+	logging.info(f"Variables: {variables}")
+	template = pdf_generator.generate_template_file(diagnosis_name, variables)
+	pdf = pdf_generator.generate_pdf_from_string(template)
+	redis_client.save_data(claim_id, base64.b64encode(pdf).decode("ascii"))
+	logging.info("Saved PDF")
+	response = {"claimSubmissionId": claim_id, "status": "IN_PROGRESS", "pdf": None}
+	channel.basic_publish(exchange=EXCHANGE, routing_key=properties.reply_to, properties=pika.BasicProperties(correlation_id=properties.correlation_id), body=json.dumps(response))
+
+
+def on_fetch_callback(channel, method, properties, body):
+	redis_client = RedisClient(redis_config)
+
+	binding_key = method.routing_key
+	message = json.loads(body)
+	logging.info(f" [x] {binding_key}: Received message: {message}")
+	claim_id = message["claimSubmissionId"]
+	if redis_client.exists(claim_id):
+		pdf = redis_client.get_data(claim_id)
+		logging.info(f"Fetched PDF")
+		response = {"claimSubmissionId": claim_id, "status": "COMPLETE", "pdfData": str(pdf.decode("ascii"))}
+	else:
+		logging.info(f"PDF still generating")
+		response = {"claimSubmissionId": claim_id, "status": "IN_PROGRESS", "pdfData": ""}
+	channel.basic_publish(exchange=EXCHANGE, routing_key=properties.reply_to, properties=pika.BasicProperties(correlation_id=properties.correlation_id), body=json.dumps(response))
+
+
+def pdf_queue_setup(channel):
+	channel.exchange_declare(exchange=EXCHANGE, exchange_type="direct", durable=True, auto_delete=True)
+	# Generate PDF Queue
+	channel.queue_declare(queue=GENERATE_QUEUE)
+	channel.queue_bind(queue=GENERATE_QUEUE, exchange=EXCHANGE)
+	channel.basic_consume(queue=GENERATE_QUEUE, on_message_callback=on_generate_callback, auto_ack=True)
+	# Fetch PDF Queue
+	channel.queue_declare(queue=FETCH_QUEUE)
+	channel.queue_bind(queue=FETCH_QUEUE, exchange=EXCHANGE)
+	channel.basic_consume(queue=FETCH_QUEUE, on_message_callback=on_fetch_callback, auto_ack=True)
+	logging.info(f" [*] Waiting for data for queue: {queue_config['generate_queue_name']}. To exit press CTRL+C")
+	logging.info(f" [*] Waiting for data for queue: {queue_config['fetch_queue_name']}. To exit press CTRL+C")
