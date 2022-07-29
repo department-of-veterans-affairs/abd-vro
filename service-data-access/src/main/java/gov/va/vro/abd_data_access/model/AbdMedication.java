88b27833baf310929dce66286c1ed7cee9751b1d
wlin2019 <55559058+wlin2019@users.noreply.github.com>
2022-07-29 12:50:22 -0400

    Mcp 1501 (#174)
    * Add additional fields in AbdMedication. Seen MCP-1501.
    
    * Change AbdMedication route field to String.
    
    * Changed route to pick only first element, removed getMedicationCodeable() section code.

--- service-data-access/src/main/java/gov/va/vro/abd_data_access/model/AbdMedication.java
+++ service-data-access/src/main/java/gov/va/vro/abd_data_access/model/AbdMedication.java
@@ -4,21 +4,23 @@ import lombok.Getter;
 import lombok.Setter;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Date;
 import java.util.List;
 
 @Getter
 @Setter
 public class AbdMedication implements Comparable<AbdMedication> {
-  private String text;
-  private String code;
-  private String date;
   private String status;
   private List<String> notes;
+  private String description;
   private int refills;
   private String duration;
+  private String authoredOn;
+  private List<String> dosageInstruction;
+  private String route;
 
   @Override
   public int compareTo(AbdMedication otherMedication) {
-    return StringUtils.compare(date, otherMedication.date);
+    return StringUtils.compare(authoredOn, otherMedication.authoredOn);
   }
 }
