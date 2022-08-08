6fe896374c382262a65ba4fca2297d2859e262b0
advcoder <ryan.plemons@va.gov>
2022-08-08 00:11:15 -0700

    rename directories to build docker instances (#207)
    * rename directories to build docker instances
    
    * rename access_claim to access-claim

--- service-python/tests/assess-claim/dc7101/bp_sufficiency_test.py
+++ service-python/tests/assess-claim/dc7101/bp_sufficiency_test.py
@@ -1,5 +1,5 @@
 import pytest
-from assess_claim_dc7101.src.lib import predominant_bp
+from assess-claim-dc7101.src.lib import predominant_bp
 
 @pytest.mark.parametrize(
     "date_of_claim, bp_readings, result",
