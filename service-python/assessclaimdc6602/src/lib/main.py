c906cc77e50b71df2fc8fc13eea11264bb446966
julianbregstone <julian>
2022-08-16 16:00:13 -0400

    [API-1478] add logic for conditions

--- service-python/assessclaimdc6602/src/lib/main.py
+++ service-python/assessclaimdc6602/src/lib/main.py
@@ -1,6 +1,7 @@
 import validator
 import logging
 from . import medication
+from . import condition
 from typing import Dict
 
 
@@ -17,16 +18,23 @@ def assess_asthma(event: Dict):
     response_body = {}
 
     if validation_results["is_valid"]:
-        active_medication = medication.medication_required(event)
-
+        active_medications = medication.medication_required(event)
+        active_conditions = condition.conditions_calculation(event)
 
     else:
-        active_medication = []
+        active_medications = []
+        active_conditions = {"conditions":[], "persistent_calculation": {"succsess" : False, "mild-persistent-asthma-or-greater": False}}
         logging.info(validation_results["errors"])
         response_body["errorMessage"] = {"errorString": "error validating request message data"}
 
     response_body.update({
-        "evidence": {"medications": active_medication}
+        "evidence": {
+            "medications": active_medications,
+            "conditions": active_conditions["conditions"]
+        },
+        "calculated": {
+            "persistent_calculation":active_conditions["persistant_calculation"]
+        }
     })
 
     return response_body
