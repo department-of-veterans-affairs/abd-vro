5a5bf1ebbf9927324306e6d9ed5fadab3e2d586b
dimitri-amida <108086155+dimitri-amida@users.noreply.github.com>
2022-08-21 15:48:36 -0700

    MCP-1679: Exception handling and cleanup (#219)
    * MCP-1679: more cleanup. Fix request/response definitions
    
    * MCP-1679: error handling
    
    * MCP-1679: more integreation tests
    
    * MCP-1679: more integration tests
    
    * MCP-1679: fixed warnings
    
    * MCP-1679: removed integration folder
    
    * MCP-1679: simplify test using camel mocks
    
    * MCP-1679: negative tests

--- api/src/main/java/gov/va/vro/api/requests/HealthDataAssessmentRequest.java
+++ api/src/main/java/gov/va/vro/api/requests/HealthDataAssessmentRequest.java
@@ -3,6 +3,8 @@ package gov.va.vro.api.requests;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -11,14 +13,15 @@ import lombok.*;
     name = "HealthDataAssessmentRequest",
     description = "Claim details for the health data assessment")
 public class HealthDataAssessmentRequest {
-  @NonNull
+  @NotBlank(message = "Veteran Icn cannot be empty")
   @Schema(description = "Veteran medical internal control number (EHR id)", example = "9000682")
   private String veteranIcn;
 
-  @NonNull
+  @NotBlank(message = "Diagnostic code cannot be empty")
   @Schema(description = "Diagnostic code for the claim contention", example = "7101")
   private String diagnosticCode;
 
-  @Schema(description = "Claim number", example = "1234")
+  @NotBlank(message = "Claim submission id cannot be empty")
+  @Schema(description = "Claim submission id", example = "1234")
   private String claimSubmissionId;
 }
