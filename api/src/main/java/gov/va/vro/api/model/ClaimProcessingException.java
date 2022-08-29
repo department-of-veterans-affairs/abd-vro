d01d8286073b349d1b9ae69dc1938700963e5f5f
julianbregstone <julian>
2022-08-29 12:19:59 -0400

    [API-1792] merge develop

--- api/src/main/java/gov/va/vro/api/model/ClaimProcessingException.java
+++ api/src/main/java/gov/va/vro/api/model/ClaimProcessingException.java
@@ -1,14 +1,34 @@
 package gov.va.vro.api.model;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-@RequiredArgsConstructor
-@Getter
 public class ClaimProcessingException extends Exception {
 
-  private final String claimSubmissionId;
-  private final HttpStatus httpStatus;
-  private final String message;
+  @Getter private final String claimSubmissionId;
+  @Getter private final HttpStatus httpStatus;
+
+  public ClaimProcessingException(String claimSubmissionId, HttpStatus httpStatus, String message) {
+    super(message);
+    this.claimSubmissionId = claimSubmissionId;
+    this.httpStatus = httpStatus;
+  }
+
+  public ClaimProcessingException(
+      String claimSubmissionId, HttpStatus httpStatus, Exception exception) {
+    super(exception);
+    this.claimSubmissionId = claimSubmissionId;
+    this.httpStatus = httpStatus;
+  }
+
+  public String getOriginalMessage() {
+    Throwable exception = super.getCause();
+    if (exception == null) {
+      return this.getMessage();
+    }
+    if (exception.getCause() == null) {
+      return exception.getMessage();
+    }
+    return exception.getCause().getMessage();
+  }
 }
---
+++ service/db/src/test/java/gov/va/vro/service/db/mapper/ClaimMapperTest.java
@@ -0,0 +1,36 @@
+package gov.va.vro.service.db.mapper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import gov.va.vro.persistence.model.ClaimEntity;
+import gov.va.vro.persistence.model.ContentionEntity;
+import gov.va.vro.persistence.model.VeteranEntity;
+import gov.va.vro.service.spi.model.Claim;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+
+class ClaimMapperTest {
+
+  @Test
+  void toClaim() {
+    VeteranEntity veteranEntity = new VeteranEntity();
+    veteranEntity.setIcn("icn2");
+    ClaimEntity claimEntity = new ClaimEntity();
+    claimEntity.setClaimSubmissionId("123");
+    claimEntity.setVeteran(veteranEntity);
+    ContentionEntity contentionEntity1 = new ContentionEntity();
+    contentionEntity1.setDiagnosticCode("7101");
+    claimEntity.addContention(contentionEntity1);
+    ContentionEntity contentionEntity2 = new ContentionEntity();
+    contentionEntity2.setDiagnosticCode("6202");
+    claimEntity.addContention(contentionEntity2);
+
+    ClaimMapper claimMapper = Mappers.getMapper(ClaimMapper.class);
+    Claim claim = claimMapper.toClaim(claimEntity);
+    assertEquals(claimEntity.getClaimSubmissionId(), claim.getClaimSubmissionId());
+    assertEquals(claimEntity.getVeteran().getIcn(), claim.getVeteranIcn());
+    assertEquals(2, claim.getContentions().size());
+    assertTrue(claim.getContentions().contains("7101"));
+    assertTrue(claim.getContentions().contains("6202"));
+  }
+}
