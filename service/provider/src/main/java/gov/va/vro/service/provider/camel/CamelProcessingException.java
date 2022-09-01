6ebc7a194d48f0997bd4caf0f5b8c4482b22c58a
dimitri <dimitri.papaioannou@amida.com>
2022-09-01 08:50:05 -0700

    MCP-1814: throw exception to avoid NPE

---
+++ service/provider/src/main/java/gov/va/vro/service/provider/camel/CamelProcessingException.java
@@ -0,0 +1,8 @@
+package gov.va.vro.service.provider.camel;
+
+public class CamelProcessingException extends RuntimeException {
+
+  public CamelProcessingException(String message) {
+    super(message);
+  }
+}
