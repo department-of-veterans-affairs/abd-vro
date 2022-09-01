c6864b8094e7a268f4cdcce5bc36df02a773efac
Julian Bregstone <54538359+jbregstone@users.noreply.github.com>
2022-09-01 10:24:44 -0400

    [1792] delete duplicate

--- service-data-access/src/main/java/gov/va/vro/abd_data_access/model/AbdMedication.java
+++ service-data-access/src/main/java/gov/va/vro/abd_data_access/model/AbdMedication.java
@@ -21,7 +21,6 @@ public class AbdMedication implements Comparable<AbdMedication> {
   private String authoredOn;
   private List<String> dosageInstructions;
   private String route;
-  private Boolean asthma_relevant;
 
   @Override
   public int compareTo(AbdMedication otherMedication) {
