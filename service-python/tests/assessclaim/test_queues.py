37ae7865d3f988550b06799157289738b85a473a
julianbregstone <julian>
2022-09-02 14:36:10 -0400

    [API-1234] add tests

--- service-python/tests/assessclaim/test_queues.py
+++ service-python/tests/assessclaim/test_queues.py
@@ -6,17 +6,20 @@ from unittest.mock import Mock
 
 
 @pytest.mark.parametrize(
-    "queue, service_queue_name, expected_log",
+    "queue, service_queue_name",
     [
-        (q1,
-         "6602"
-         ""),
-        (q2,
-         "7101"
-         "")
+        (
+                q1,
+                "6602"
+        ),
+        (
+                q2,
+                "7101"
+         )
     ]
 )
-def test_queues(queue, expected_log, service_queue_name, caplog):
+def test_queues(queue, service_queue_name, caplog):
+    channel = Mock(autospec=True, create=True)
     with caplog.at_level(logging.INFO):
-        queue.queue_setup(channel=Mock)
+        queue.queue_setup(channel=channel)
     assert f' [*] Waiting for data for queue: {service_queue_name}. To exit press CTRL+C' in caplog.text
