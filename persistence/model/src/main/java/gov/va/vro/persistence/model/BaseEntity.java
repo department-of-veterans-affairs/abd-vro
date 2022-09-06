e62da6fecf12f1196707912bcbfe6b765d28d778
Yoom Lam <yoom@navapbc.com>
2022-09-06 17:59:00 -0500

    update UUID column definition to fix test

--- persistence/model/src/main/java/gov/va/vro/persistence/model/BaseEntity.java
+++ persistence/model/src/main/java/gov/va/vro/persistence/model/BaseEntity.java
@@ -7,12 +7,13 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.domain.Persistable;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 /** Base class for entities that maintain created/updated time */
 @Getter
@@ -23,6 +24,7 @@ public abstract class BaseEntity implements Persistable<UUID> {
   @Id
   @GeneratedValue(generator = "uuid")
   @GenericGenerator(name = "uuid", strategy = "uuid2")
+  @Column(columnDefinition = "BINARY(16)")
   private UUID id;
 
   @CreatedDate private LocalDateTime createdAt;
