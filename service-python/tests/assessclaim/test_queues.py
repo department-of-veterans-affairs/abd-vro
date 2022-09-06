8e2043c6fa6da522575253aa3d61d1074ff023ea
julianbregstone <julian>
2022-09-06 15:21:59 -0400

    [API-1234] finalize test for request handler

--- service-python/tests/assessclaim/test_queues.py
+++ service-python/tests/assessclaim/test_queues.py
@@ -3,7 +3,9 @@ import logging
 import json
 from assessclaimdc6602.src.lib import queues as q6602
 from assessclaimdc7101.src.lib import queues as q7101
-from unittest.mock import Mock
+from assessclaimdc6602.src.lib.main import assess_asthma as main6602
+from assessclaimdc7101.src.lib.main import assess_hypertension as main7101
+from unittest.mock import Mock, patch
 
 
 @pytest.mark.parametrize(
@@ -34,21 +36,23 @@ def test_queue_setup(queue, service_queue_name, caplog):
 
 
 @pytest.mark.parametrize(
-    "queue, diagnosticCode, body",
+    "queue, diagnosticCode, body, main",
     [
         (
                 q6602,
                 "6602",
-                {"evidence": "some medical data body"}
+                {"evidence": "some medical data body"},
+                main6602
         ),
         (
                 q7101,
                 "7101",
-                {"evidence": "some medical data body"}
+                {"evidence": "some medical data body"},
+                main7101
         )
     ]
 )
-def test_on_request_callback(queue, diagnosticCode, body, caplog):
+def test_on_request_callback(queue, diagnosticCode, body, main, caplog):
 
     channel = Mock(autospec=True, create=True)
     method = Mock(autospec=True, create=True)
@@ -56,13 +60,14 @@ def test_on_request_callback(queue, diagnosticCode, body, caplog):
     properties.correlation_id = 1234
     properties.reply_to = "some_queue"
     method.routing_key = diagnosticCode
-    main = Mock()
-    main.assess_hypertension = Mock(return_value=True)
+
     body_formatted = json.dumps(body).encode('utf-8')
 
     with caplog.at_level(logging.INFO):
+        with patch(f"assessclaimdc{diagnosticCode}.src.lib.main.{main.__name__}",
+                                                 return_value=True):
 
-        queue.on_request_callback(channel, method, properties, body_formatted)
+            queue.on_request_callback(channel, method, properties, body_formatted)
 
     assert f" [x] {diagnosticCode}: Received message: {properties.correlation_id}" in caplog.text
-    assert f" [x] {diagnosticCode}: Message sent to: {properties.reply_to}"
+    assert f" [x] {diagnosticCode}: Message sent to: {properties.reply_to}" in caplog.text
