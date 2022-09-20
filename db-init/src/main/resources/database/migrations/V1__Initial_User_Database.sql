abb89e005d02be033f05ffa017fbe72d8dee2775
Afsin Ustundag <afsin@amida.com>
2022-09-20 14:08:37 -0400

    seperate flyway user from service user

--- db-init/src/main/resources/database/migrations/V1__Initial_User_Database.sql
+++ db-init/src/main/resources/database/migrations/V1__Initial_User_Database.sql
@@ -1,11 +1,12 @@
 -- Flyway user and db is created by postgres container initialization and can serve as admin for the db
 
-CREATE USER #[username];
-ALTER ROLE #[username] login password '#[userpassword]';
+CREATE ROLE #[username] LOGIN PASSWORD '#[userpassword]';
+
 GRANT CONNECT ON DATABASE #[dbname] TO #[username];
-GRANT ALL PRIVILEGES ON DATABASE #[dbname] TO #[username];
+
 GRANT USAGE ON SCHEMA #[schemaname] TO #[username];
-ALTER DEFAULT PRIVILEGES IN SCHEMA #[schemaname] GRANT SELECT ON TABLES TO #[username];
-ALTER DEFAULT PRIVILEGES IN SCHEMA #[schemaname] GRANT SELECT ON SEQUENCES TO #[username];
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA #[schemaname] GRANT SELECT, INSERT, UPDATE ON TABLES TO #[username];
+ALTER DEFAULT PRIVILEGES IN SCHEMA #[schemaname] GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO #[username];
 ALTER DEFAULT PRIVILEGES IN SCHEMA #[schemaname] GRANT EXECUTE ON FUNCTIONS TO #[username];
 
