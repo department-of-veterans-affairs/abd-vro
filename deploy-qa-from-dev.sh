3541c19c68e477f2c124b25cac53366716c8fa18
advcoder <ryan.plemons@va.gov>
2022-09-23 04:14:18 -0700

    reset back to init-db

--- deploy-qa-from-dev.sh
+++ deploy-qa-from-dev.sh
@@ -12,21 +12,21 @@ else
 fi
 docker pull "ghcr.io/${github_repository}/dev_vro-app:${COMMIT_SHA:0:7}"
 docker pull "ghcr.io/${github_repository}/dev_vro-postgres:${COMMIT_SHA:0:7}"
-docker pull "ghcr.io/${github_repository}/dev_vro-initdb:${COMMIT_SHA:0:7}"
+docker pull "ghcr.io/${github_repository}/dev_vro-init-db:${COMMIT_SHA:0:7}"
 docker pull "ghcr.io/${github_repository}/dev_vro-service-assessclaimdc7101:${COMMIT_SHA:0:7}"
 docker pull "ghcr.io/${github_repository}/dev_vro-service-assessclaimdc6602:${COMMIT_SHA:0:7}"
 docker pull "ghcr.io/${github_repository}/dev_vro-service-pdfgenerator:${COMMIT_SHA:0:7}"
 docker pull "ghcr.io/${github_repository}/dev_vro-service-data-access:${COMMIT_SHA:0:7}"
 docker tag "ghcr.io/${github_repository}/dev_vro-app:${COMMIT_SHA:0:7}" "ghcr.io/${github_repository}/qa_vro-app:${COMMIT_SHA:0:7}"
 docker tag "ghcr.io/${github_repository}/dev_vro-postgres:${COMMIT_SHA:0:7}" "ghcr.io/${github_repository}/qa_vro-postgres:${COMMIT_SHA:0:7}"
-docker tag "ghcr.io/${github_repository}/dev_vro-initdb:${COMMIT_SHA:0:7}" "ghcr.io/${github_repository}/qa_vro-initdb:${COMMIT_SHA:0:7}"
+docker tag "ghcr.io/${github_repository}/dev_vro-init-db:${COMMIT_SHA:0:7}" "ghcr.io/${github_repository}/qa_vro-init-db:${COMMIT_SHA:0:7}"
 docker tag "ghcr.io/${github_repository}/dev_vro-service-assessclaimdc7101:${COMMIT_SHA:0:7}" "ghcr.io/${github_repository}/qa_vro-service-assessclaimdc7101:${COMMIT_SHA:0:7}"
 docker tag "ghcr.io/${github_repository}/dev_vro-service-assessclaimdc6602:${COMMIT_SHA:0:7}" "ghcr.io/${github_repository}/qa_vro-service-assessclaimdc6602:${COMMIT_SHA:0:7}"
 docker tag "ghcr.io/${github_repository}/dev_vro-service-pdfgenerator:${COMMIT_SHA:0:7}" "ghcr.io/${github_repository}/qa_vro-service-pdfgenerator:${COMMIT_SHA:0:7}"
 docker tag "ghcr.io/${github_repository}/dev_vro-service-data-access:${COMMIT_SHA:0:7}" "ghcr.io/${github_repository}/qa_vro-service-data-access:${COMMIT_SHA:0:7}"
 docker tag "ghcr.io/${github_repository}/dev_vro-app:${COMMIT_SHA:0:7}" "ghcr.io/${github_repository}/qa_vro-app:latest"
 docker tag "ghcr.io/${github_repository}/dev_vro-postgres:${COMMIT_SHA:0:7}" "ghcr.io/${github_repository}/qa_vro-postgres:latest"
-docker tag "ghcr.io/${github_repository}/dev_vro-initdb:${COMMIT_SHA:0:7}" "ghcr.io/${github_repository}/qa_vro-initdb:latest"
+docker tag "ghcr.io/${github_repository}/dev_vro-init-db:${COMMIT_SHA:0:7}" "ghcr.io/${github_repository}/qa_vro-init-db:latest"
 docker tag "ghcr.io/${github_repository}/dev_vro-service-assessclaimdc7101:${COMMIT_SHA:0:7}" "ghcr.io/${github_repository}/qa_vro-service-assessclaimdc7101:latest"
 docker tag "ghcr.io/${github_repository}/dev_vro-service-assessclaimdc6602:${COMMIT_SHA:0:7}" "ghcr.io/${github_repository}/qa_vro-service-assessclaimdc6602:latest"
 docker tag "ghcr.io/${github_repository}/dev_vro-service-pdfgenerator:${COMMIT_SHA:0:7}" "ghcr.io/${github_repository}/qa_vro-service-pdfgenerator:latest"
@@ -34,7 +34,7 @@ docker tag "ghcr.io/${github_repository}/dev_vro-service-data-access:${COMMIT_SH
 docker push -a ghcr.io/${github_repository}/qa_vro-postgres
 docker push -a ghcr.io/${github_repository}/qa_vro-app
 docker push -a ghcr.io/${github_repository}/qa_vro-service-pdfgenerator
-docker push -a ghcr.io/${github_repository}/qa_vro-initdb
+docker push -a ghcr.io/${github_repository}/qa_vro-init-db
 docker push -a ghcr.io/${github_repository}/qa_vro-service-assessclaimdc7101
 docker push -a ghcr.io/${github_repository}/qa_vro-service-assessclaimdc6602
 docker push -a ghcr.io/${github_repository}/qa_vro-service-data-access
@@ -43,11 +43,11 @@ docker rmi ghcr.io/${github_repository}/qa_vro-postgres:${COMMIT_SHA:0:7} \
             ghcr.io/${github_repository}/qa_vro-service-pdfgenerator:${COMMIT_SHA:0:7} \
             ghcr.io/${github_repository}/qa_vro-service-assessclaimdc7101:${COMMIT_SHA:0:7} \
             ghcr.io/${github_repository}/qa_vro-service-assessclaimdc6602:${COMMIT_SHA:0:7} \
-            ghcr.io/${github_repository}/qa_vro-initdb:${COMMIT_SHA:0:7} \
+            ghcr.io/${github_repository}/qa_vro-init-db:${COMMIT_SHA:0:7} \
             ghcr.io/${github_repository}/qa_vro-service-data-access:${COMMIT_SHA:0:7} \
             ghcr.io/${github_repository}/qa_vro-postgres:latest \
             ghcr.io/${github_repository}/qa_vro-app:latest \
-            ghcr.io/${github_repository}/qa_vro-initdb:latest \
+            ghcr.io/${github_repository}/qa_vro-init-db:latest \
             ghcr.io/${github_repository}/qa_vro-service-pdfgenerator:latest \
             ghcr.io/${github_repository}/qa_vro-service-assessclaimdc7101:latest \
             ghcr.io/${github_repository}/qa_vro-service-assessclaimdc6602:latest \
