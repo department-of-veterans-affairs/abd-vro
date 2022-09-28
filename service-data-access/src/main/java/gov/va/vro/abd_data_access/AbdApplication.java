19211504cb8447d52f0a913d4264caef20e701a3
Warren Lin <55559058+wlin2019@users.noreply.github.com>
2022-09-28 11:23:30 -0400

    Mcp 1899 - Enable spotlessCheck for service-data-access (#408)
    * Add Software BOM GitHub Action (#375)
    
    allow manual trigger of sbom.yml
    
    fix sbom.yml
    
    * Fix a bug; add unit tests in service-data-access to bump the code coverage to 80%.
    
    * Update jwt package version (jjwt from 0.2 to 0.9.1, java-jwt from 2.2.2 to 4.0.0).
    
    * Minor adjustment for PR comments.
    
    * Remove * in import.
    
    * fix app:dockerPrepare problem in Gradle:
     Execution optimizations have been disabled for task ':app:dockerPrepare' to ensure correctness
    
    * Fix for spotless check. See MCP-1899
    
    * Minor cleanup.
    
    Co-authored-by: Yoom Lam <yoom@navapbc.com>

--- service-data-access/src/main/java/gov/va/vro/abd_data_access/AbdApplication.java
+++ service-data-access/src/main/java/gov/va/vro/abd_data_access/AbdApplication.java
@@ -1,9 +1,10 @@
 package gov.va.vro.abd_data_access;
 
+import org.springdoc.hateoas.SpringDocHateoasConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SpringDocHateoasConfiguration.class})
 public class AbdApplication {
   public static void main(String[] args) {
     SpringApplication.run(AbdApplication.class, args);
