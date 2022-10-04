558c3d6df524d7021949659588f907ab8a4058cb
warren <wlin305@gmail.com>
2022-10-04 16:42:11 -0400

    Remove unnecessary code.

--- app/src/main/java/gov/va/vro/config/SecurityConfig.java
+++ app/src/main/java/gov/va/vro/config/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -22,7 +21,6 @@ import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 @EnableWebSecurity
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @RequiredArgsConstructor
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
   @Value("${apiauth.hdr-key-name}")
