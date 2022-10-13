dd789eb169fe0a1b5288731e129cc79eee2ad06a
Yoom Lam <yoom@navapbc.com>
2022-10-12 19:24:17 -0500

    clean up; memoize writer

--- console/src/main/groovy/gov/va/vro/consolegroovy/commands/PrintJson.groovy
+++ console/src/main/groovy/gov/va/vro/consolegroovy/commands/PrintJson.groovy
@@ -1,77 +1,77 @@
 package gov.va.vro.consolegroovy.commands
 
-import com.fasterxml.jackson.annotation.JsonFilter
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.ObjectWriter
-import com.fasterxml.jackson.databind.ser.FilterProvider
-import com.fasterxml.jackson.databind.ser.PropertyFilter
-import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter
-import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider
+import gov.va.vro.persistence.model.AssessmentResultEntity
 import gov.va.vro.persistence.model.ClaimEntity
 import gov.va.vro.persistence.model.ContentionEntity
+import gov.va.vro.persistence.model.EvidenceSummaryDocumentEntity
 import org.apache.groovy.groovysh.CommandSupport
 import org.apache.groovy.groovysh.Groovysh
 
 //@CompileStatic
 class PrintJson extends CommandSupport {
 
-    private final ObjectWriter defaultWriter
-    private final Map<Class<?>, ObjectWriter> writers = [:]
+    private final ObjectMapper objectMapper;
 
     protected PrintJson(final Groovysh shell, ObjectMapper objectMapper) {
-        super(shell, 'print-json', 'pj')
-        // TODO: add description, usage, etc.
-        this.defaultWriter=objectMapper.writer()
+        super(shell, 'printJson', 'pj')
+        // TODO: add Command description, usage, etc.
 
-        // TODO: configure when needed
-        writers[ClaimEntity] = configureClaimJsonWriter(objectMapper.copy())
+        this.objectMapper = objectMapper
     }
 
     Object execute(List args) {
-        println "args: ${args}"
+        log.debug "args: ${args}"
         args.collect({
             def varObj = variables.get(it)
             if (varObj==null) return "null"
 
-            if(true) {
-                println writers.keySet() + " --- "+ varObj.class
-                ObjectWriter writer = writers[varObj.class]// ?: defaultWriter
-                writer.writeValueAsString(varObj)
-            } else {
-                def generator = claimJsonGenerator()
-                generator.toJson(varObj)
-            }
+            ObjectWriter writer = writerForObjectMemoized(varObj)
+            writer.writeValueAsString(varObj)
         }).join('\n')
     }
 
-    // Doesn't work
-    def claimJsonGenerator(){
-        new groovy.json.JsonGenerator.Options().excludeNulls()
-            .dateFormat('dd-MM-yyyy').excludeFieldsByName( 'contentions').build()
+    def mapperForObject = {
+        switch(it){
+            case ClaimEntity: return configureClaimJsonMapper(objectMapper)
+            case ContentionEntity: return configureContentionJsonMapper(objectMapper)
+            default: return configureDefaultJsonMapper(objectMapper)
+        }
     }
 
-    def configureClaimJsonWriter(ObjectMapper mapper){
+    def writerForObject = {
+        log.debug "Creating writer of object: ${it.class}"
+        mapperForObject(it).writerWithDefaultPrettyPrinter()
+    }
+    def writerForObjectMemoized = writerForObject.memoize()
+
+    def configureDefaultJsonMapper(ObjectMapper mapper){
+        // ignore certain fields to prevent recursion
+        mapper.addMixIn(AssessmentResultEntity, IgnoreFieldContentionMixin);
+        mapper.addMixIn(EvidenceSummaryDocumentEntity, IgnoreFieldContentionMixin);
+        mapper
+    }
+
+    def configureClaimJsonMapper(ObjectMapper mapper){
+        // ignore certain fields to prevent recursion
         mapper.addMixIn(ContentionEntity, IgnoreFieldClaimMixin);
-
-        return mapper.writer()
-
-//        def filters = claimJsonFilter()
-//        mapper.writer(filters)
+        configureDefaultJsonMapper(mapper)
     }
 
-    // Not needed
-    def claimJsonFilter(){
-        PropertyFilter propFilter = SimpleBeanPropertyFilter.serializeAllExcept( 'contentions')
-        FilterProvider filters = new SimpleFilterProvider().addFilter("claimJsonFilter", propFilter as PropertyFilter)
-        filters
+    def configureContentionJsonMapper(ObjectMapper mapper){
+        // ignore certain fields to prevent recursion
+        mapper.addMixIn(ClaimEntity, IgnoreFieldContentionsMixin);
+        configureDefaultJsonMapper(mapper)
     }
-    @JsonFilter("claimJsonFilter")
-    class ClaimJsonMixin { }
 
     @JsonIgnoreProperties(['claim'])
     static class IgnoreFieldClaimMixin { }
 
     @JsonIgnoreProperties(['contention'])
     static class IgnoreFieldContentionMixin { }
+
+    @JsonIgnoreProperties(['contentions'])
+    static class IgnoreFieldContentionsMixin { }
 }
