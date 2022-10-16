90ab643103d828f38dc34d6171a7a2200f01bf6c
Afsin Ustundag <afsin@amida.com>
2022-10-16 19:19:09 -0400

    start test

---
+++ end2end-test/src/test/java/gov/va/vro/end2end/Dc7701Tests.java
@@ -0,0 +1,59 @@
+package gov.va.vro.end2end;
+
+import gov.va.vro.api.requests.HealthDataAssessmentRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Slf4j
+public class Dc7701Tests {
+  /*
+  WebClient client =
+      WebClient.builder()
+          .baseUrl("http://localhost:8080/v1")
+          .defaultHeader("X-API-KEY", "test-key-01")
+          .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+          .build();
+  */
+  @Test
+  public void positive01() {
+    /*
+    HealthDataAssessmentRequest req = new HealthDataAssessmentRequest();
+    req.setClaimSubmissionId("7001");
+    req.setVeteranIcn("9000682");
+    req.setDiagnosticCode("7101");
+
+    String result =
+        WebTestClient.bindToServer()
+            .baseUrl("http://localhost:8080/v1")
+            .defaultHeader("X-API-KEY", "test-key-01")
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .build()
+            .post()
+            .uri("/full-health-data-assessment")
+            .exchange()
+            .expectStatus()
+            .isCreated()
+            .expectBody(String.class)
+            .returnResult()
+            .getResponseBody();
+
+    log.info(result);
+    */
+    /*
+     */
+    /*
+    WebClient.UriSpec<WebClient.RequestBodySpec> uriSpec = client.post();
+
+    WebClient.RequestBodySpec bodySpec = uriSpec.uri("/full-health-data-assessment");
+    WebClient.RequestHeadersSpec headerSpec = bodySpec.body(BodyInserters.fromValue(req));
+
+    WebClient.ResponseSpec responseSpec = headerSpec.retrieve();
+
+    responseSpec.
+    */
+  }
+}
