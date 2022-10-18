08ba31919bda1ca9a7b46647e78ba88b05db1ab3
dimitri-amida <108086155+dimitri-amida@users.noreply.github.com>
2022-10-18 07:54:16 -0700

    MCP-1932: examOrderingStatus controller shell (#513)
    * MCP-1932: controller shell
    
    * MCP-1932: consolidated tags
    
    * MCP-1932: update API definitions
    
    * MCP-1932: merged with develop

--- domain/src/main/java/gov/va/vro/model/mas/ClaimCondition.java
+++ domain/src/main/java/gov/va/vro/model/mas/ClaimCondition.java
@@ -2,6 +2,7 @@ package gov.va.vro.model.mas;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,15 +12,18 @@ import javax.validation.constraints.NotBlank;
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class ClaimDetailConditions {
+public class ClaimCondition {
 
   private String name;
 
+  @Schema(description = "Diagnostic code", example = "1111")
   @NotBlank(message = "Diagnostic Code is required")
   private String diagnosticCode;
 
+  @Schema(description = "Disability action type", example = "INCREASE")
   private String disabilityActionType;
 
+  @Schema(description = "Disability classification code", example = "3460")
   private String disabilityClassificationCode;
 
   private String ratedDisabilityId;
