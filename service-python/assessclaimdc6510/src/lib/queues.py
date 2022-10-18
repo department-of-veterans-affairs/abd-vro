b37a06523c56f80f79d9b2f68fea56277f783145
julianbregstone <julian>
2022-10-18 12:14:58 -0400

    [API-1234] fix queue

--- service-python/assessclaimdc6510/src/lib/queues.py
+++ service-python/assessclaimdc6510/src/lib/queues.py
@@ -33,10 +33,10 @@ def queue_setup(channel):
     channel.exchange_declare(
         exchange=EXCHANGE, exchange_type="direct", durable=True, auto_delete=True
     )
-    channel.queue_declare(queue=SERVICE_QUEUE)
+    channel.queue_declare(queue=SERVICE_QUEUE, durable=True, auto_delete=True)
     channel.queue_bind(queue=SERVICE_QUEUE, exchange=EXCHANGE)
 
-    channel.basic_qos(prefetch_count=1)
+    channel.basic_qos(prefetch_count=250)
     channel.basic_consume(
         queue=SERVICE_QUEUE, on_message_callback=on_request_callback, auto_ack=True
     )
