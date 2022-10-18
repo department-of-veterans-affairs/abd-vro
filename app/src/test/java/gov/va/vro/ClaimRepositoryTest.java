f230d9d6f2e7ee58e49762849f8790a84a68b6e1
ianwolson <ian@amida.com>
2022-10-18 12:59:51 -0400

    spotless apply

--- app/src/test/java/gov/va/vro/ClaimRepositoryTest.java
+++ app/src/test/java/gov/va/vro/ClaimRepositoryTest.java
@@ -8,7 +8,6 @@ import gov.va.vro.persistence.model.ContentionEntity;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import javax.validation.constraints.AssertTrue;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
