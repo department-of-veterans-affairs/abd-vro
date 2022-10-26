8a62e52669118fba411ebc9be108184a4cfa048e
dimitri-amida <108086155+dimitri-amida@users.noreply.github.com>
2022-10-26 07:04:56 -0700

    MCP-1986: added event query. Temporary fix for CI

--- persistence/model/src/main/java/gov/va/vro/persistence/repository/AuditEventRepository.java
+++ persistence/model/src/main/java/gov/va/vro/persistence/repository/AuditEventRepository.java
@@ -4,7 +4,11 @@ import gov.va.vro.persistence.model.AuditEventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface AuditEventRepository extends JpaRepository<AuditEventEntity, UUID> {}
+public interface AuditEventRepository extends JpaRepository<AuditEventEntity, UUID> {
+
+    List<AuditEventEntity> findByEventIdOrderByEventTimeDesc(String eventId);
+}
