57934b5718bd0c548f6532d189595211124a2f56
dimitri-amida <108086155+dimitri-amida@users.noreply.github.com>
2022-10-27 12:55:36 -0700

    MCP-1986: added javadoc

--- shared/domain/src/main/java/gov/va/vro/model/event/JsonConverter.java
+++ shared/domain/src/main/java/gov/va/vro/model/event/JsonConverter.java
@@ -5,6 +5,7 @@ import lombok.SneakyThrows;
 
 import java.util.function.Function;
 
+/** Maps an object to a JSON String */
 public class JsonConverter implements Function<Auditable, String> {
 
   private final ObjectMapper objectMapper = new ObjectMapper();
