d4fae2f13fae32ffda5cb55ef01aec207ba8619a
rajesh-Krishnasamy <rajesh@amida.com>
2022-10-26 21:51:33 -0400

    MAS APIs

--- service/provider/src/main/java/gov/va/vro/service/provider/mas/model/MasCollectionStatus.java
+++ service/provider/src/main/java/gov/va/vro/service/provider/mas/model/MasCollectionStatus.java
@@ -9,6 +9,6 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class MasCollectionStatus {
-  private int collectionId;
+  private int collectionsId;
   private String collectionStatus;
 }
