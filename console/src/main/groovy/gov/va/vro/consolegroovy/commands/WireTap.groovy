397efc0fdffb25145adcc1b9fabaafc6093d7060
Yoom Lam <yoom@navapbc.com>
2022-10-31 14:54:35 -0500

    pretty-print json

--- console/src/main/groovy/gov/va/vro/consolegroovy/commands/WireTap.groovy
+++ console/src/main/groovy/gov/va/vro/consolegroovy/commands/WireTap.groovy
@@ -1,7 +1,10 @@
 package gov.va.vro.consolegroovy.commands
 
+import groovy.json.JsonOutput
 import groovy.transform.CompileStatic
 import org.apache.camel.CamelContext
+import org.apache.camel.Exchange
+import org.apache.camel.Processor
 import org.apache.camel.RoutesBuilder
 import org.apache.camel.builder.RouteBuilder
 import org.apache.groovy.groovysh.CommandSupport
@@ -15,7 +18,7 @@ class WireTap extends CommandSupport {
     super(shell, 'wireTap', 'wt')
     // TODO: add Command description, usage, etc.
 
-    this.camelContext=camelContext
+    this.camelContext = camelContext
   }
 
   Object execute(final List<String> args) {
@@ -29,22 +32,30 @@ class WireTap extends CommandSupport {
     })
   }
 
-  String subscribeToTopic(String wireTapName){
-    RoutesBuilder routeBuilder=new WireTapRoute(wireTapName)
+  String subscribeToTopic(String wireTapName) {
+    RoutesBuilder routeBuilder = new WireTapRoute(wireTapName)
     camelContext.addRoutes(routeBuilder)
     "tap-${wireTapName}"
   }
 
-  class WireTapRoute extends RouteBuilder {
+  static class WireTapRoute extends RouteBuilder {
     final String tapName;
-    WireTapRoute(String wireTapName){
-      this.tapName=wireTapName
+
+    WireTapRoute(String wireTapName) {
+      this.tapName = wireTapName
     }
 
     @Override
     void configure() throws Exception {
       from("rabbitmq:tap-${tapName}?exchangeType=topic&queue=console-${tapName}")
-          .routeId("console-${tapName}").to("log:${tapName}")
+          .routeId("console-${tapName}")
+          .process(prettyPrinter)
+          .to("log:${tapName}?plain=true")
+    }
+
+    Processor prettyPrinter = { Exchange exchange ->
+      def json = exchange.getIn().getBody(String)
+      exchange.getMessage().setBody(JsonOutput.prettyPrint(json))
     }
   }
 }
