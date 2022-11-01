da5e7998d6798214736749ce5df83c946a060457
warren <wlin305@gmail.com>
2022-11-01 09:27:13 -0400

    Add test

---
+++ console/src/test/groovy/gov/va/vro/service/provider/bip/service/BipApiServiceTest.java
@@ -0,0 +1,16 @@
+package gov.va.vro.service.provider.bip.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author warren @Date 11/1/22
+ */
+class BipApiServiceTest {
+
+    @Test
+    public void testUpdateClaimStatus() {
+
+    }
+}
