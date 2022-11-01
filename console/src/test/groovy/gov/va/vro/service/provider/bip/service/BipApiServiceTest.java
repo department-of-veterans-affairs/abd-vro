98c1cc982f6d3a00e7eaea28b139a870710f3b0d
warren <wlin305@gmail.com>
2022-11-01 11:10:35 -0400

    update test

--- console/src/test/groovy/gov/va/vro/service/provider/bip/service/BipApiServiceTest.java
+++ console/src/test/groovy/gov/va/vro/service/provider/bip/service/BipApiServiceTest.java
@@ -1,6 +1,8 @@
 package gov.va.vro.service.provider.bip.service;
 
+import gov.va.vro.service.provider.bip.BipApiProps;
 import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,6 +13,10 @@ class BipApiServiceTest {
 
     @Test
     public void testUpdateClaimStatus() {
+
+        RestTemplate restTemplate = new RestTemplate();
+        BipApiProps props = new BipApiProps();
+        BipApiService service = new BipApiService(restTemplate, props);
 
     }
 }
