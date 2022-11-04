98118475ddace06570b0817eea03c14a4fa9c198
dimitri-amida <108086155+dimitri-amida@users.noreply.github.com>
2022-11-04 08:27:18 -0700

    MCP-2020:  Combine lighthouse evidence and call "Health Assess" (#624)
    * MCP-2020: Simplify claim router
    
    * MCP-2020: Deleted duplicate objects. Call Lighthouse and extract evidence
    
    * MCP-2020: splotless
    
    * MCP-2020: fixed health route
    
    * MCP-2020: clean up code
    
    * MCP-2020: merged
    
    * MCP-2020: return to last working version
    
    * MCP-2020: get payload from properties
    
    * MCP-2020: fixed NPE
    
    * MCP-2020: hook up health assessment service
    
    * MCP-2020: hook up health assessment service and collect results
    
    * MCP-2020: hook up generate PDF
    
    * MCP-2020: hook up event auditing via intercepts
    
    * MCP-2020: clean up

--- service/provider/src/main/java/gov/va/vro/service/provider/camel/MasIntegrationRoutes.java
+++ service/provider/src/main/java/gov/va/vro/service/provider/camel/MasIntegrationRoutes.java
@@ -1,20 +1,31 @@
 package gov.va.vro.service.provider.camel;
 
 import gov.va.vro.camel.FunctionProcessor;
+import gov.va.vro.model.*;
+import gov.va.vro.model.event.Auditable;
 import gov.va.vro.model.event.JsonConverter;
 import gov.va.vro.model.mas.MasAutomatedClaimPayload;
 import gov.va.vro.service.event.AuditEventProcessor;
 import gov.va.vro.service.provider.MasPollingProcessor;
 import gov.va.vro.service.provider.mas.service.MasCollectionService;
+import gov.va.vro.service.provider.mas.service.MasTransferObject;
+import gov.va.vro.service.spi.model.Claim;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.Exchange;
 import org.apache.camel.ExchangePattern;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jackson.JacksonDataFormat;
+import org.apache.camel.processor.aggregate.GroupedBodyAggregationStrategy;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Function;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class MasIntegrationRoutes extends RouteBuilder {
 
   public static final String ENDPOINT_MAS =
@@ -36,31 +47,28 @@ public class MasIntegrationRoutes extends RouteBuilder {
 
   private final MasCollectionService masCollectionService;
 
+  private final SlipClaimSubmitRouter slipClaimSubmitRouter;
+
   @Override
   public void configure() {
     configureExceptionHandling();
+    configureIntercepts();
     configureAutomatedClaim();
-    configureOrderExamStatus();
     configureMasProcessing();
+    configureOrderExamStatus();
   }
 
   private void configureAutomatedClaim() {
     String routeId = "mas-claim-notification";
     from(ENDPOINT_AUTOMATED_CLAIM)
         .routeId(routeId)
-        .process(
-            auditEventProcessor.event(
-                routeId, "Setting a delay before staring Automated claim processing."))
         .delay(header(MAS_DELAY_PARAM))
         .setExchangePattern(ExchangePattern.InOnly)
-        .process(auditEventProcessor.event(routeId, "Calling endpoint " + ENDPOINT_MAS))
         .to(ENDPOINT_MAS);
 
     from(ENDPOINT_MAS)
         .routeId("mas-claim-processing")
         .unmarshal(new JacksonDataFormat(MasAutomatedClaimPayload.class))
-        .process(
-            auditEventProcessor.event("mas-claim-processing", "Entering endpoint " + ENDPOINT_MAS))
         .process(masPollingProcessor)
         .setExchangePattern(ExchangePattern.InOnly)
         .log("MAS response: ${body}");
@@ -68,20 +76,72 @@ public class MasIntegrationRoutes extends RouteBuilder {
 
   private void configureMasProcessing() {
     String routeId = "mas-processing";
+    String lighthouseEndpoint = "direct:lighthouse-claim-submit";
+    String collectEvidenceEndpoint = "direct:collect-evidence";
     from(ENDPOINT_MAS_PROCESSING)
         .routeId(routeId)
-        .process(auditEventProcessor.event(routeId, "Calling Collect Annotations"))
-        // Call Mas API to collect annotations
-        .process(FunctionProcessor.fromFunction(masCollectionService::collectAnnotations))
-        // TODO:  call Lighthouse and combine evidence
-        // TODO: call "assess claim" service based on condition
-        .process(FunctionProcessor.fromFunction(MasCollectionService::getGeneratePdfPayload))
-        .process(auditEventProcessor.event(routeId, "Completed Collect Annotations"))
-        // TODO: Call Generate PDF
+        .setProperty("diagnosticCode", simple("${body.diagnosticCode}"))
+        .setProperty("claim", simple("${body}"))
+        .to(collectEvidenceEndpoint) // collect evidence from lighthouse and MAS
+        .setProperty("evidence", simple("${body}"))
+        .routingSlip(
+            method(
+                slipClaimSubmitRouter, "routeHealthAssessV2")) // TODO: call "health assess" service
+        .unmarshal(new JacksonDataFormat(AbdEvidenceWithSummary.class))
+        .process( // TODO: This is to print all the validation errors
+            new Processor() {
+              @Override
+              public void process(Exchange exchange) {
+                MasAutomatedClaimPayload claimPayload =
+                    (MasAutomatedClaimPayload) exchange.getProperty("claim");
+                AbdEvidenceWithSummary evidence =
+                    exchange.getMessage().getBody(AbdEvidenceWithSummary.class);
+                HealthDataAssessment assessment =
+                    (HealthDataAssessment) exchange.getProperty("evidence");
+                if (evidence.getErrorMessage() != null) {
+                  log.error("Health Assessment Failed");
+                }
+                // FAKE IT: Let's pretend Health assessment passed
+                var masTransferObject =
+                    new MasTransferObject(claimPayload, assessment.getEvidence());
+                exchange.getMessage().setBody(masTransferObject);
+              }
+            })
+        //
         // TODO: call pcOrderExam in the absence of evidence
         // TODO: Call claim status update
-        // Generate PDF
+        .process(FunctionProcessor.fromFunction(MasCollectionService::getGeneratePdfPayload))
         .to(PrimaryRoutes.ENDPOINT_GENERATE_PDF);
+    // TODO upload PDF
+
+    from(collectEvidenceEndpoint)
+        .routeId("automated-claim-collect-evidence")
+        .multicast(new GroupedBodyAggregationStrategy())
+        .process(
+            FunctionProcessor.fromFunction(masCollectionService::collectAnnotations)) // call MAS
+        .to(lighthouseEndpoint) // call Lighthouse
+        .end() // end multicast
+        .process( // combine evidence
+            FunctionProcessor.fromFunction(
+                (Function<List<HealthDataAssessment>, HealthDataAssessment>)
+                    abdEvidences ->
+                        MasCollectionService.combineEvidence(
+                            abdEvidences.get(0), abdEvidences.get(1))));
+
+    from(lighthouseEndpoint)
+        .routeId("automated-claim-lighthouse")
+        .process(
+            FunctionProcessor.fromFunction(
+                (Function<MasAutomatedClaimPayload, Claim>)
+                    payload ->
+                        Claim.builder()
+                            .claimSubmissionId(payload.getClaimDetail().getBenefitClaimId())
+                            .diagnosticCode(
+                                payload.getClaimDetail().getConditions().getDiagnosticCode())
+                            .veteranIcn(payload.getVeteranIdentifiers().getIcn())
+                            .build()))
+        .routingSlip(method(slipClaimSubmitRouter, "routeClaimSubmit"))
+        .unmarshal(new JacksonDataFormat(HealthDataAssessment.class));
   }
 
   private void configureOrderExamStatus() {
@@ -101,6 +161,20 @@ public class MasIntegrationRoutes extends RouteBuilder {
               var message = exchange.getMessage();
               var body = message.getBody();
               auditEventProcessor.logException(body, exception, exchange.getFromRouteId());
+            });
+  }
+
+  private void configureIntercepts() {
+    interceptFrom("*")
+        .process(
+            exchange -> {
+              String routeId = exchange.getFromRouteId();
+              String endpoint = exchange.getFromEndpoint().getEndpointUri();
+              Object body = exchange.getMessage().getBody();
+              if (body instanceof Auditable) {
+                auditEventProcessor.logEvent(
+                    (Auditable) body, routeId, "Received message from endpoint " + endpoint);
+              }
             });
   }
 }
