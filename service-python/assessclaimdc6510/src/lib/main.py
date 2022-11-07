0a20b105f754a2343014c8b4d8f4effb83048ec7
julianbregstone <julian>
2022-11-07 14:21:08 -0500

    [API-1948] conform to gradle standard

--- service-python/assessclaimdc6510/src/lib/main.py
+++ service-python/assessclaimdc6510/src/lib/main.py
@@ -17,40 +17,37 @@ def assess_sinusitis(event: Dict):
     validation_results = utils.validate_request_body(event)
 
     response_body = {}
+    if "dateOfClaim" not in event:
+        event["dateOfClaim"] = str(date.today())
 
     if validation_results["is_valid"]:
-        if "date_of_claim" not in event:
-            event["date_of_claim"] = str(date.today())
-        try:
-            active_medications = medication.medication_required(event)
-            conditions = condition.conditions_calculation(event)
-            procedures = procedure.procedures_calculation(event)
-            response_body.update(
-                {
-                    "evidence": {
-                        "medications": active_medications["medications"],
-                        "conditions": conditions["conditions"],
-                        "procedures": procedures["procedures"]
-                    },
-                    "evidenceSummary": {
-                        "relevantMedCount": active_medications["relevantMedCount"],
-                        "totalMedCount": active_medications["totalMedCount"],
-                        "relevantConditionsCount": conditions["relevantConditionsCount"],
-                        "totalConditionsCount": conditions["totalConditionsCount"],
-                        "relevantProceduresCount": procedures["relevantProceduresCount"],
-                        "totalProceduresCount": procedures["totalProceduresCount"],
-                    },
-                    "calculated": {
-                        "radicalSurgery": procedures["radicalSurgery"],
-                        "multipleSurgery": procedures["multipleSurgery"],
-                        "constantSinusitis": conditions["constantSinusitis"]
-                    }
+        active_medications = medication.medication_required(event)
+        conditions = condition.conditions_calculation(event)
+        procedures = procedure.procedures_calculation(event)
+        response_body.update(
+            {
+                "evidence": {
+                    "medications": active_medications["medications"],
+                    "conditions": conditions["conditions"],
+                    "procedures": procedures["procedures"]
+                },
+                "evidenceSummary": {
+                    "relevantMedCount": active_medications["relevantMedCount"],
+                    "totalMedCount": active_medications["totalMedCount"],
+                    "relevantConditionsCount": conditions["relevantConditionsCount"],
+                    "totalConditionsCount": conditions["totalConditionsCount"],
+                    "relevantProceduresCount": procedures["relevantProceduresCount"],
+                    "totalProceduresCount": procedures["totalProceduresCount"],
+                },
+                "calculated": {
+                    "radicalSurgery": procedures["radicalSurgery"],
+                    "multipleSurgery": procedures["multipleSurgery"],
+                    "constantSinusitis": conditions["constantSinusitis"]
                 }
-            )
-        except Exception as e:
-            logging.error(e, exc_info=True)
-            response_body["errorMessage"] = str(e)
+            }
+        )
+        logging.info("Message processed successfully")
     else:
+        logging.info(f"Message failed to process due to: {validation_results['errors']}")
         response_body["errorMessage"] = "error validating request message data"
-
     return response_body
