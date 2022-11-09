9c5d56d6bb0612459a9aba28761dcbd808136fad
advcoder <ryan.plemons@va.gov>
2022-11-08 17:28:32 -0700

    added better script

--- build-images.sh
+++ build-images.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-./gradlew build docker
+java -version
+sudo apt-get update -y
+sudo apt-get install temurin-17-jdk -y
+export JAVA_HOME=/usr/lib/jvm/temurin-17-jdk-amd64
+export PATH=$JAVA_HOME:$PATH
+
+./gradlew build docker -PGITHUB_ACCESS_TOKEN=$GITHUB_ACCESS_TOKEN
