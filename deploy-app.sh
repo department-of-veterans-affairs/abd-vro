08d36f10a282e4ea09c44e90cd07d1fdfeeb18a5
Yoom Lam <yoom@navapbc.com>
2022-11-16 17:08:16 -0600

    DRY deploy-app.sh

--- deploy-app.sh
+++ deploy-app.sh
@@ -27,6 +27,25 @@ else
   VERSION=${GIT_SHA:0:7}
 fi
 
+source scripts/image_vars.src
+generateImageArgs(){
+  local _IMAGE_TAG=$2
+
+  case "$1" in
+    dev|qa) IMG_NAME_PREFIX="${1}_";;
+    sandbox|prod|prod-test) IMG_NAME_PREFIX="";;
+    *) { echo "Unknown environment: $1"; exit 20; }
+  esac
+
+  for PREFIX in ${VAR_PREFIXES_ARR[@]}; do
+    local HELM_KEY=`getVarValue ${PREFIX} _HELM_KEY`
+    local IMAGE_NAME=${IMG_NAME_PREFIX}`getVarValue ${PREFIX} _IMG`
+    echo "--set-string images.$HELM_KEY.tag=${_IMAGE_TAG} "
+    echo "--set-string images.$HELM_KEY.imageName=${IMAGE_NAME} "
+  done
+}
+VRO_IMAGE_ARGS=$(generateImageArgs "${ENV}" "${IMAGE_TAG}")
+
 if [ "${ENV}" == "qa" ] || [ "${ENV}" == "dev" ]
 then
 : "${TEAMNAME:=va-abd-rrd}"
@@ -34,30 +53,17 @@ then
 helm del $HELM_APP_NAME -n ${TEAMNAME}-${ENV}
 helm upgrade --install $HELM_APP_NAME helmchart \
               --set-string environment=${ENV} \
-              --set-string images.app.tag=${IMAGE_TAG} \
-              --set-string images.redis.tag=latest \
-              --set-string images.db.tag=${IMAGE_TAG} \
-              --set-string images.mq.tag="3" \
-              --set-string images.dbInit.tag=${IMAGE_TAG} \
-              --set-string images.pdfGenerator.tag=${IMAGE_TAG} \
-              --set-string images.serviceAssessClaimDC7101.tag=${IMAGE_TAG} \
-              --set-string images.serviceAssessClaimDC6602.tag=${IMAGE_TAG} \
-              --set-string images.serviceDataAccess.tag=${IMAGE_TAG} \
               --set-string info.version=${IMAGE_TAG} \
               --set-string info.git_hash=${GIT_SHA} \
               --set-string info.deploy_env=${ENV} \
               --set-string info.github_token=${GITHUB_ACCESS_TOKEN} \
-              --set-string images.app.imageName=${ENV}_vro-app \
+              --set-string images.redis.tag=latest \
               --set-string images.redis.imageName=redis \
-              --set-string images.db.imageName=${ENV}_vro-postgres \
+              --set-string images.mq.tag="3" \
               --set-string images.mq.imageName=vro-rabbitmq \
-              --set-string images.dbInit.imageName=${ENV}_vro-db-init \
-              --set-string images.pdfGenerator.imageName=${ENV}_vro-service-pdfgenerator \
-              --set-string images.serviceAssessClaimDC7101.imageName=${ENV}_vro-service-assessclaimdc7101 \
-              --set-string images.serviceAssessClaimDC6602.imageName=${ENV}_vro-service-assessclaimdc6602 \
-              --set-string images.serviceDataAccess.imageName=${ENV}_vro-service-data-access \
+              ${VRO_IMAGE_ARGS} \
               --debug \
-              -n ${TEAMNAME}-"${ENV}" #--dry-run
+              -n ${TEAMNAME}-${ENV} #--dry-run
               #-f helmchart/"${ENV}".yaml
 elif [ "${ENV}" == "sandbox" ] || [ "${ENV}" == "prod" || "${ENV}" == "prod-test" ]
 then
@@ -67,28 +73,15 @@ helm del $HELM_APP_NAME -n ${TEAMNAME}-"${ENV}"
 helm upgrade --install $HELM_APP_NAME helmchart \
               --set-string images.repo=abd-vro-internal \
               --set-string environment="${ENV}"\
-              --set-string images.app.tag="${IMAGE_TAG}"\
-              --set-string images.redis.tag="latest"\
-              --set-string images.db.tag="${IMAGE_TAG}"\
-              --set-string images.mq.tag="3"\
-              --set-string images.dbInit.tag="${IMAGE_TAG}"\
-              --set-string images.pdfGenerator.tag="${IMAGE_TAG}"\
-              --set-string images.serviceAssessClaimDC7101.tag="${IMAGE_TAG}"\
-              --set-string images.serviceAssessClaimDC6602.tag="${IMAGE_TAG}"\
-              --set-string images.serviceDataAccess.tag="${IMAGE_TAG}"\
               --set-string info.version="${IMAGE_TAG}"\
               --set-string info.git_hash="${GIT_SHA}" \
               --set-string info.deploy_env="${ENV}" \
               --set-string info.github_token="${GITHUB_ACCESS_TOKEN}" \
-              --set-string images.app.imageName=vro-app \
+              --set-string images.redis.tag="latest"\
               --set-string images.redis.imageName=redis \
-              --set-string images.db.imageName=vro-postgres \
+              --set-string images.mq.tag="3"\
               --set-string images.mq.imageName=rabbitmq \
-              --set-string images.dbInit.imageName=vro-db-init \
-              --set-string images.pdfGenerator.imageName=vro-service-pdfgenerator \
-              --set-string images.serviceAssessClaimDC7101.imageName=vro-service-assessclaimdc7101 \
-              --set-string images.serviceAssessClaimDC6602.imageName=vro-service-assessclaimdc6602 \
-              --set-string images.serviceDataAccess.imageName=vro-service-data-access \
+              ${VRO_IMAGE_ARGS} \
               --debug \
               -n ${TEAMNAME}-"${ENV}"
 else
@@ -96,28 +89,14 @@ helm del abd-vro
 helm upgrade --install abd-vro helmchart \
               --set-string images.repo=abd-vro-internal \
               --set-string environment="${ENV}"\
-              --set-string images.app.tag="${IMAGE_TAG}"\
-              --set-string images.redis.tag="latest"\
-              --set-string images.db.tag="${IMAGE_TAG}"\
-              --set-string images.mq.tag="3"\
-              --set-string images.dbInit.tag="${IMAGE_TAG}"\
-              --set-string images.pdfGenerator.tag="${IMAGE_TAG}"\
-              --set-string images.serviceAssessClaimDC7101.tag="${IMAGE_TAG}"\
-              --set-string images.serviceAssessClaimDC6602.tag="${IMAGE_TAG}"\
-              --set-string images.serviceDataAccess.tag="${IMAGE_TAG}"\
               --set-string info.version="${IMAGE_TAG}"\
               --set-string info.git_hash="${GIT_SHA}" \
               --set-string info.deploy_env="${ENV}" \
               --set-string info.github_token="${GITHUB_ACCESS_TOKEN}" \
-              --set-string images.app.imageName=vro-app \
               --set-string images.redis.imageName=redis \
-              --set-string images.db.imageName=vro-postgres \
+              --set-string images.redis.tag="latest"\
               --set-string images.mq.imageName=rabbitmq \
-              --set-string images.dbInit.imageName=vro-db-init \
-              --set-string images.pdfGenerator.imageName=vro-service-pdfgenerator \
-              --set-string images.serviceAssessClaimDC7101.imageName=vro-service-assessclaimdc7101 \
-              --set-string images.serviceAssessClaimDC6602.imageName=vro-service-assessclaimdc6602 \
-              --set-string images.serviceDataAccess.imageName=vro-service-data-access \
+              --set-string images.mq.tag="3"\
+              ${VRO_IMAGE_ARGS} \
               --debug
-
 fi
