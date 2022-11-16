79f9073543189fe4fd44a694deec1836d3760b03
Tanner Heffner <tanner.heffner@agile6.com>
2022-11-16 09:45:39 -0800

    stub yaml

--- service-python/featuretoggle/src/lib/main.py
+++ service-python/featuretoggle/src/lib/main.py
@@ -1,5 +1,12 @@
 import logging
+# import yaml
 from typing import Dict
+
+# stream = open('features.yml', 'r')
+# data = yaml.load(stream)
+#
+# for item in d['features'].items()
+# v['enabled'] for v in d['features'].values()
 
 def report_feature_toggles(event: Dict):
     """
