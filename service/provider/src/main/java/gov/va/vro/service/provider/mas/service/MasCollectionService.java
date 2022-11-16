22a9afc8c96fac0be17badc8acd1b411df544aad
rajesh-Krishnasamy <rajesh@amida.com>
2022-11-16 11:19:15 -0500

    MCP 2040 - Fix check style

--- service/provider/src/main/java/gov/va/vro/service/provider/mas/service/MasCollectionService.java
+++ service/provider/src/main/java/gov/va/vro/service/provider/mas/service/MasCollectionService.java
@@ -12,9 +12,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -32,18 +30,18 @@ public class MasCollectionService {
     log.info("Checking collection status for collection {}.", collectionId);
     try {
       var response =
-              masCollectionAnnotsApiService.getMasCollectionStatus(
-                      Collections.singletonList(collectionId));
+          masCollectionAnnotsApiService.getMasCollectionStatus(
+              Collections.singletonList(collectionId));
       log.info("Collection Status Response : response Size: " + response.size());
       for (MasCollectionStatus masCollectionStatus : response) {
         log.info(
-                "Collection Status Response : Collection ID  {} ",
-                masCollectionStatus.getCollectionsId());
+            "Collection Status Response : Collection ID  {} ",
+            masCollectionStatus.getCollectionsId());
         log.info(
-                "Collection Status Response : Collection Status {} ",
-                masCollectionStatus.getCollectionStatus());
+            "Collection Status Response : Collection Status {} ",
+            masCollectionStatus.getCollectionStatus());
         if ((MasStatus.PROCESSED)
-                .equals(MasStatus.getMasStatus(masCollectionStatus.getCollectionStatus()))) {
+            .equals(MasStatus.getMasStatus(masCollectionStatus.getCollectionStatus()))) {
           return true;
         }
       }
@@ -55,27 +53,27 @@ public class MasCollectionService {
   }
 
   public HealthDataAssessment collectAnnotations(MasAutomatedClaimPayload claimPayload)
-          throws MasException {
+      throws MasException {
 
     log.info(
-            "Collection {} is ready for processing, calling collection annotation service ",
-            claimPayload.getCollectionId());
+        "Collection {} is ready for processing, calling collection annotation service ",
+        claimPayload.getCollectionId());
 
     var response =
-            masCollectionAnnotsApiService.getCollectionAnnots(claimPayload.getCollectionId());
+        masCollectionAnnotsApiService.getCollectionAnnots(claimPayload.getCollectionId());
     if (response.isEmpty()) {
       throw new MasException(
-              "No annotations found for collection id " + claimPayload.getCollectionId());
+          "No annotations found for collection id " + claimPayload.getCollectionId());
     }
     MasCollectionAnnotation masCollectionAnnotation = response.get(0);
     log.info(
-            "Collection Annotation Response : Collection ID  {} and Veteran File ID {}",
-            masCollectionAnnotation.getCollectionsId(),
-            masCollectionAnnotation.getVtrnFileId());
+        "Collection Annotation Response : Collection ID  {} and Veteran File ID {}",
+        masCollectionAnnotation.getCollectionsId(),
+        masCollectionAnnotation.getVtrnFileId());
 
     MasCollectionAnnotsResults masCollectionAnnotsResults = new MasCollectionAnnotsResults();
     AbdEvidence abdEvidence =
-            masCollectionAnnotsResults.mapAnnotsToEvidence(masCollectionAnnotation);
+        masCollectionAnnotsResults.mapAnnotsToEvidence(masCollectionAnnotation);
 
     log.info("AbdEvidence : Medications {}  ", abdEvidence.getMedications().size());
     log.info("AbdEvidence : Conditions {}  ", abdEvidence.getConditions().size());
@@ -86,12 +84,12 @@ public class MasCollectionService {
     healthDataAssessment.setEvidence(abdEvidence);
     healthDataAssessment.setVeteranIcn(claimPayload.getVeteranIdentifiers().getIcn());
     healthDataAssessment.setDisabilityActionType(
-            claimPayload.getClaimDetail().getConditions().getDisabilityActionType());
+        claimPayload.getClaimDetail().getConditions().getDisabilityActionType());
     return healthDataAssessment;
   }
 
   public static HealthDataAssessment combineEvidence(
-          HealthDataAssessment lighthouseAssessment, HealthDataAssessment masApiAssessment) {
+      HealthDataAssessment lighthouseAssessment, HealthDataAssessment masApiAssessment) {
     AbdEvidence lighthouseEvidence = lighthouseAssessment.getEvidence();
     AbdEvidence masApiEvidence = masApiAssessment.getEvidence();
     // for now, we just add up the lists
@@ -99,37 +97,37 @@ public class MasCollectionService {
     log.info("combineEvidence >> MAS : " + masApiEvidence != null ? "not null" : "null");
     AbdEvidence compositeEvidence = new AbdEvidence();
     compositeEvidence.setBloodPressures(
-            merge(
-                    lighthouseEvidence != null ? lighthouseEvidence.getBloodPressures() : null,
-                    masApiEvidence != null ? masApiEvidence.getBloodPressures() : null)
-                    .stream()
-                    .flatMap(s -> Stream.ofNullable(s))
-                    .distinct()
-                    .collect(Collectors.toList()));
+        merge(
+                lighthouseEvidence != null ? lighthouseEvidence.getBloodPressures() : null,
+                masApiEvidence != null ? masApiEvidence.getBloodPressures() : null)
+            .stream()
+            .flatMap(s -> Stream.ofNullable(s))
+            .distinct()
+            .collect(Collectors.toList()));
     compositeEvidence.setConditions(
-            merge(
-                    lighthouseEvidence != null ? lighthouseEvidence.getConditions() : null,
-                    masApiEvidence != null ? masApiEvidence.getConditions() : null)
-                    .stream()
-                    .flatMap(s -> Stream.ofNullable(s))
-                    .distinct()
-                    .collect(Collectors.toList()));
+        merge(
+                lighthouseEvidence != null ? lighthouseEvidence.getConditions() : null,
+                masApiEvidence != null ? masApiEvidence.getConditions() : null)
+            .stream()
+            .flatMap(s -> Stream.ofNullable(s))
+            .distinct()
+            .collect(Collectors.toList()));
     compositeEvidence.setMedications(
-            merge(
-                    lighthouseEvidence != null ? lighthouseEvidence.getMedications() : null,
-                    masApiEvidence != null ? masApiEvidence.getMedications() : null)
-                    .stream()
-                    .flatMap(s -> Stream.ofNullable(s))
-                    .distinct()
-                    .collect(Collectors.toList()));
+        merge(
+                lighthouseEvidence != null ? lighthouseEvidence.getMedications() : null,
+                masApiEvidence != null ? masApiEvidence.getMedications() : null)
+            .stream()
+            .flatMap(s -> Stream.ofNullable(s))
+            .distinct()
+            .collect(Collectors.toList()));
     compositeEvidence.setProcedures(
-            merge(
-                    lighthouseEvidence != null ? lighthouseEvidence.getProcedures() : null,
-                    masApiEvidence != null ? masApiEvidence.getProcedures() : null)
-                    .stream()
-                    .flatMap(s -> Stream.ofNullable(s))
-                    .distinct()
-                    .collect(Collectors.toList()));
+        merge(
+                lighthouseEvidence != null ? lighthouseEvidence.getProcedures() : null,
+                masApiEvidence != null ? masApiEvidence.getProcedures() : null)
+            .stream()
+            .flatMap(s -> Stream.ofNullable(s))
+            .distinct()
+            .collect(Collectors.toList()));
     lighthouseAssessment.setEvidence(compositeEvidence);
 
     return lighthouseAssessment;
@@ -152,7 +150,7 @@ public class MasCollectionService {
     generatePdfPayload.setEvidence(transferObject.getEvidence());
     generatePdfPayload.setClaimSubmissionId(claimPayload.getClaimDetail().getBenefitClaimId());
     generatePdfPayload.setDiagnosticCode(
-            claimPayload.getClaimDetail().getConditions().getDiagnosticCode());
+        claimPayload.getClaimDetail().getConditions().getDiagnosticCode());
     VeteranInfo veteranInfo = new VeteranInfo();
     veteranInfo.setFirst(claimPayload.getFirstName());
     veteranInfo.setLast(claimPayload.getLastName());
@@ -160,10 +158,9 @@ public class MasCollectionService {
     veteranInfo.setBirthdate(claimPayload.getDateOfBirth());
     generatePdfPayload.setVeteranInfo(veteranInfo);
     log.info(
-            "Generating pdf for claim: {} and diagnostic code {}",
-            generatePdfPayload.getClaimSubmissionId(),
-            generatePdfPayload.getDiagnosticCode());
+        "Generating pdf for claim: {} and diagnostic code {}",
+        generatePdfPayload.getClaimSubmissionId(),
+        generatePdfPayload.getDiagnosticCode());
     return generatePdfPayload;
   }
-
 }
