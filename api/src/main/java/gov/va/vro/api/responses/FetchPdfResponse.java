657ca5ba42fa03624b72c9ce4333efa447f96b90
Afsin Ustundag <afsin@amida-tech.com>
2022-11-16 08:58:29 -0500

    Allow application/pdf hot fix (#697)
    * update expat to expat=2.5.0-r0
    
    * update secrel version
    
    * update languages for secrel
    
    * moving main_consumer.py into project
    
    * update config files
    
    * update filesystem
    
    * update build config
    
    * path adjustment
    
    * path adjustment
    
    * path adjustment
    
    * removing path and executing from base like we should always be doing
    
    * add java to secrel languages
    
    * put configs back to normal
    
    * reset config.yml back to where it was
    
    * comment build contexts
    
    * update secrel to perform a build
    
    * temporarily reducing coverage failure threshold
    
    * fix build errors
    
    * change context mapping
    
    * adding the access_token to config.yml
    
    * remove the pre-build from secrel
    
    * quick test
    
    * update to 3.0.1
    
    * added better script
    
    * install hadolint and shellcheck
    
    * config and context updates
    
    * we don't need to run docker
    
    * figure out pathing issue to entrypoint.sh
    
    * fix build issue with release tag
    
    * pathing cleanup
    
    * evidence-pdf GET accepts application/pdf (#696)
    
    GET evidence-pdf accepts application/pdf
    
    Co-authored-by: Afsin Ustundag <afsin@amida.com>
    
    Co-authored-by: Ryan Plemons <ryan@digitaltransfusion.net>
    Co-authored-by: advcoder <ryan.plemons@va.gov>
    Co-authored-by: Afsin Ustundag <afsin@amida.com>

--- api/src/main/java/gov/va/vro/api/responses/FetchPdfResponse.java
+++ api/src/main/java/gov/va/vro/api/responses/FetchPdfResponse.java
@@ -3,11 +3,15 @@ package gov.va.vro.api.responses;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
+@Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class FetchPdfResponse {
 
