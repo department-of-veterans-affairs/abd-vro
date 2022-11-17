4ba0fc9b21c11e2c457941824f849d6d9dd765b7
Yoom Lam <yoom@navapbc.com>
2022-11-16 21:10:48 -0600

    Fixes problems from merging qa branch (#705)
    * fix merge from qa branch
    * move ./scripts/build-images.sh
    * require 80% Python code coverage
    * update version: 1.0.25

--- scripts/deploy-app.sh
+++ scripts/deploy-app.sh
@@ -3,7 +3,7 @@
 ENV=$(tr '[A-Z]' '[a-z]' <<< "$1")
 
 #verify we have an environment set
-if [ "${ENV}" != "sandbox" ] && [ "${ENV}" != "dev" ] && [ "${ENV}" != "qa" ] && [ "${ENV}" != "prod" "${ENV}" != "prod-test" ]
+if [ "${ENV}" != "sandbox" ] && [ "${ENV}" != "dev" ] && [ "${ENV}" != "qa" ] && [ "${ENV}" != "prod" ] && [ "${ENV}" != "prod-test" ]
 then
   echo "Please enter valid environment (dev, sandbox, qa, prod, prod-test)" && exit 1
 fi
@@ -37,9 +37,9 @@ generateImageArgs(){
     *) { echo "Unknown environment: $1"; exit 20; }
   esac
 
-  for PREFIX in ${VAR_PREFIXES_ARR[@]}; do
-    local HELM_KEY=`getVarValue ${PREFIX} _HELM_KEY`
-    local IMAGE_NAME=${IMG_NAME_PREFIX}`getVarValue ${PREFIX} _IMG`
+  for PREFIX in "${VAR_PREFIXES_ARR[@]}"; do
+    local HELM_KEY=$(getVarValue "${PREFIX}" _HELM_KEY)
+    local IMAGE_NAME=${IMG_NAME_PREFIX}$(getVarValue "${PREFIX}" _IMG)
     echo "--set-string images.$HELM_KEY.tag=${_IMAGE_TAG} "
     echo "--set-string images.$HELM_KEY.imageName=${IMAGE_NAME} "
   done
@@ -65,7 +65,7 @@ helm upgrade --install $HELM_APP_NAME helmchart \
               --debug \
               -n ${TEAMNAME}-${ENV} #--dry-run
               #-f helmchart/"${ENV}".yaml
-elif [ "${ENV}" == "sandbox" ] || [ "${ENV}" == "prod" || "${ENV}" == "prod-test" ]
+elif [ "${ENV}" == "sandbox" ] || [ "${ENV}" == "prod" ] || [ "${ENV}" == "prod-test" ]
 then
 : "${TEAMNAME:=va-abd-rrd}"
 : "${HELM_APP_NAME:=abd-vro}"
