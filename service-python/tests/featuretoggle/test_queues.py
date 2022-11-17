e03946d2b4823acb6f7eca2871e538492a6d3b6b
Tanner Heffner <tanner.heffner@agile6.com>
2022-11-17 11:08:57 -0800

    fix queue name in test?

--- service-python/tests/featuretoggle/test_queues.py
+++ service-python/tests/featuretoggle/test_queues.py
@@ -8,7 +8,7 @@ from featuretoggle.src.lib import queues
 
 
 @pytest.mark.parametrize(
-    "queue, toggle_queue_name", [(queues, "featuretoggle")]
+    "queue, toggle_queue_name", "feature-toggle-queue"
 )
 def test_queue_setup(queue, toggle_queue_name, caplog):
     queue_name = f"featuretoggle.{toggle_queue_name}"
