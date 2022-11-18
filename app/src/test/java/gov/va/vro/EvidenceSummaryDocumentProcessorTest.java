184eef5bb477d02fc35864a63c258ed728188e68
ianwolson <ian@amida.com>
2022-11-18 14:33:32 -0500

    Clean up test

--- app/src/test/java/gov/va/vro/EvidenceSummaryDocumentProcessorTest.java
+++ app/src/test/java/gov/va/vro/EvidenceSummaryDocumentProcessorTest.java
@@ -1,6 +1,7 @@
 package gov.va.vro;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gov.va.vro.api.requests.GeneratePdfRequest;
@@ -53,9 +54,9 @@ public class EvidenceSummaryDocumentProcessorTest extends BaseControllerTest {
     var response = post("/v1/evidence-pdf", input, GeneratePdfResponse.class);
 
     // Verify that the evidence summary document is created and saved correctly.
-    assert claim.getId() != null;
+    assertNotNull(claim.getId());
     var claim2 = claimRepository.findById(claim.getId()).orElse(null);
-    assert claim2 != null;
+    assertNotNull(claim2);
     assertEquals(claim2.getContentions().get(0).getEvidenceSummaryDocuments().size(), 1);
     EvidenceSummaryDocumentEntity evidenceSummaryDocument =
         claim2.getContentions().get(0).getEvidenceSummaryDocuments().get(0);
@@ -72,30 +73,27 @@ public class EvidenceSummaryDocumentProcessorTest extends BaseControllerTest {
   @Test
   @DirtiesContext
   void negativeEvidenceSummaryDocumentProcessorWrongDiagnosticCode() throws Exception {
-    // Create veteran, claim, and contention and then save.
+    // Create veteran and save.
     var veteran = TestDataSupplier.createVeteran("X", "Y");
     veteranRepository.save(veteran);
-    ContentionEntity contention = new ContentionEntity("c1");
 
-    // Set an invalid diagnostic code.
-    contention.setDiagnosticCode("1111");
+    // Create a contention and set an invalid diagnostic code, then create claim and add.
+    ContentionEntity contention = new ContentionEntity("1111");
     var claim = TestDataSupplier.createClaim("1234", "type", veteran);
     claim.addContention(contention);
     claim = claimRepository.save(claim);
 
     // Call generate-pdf endpoint to activate the Evidence Summary Document processor.
     var mapper = new ObjectMapper();
-    var mockResponseObj = new GeneratePdfResponse("1234", "7701", "COMPLETE");
-    String mockResponse = mapper.writeValueAsString(mockResponseObj);
     InputStream stream = pdfGeneratorInput01.getInputStream();
     String inputAsString = new String(stream.readAllBytes(), StandardCharsets.UTF_8);
     GeneratePdfRequest input = mapper.readValue(inputAsString, GeneratePdfRequest.class);
     var response = post("/v1/evidence-pdf", input, GeneratePdfResponse.class);
 
     // Verify that the evidence summary document is not created or saved.
-    assert claim.getId() != null;
+    assertNotNull(claim.getId());
     var claim2 = claimRepository.findById(claim.getId()).orElse(null);
-    assert claim2 != null;
+    assertNotNull(claim2);
     assertEquals(claim2.getContentions().get(0).getEvidenceSummaryDocuments().size(), 0);
   }
 }
