a774a90ce2c23133e8da5f3f0814e6c962bcabce
ianwolson <109168960+ianwolson@users.noreply.github.com>
2022-11-26 17:22:09 -0500

    MCP-1897 Save evidence summary document to db (#626)
    * Initial commit, need more info to match claim and contention.
    
    * Edit evidence_summary_document table, continued work on service.
    
    * Primary routes
    
    * Primary routes
    
    * spotless apply
    
    * clean up code
    
    * Get document name
    
    * change class back
    
    * delete class
    
    * made static map and removed capitalisation
    
    * spotless apply
    
    * end of file
    
    * PR comments and tests
    
    * Fix test
    
    * Fix
    
    * Fix Jacoco
    
    * Fix Jacoco
    
    * Clean up test
    
    * Clean up test
    
    * Fix test
    
    Co-authored-by: Afsin Ustundag <afsin@amida-tech.com>

--- service/spi/src/main/java/gov/va/vro/service/spi/db/SaveToDbService.java
+++ service/spi/src/main/java/gov/va/vro/service/spi/db/SaveToDbService.java
@@ -2,6 +2,7 @@ package gov.va.vro.service.spi.db;
 
 import gov.va.vro.model.AbdEvidenceWithSummary;
 import gov.va.vro.service.spi.model.Claim;
+import gov.va.vro.service.spi.model.GeneratePdfPayload;
 
 import java.util.UUID;
 
@@ -9,4 +10,6 @@ public interface SaveToDbService {
   Claim insertClaim(Claim claim);
 
   void insertAssessmentResult(UUID claimId, AbdEvidenceWithSummary evidence, String diagnosticCode);
+
+  void insertEvidenceSummaryDocument(GeneratePdfPayload request, String documentName);
 }
