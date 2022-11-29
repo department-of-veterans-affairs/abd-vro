519ac980aa32b929f40a844ffc13a239d7a46d7f
dimitri-amida <108086155+dimitri-amida@users.noreply.github.com>
2022-11-29 10:42:37 -0800

    MCP-2004: Check if automated claim is in scope (#746)
    * MCP-2004: Check if in scope
    
    * MCP-2004: merged with develop
    
    * MCP-2004: send slack if not in scope
    
    * MCP-2004: added tests for in and out of scope
    
    * MCP-2004: added off-ramp route
    
    * MCP-2004: added test for off-ramp route
    
    * MCP-2004: increase e2e timeout

--- controller/src/main/java/gov/va/vro/controller/MasController.java
+++ controller/src/main/java/gov/va/vro/controller/MasController.java
@@ -2,6 +2,7 @@ package gov.va.vro.controller;
 
 import gov.va.vro.api.resources.MasResource;
 import gov.va.vro.api.responses.MasResponse;
+import gov.va.vro.model.event.AuditEvent;
 import gov.va.vro.model.mas.MasAutomatedClaimPayload;
 import gov.va.vro.model.mas.MasExamOrderStatusPayload;
 import gov.va.vro.service.provider.CamelEntrance;
@@ -25,8 +26,31 @@ public class MasController implements MasResource {
   public ResponseEntity<MasResponse> automatedClaim(MasAutomatedClaimPayload payload) {
     log.info(
         "Received MAS automated claim request with collection ID {}", payload.getCollectionId());
-    camelEntrance.notifyAutomatedClaim(
-        payload, masConfig.getMasProcessingInitialDelay(), masConfig.getMasRetryCount());
+    String message;
+    if (payload.isInScope()) {
+      // TODO: check if it has anchor
+      camelEntrance.notifyAutomatedClaim(
+          payload, masConfig.getMasProcessingInitialDelay(), masConfig.getMasRetryCount());
+      message = "Received";
+    } else {
+      // send slack notification
+      var auditEvent = buildAuditEvent(payload);
+      camelEntrance.sendSlack(auditEvent);
+      camelEntrance.offRampClaim(payload);
+      message = "Out of scope";
+    }
+    MasResponse response =
+        MasResponse.builder()
+            .id(Integer.toString(payload.getCollectionId()))
+            .message(message)
+            .build();
+    return ResponseEntity.ok(response);
+  }
+
+  @Override
+  public ResponseEntity<MasResponse> examOrderingStatus(MasExamOrderStatusPayload payload) {
+    log.info("Received MAS order status request with collection ID {}", payload.getCollectionId());
+    camelEntrance.examOrderingStatus(payload);
     MasResponse response =
         MasResponse.builder()
             .id(Integer.toString(payload.getCollectionId()))
@@ -35,15 +59,14 @@ public class MasController implements MasResource {
     return ResponseEntity.ok(response);
   }
 
-  @Override
-  public ResponseEntity<MasResponse> examOrderingStatus(MasExamOrderStatusPayload payload) {
-    log.info("Received MAS order statues request with collection ID {}", payload.getCollectionId());
-    camelEntrance.examOrderingStatus(payload);
-    MasResponse response =
-        MasResponse.builder()
-            .id(Integer.toString(payload.getCollectionId()))
-            .message("Received")
-            .build();
-    return ResponseEntity.ok(response);
+  private static AuditEvent buildAuditEvent(MasAutomatedClaimPayload payload) {
+    return AuditEvent.builder()
+        .eventId(Integer.toString(payload.getCollectionId()))
+        .payloadType(MasAutomatedClaimPayload.class)
+        .routeId("/automatedClaim")
+        .message(
+            String.format(
+                "Request with collection Id %s is not in scope.", payload.getCollectionId()))
+        .build();
   }
 }
