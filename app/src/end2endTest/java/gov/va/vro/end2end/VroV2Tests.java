d36bb725c403e6a6ca559b6ab0d765f1f471cc2e
dimitri-amida <108086155+dimitri-amida@users.noreply.github.com>
2022-12-08 13:56:47 -0800

    MCP-2094: claim request off ramp cases

--- app/src/end2endTest/java/gov/va/vro/end2end/VroV2Tests.java
+++ app/src/end2endTest/java/gov/va/vro/end2end/VroV2Tests.java
@@ -7,14 +7,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import gov.va.vro.api.responses.MasResponse;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.Map;
 
 public class VroV2Tests {
 
   private static final String BASE_URL = "http://localhost:8080/v2";
+  private static final String EXAM_ORDERING_URL = BASE_URL + "/examOrderingStatus";
+  private static final String AUTOMATED_CLAIM_URL = BASE_URL + "/automatedClaim";
 
   private static final String JWT_TOKEN =
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImMwOTI5NTJlLTM4ZDYtNDNjNi05MzBlLWZmOTNiYTUxYjA4ZiJ9.eyJleHAiOjk5OTk5OTk5OTksImlhdCI6MTY0MTA2Nzk0OSwianRpIjoiNzEwOTAyMGEtMzlkOS00MWE4LThlNzgtNTllZjAwYTlkNDJlIiwiaXNzIjoiaHR0cHM6Ly9zYW5kYm94LWFwaS52YS5nb3YvaW50ZXJuYWwvYXV0aC92Mi92YWxpZGF0aW9uIiwiYXVkIjoibWFzX2RldiIsInN1YiI6IjhjNDkyY2NmLTk0OGYtNDQ1Zi05NmY4LTMxZTdmODU5MDlkMiIsInR5cCI6IkJlYXJlciIsImF6cCI6Im1hc19kZXYiLCJzY29wZSI6Im9wZW5pZCB2cm9fbWFzIiwiY2xpZW50SWQiOiJtYXNfZGV2In0.Qb41CR1JIGGRlryi-XVtqyeNW73cU1YeBVqs9Bps3TA";
@@ -27,9 +35,8 @@ public class VroV2Tests {
   void testExamOrderingStatus_invalidRequest() {
     var request = getOrderingStatusInvalidRequest();
     var requestEntity = getEntity(request);
-    String url = BASE_URL + "/examOrderingStatus";
     try {
-      restTemplate.postForEntity(url, requestEntity, String.class);
+      restTemplate.postForEntity(EXAM_ORDERING_URL, requestEntity, String.class);
       fail("Should have thrown exception");
     } catch (Exception e) {
       assertEquals(
@@ -42,11 +49,54 @@ public class VroV2Tests {
   void testExamOrderingStatus() {
     var request = getOrderingStatusValidRequest();
     var requestEntity = getEntity(request);
-    String url = BASE_URL + "/examOrderingStatus";
-    var response = restTemplate.postForEntity(url, requestEntity, MasResponse.class);
+    var response = restTemplate.postForEntity(EXAM_ORDERING_URL, requestEntity, MasResponse.class);
     assertEquals(HttpStatus.OK, response.getStatusCode());
     var masResponse = response.getBody();
     assertEquals("Received", masResponse.getMessage());
+  }
+
+  @Test
+  void testAutomatedClaim() {
+    var path = "test-mas/claim-350-7101.json";
+    var content = resourceToString(path);
+    var requestEntity = getEntity(content);
+    var response =
+        restTemplate.postForEntity(AUTOMATED_CLAIM_URL, requestEntity, MasResponse.class);
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    var masResponse = response.getBody();
+    assertEquals("Received", masResponse.getMessage());
+  }
+
+  @Test
+  void testAutomatedClaim_outOfScope() {
+    var path = "test-mas/claim-350-7101-outofscope.json";
+    var content = resourceToString(path);
+    String url = BASE_URL + "/automatedClaim";
+    var requestEntity = getEntity(content);
+    var response = restTemplate.postForEntity(url, requestEntity, MasResponse.class);
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    var masResponse = response.getBody();
+    assertEquals("Out of scope", masResponse.getMessage());
+  }
+
+  @Test
+  void testAutomatedClaim_missingAnchor() {
+    var path = "test-mas/claim-351-7101-noanchor.json";
+    var content = resourceToString(path);
+    String url = BASE_URL + "/automatedClaim";
+    var requestEntity = getEntity(content);
+    var response = restTemplate.postForEntity(url, requestEntity, MasResponse.class);
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    var masResponse = response.getBody();
+    assertEquals("MissingAnchor", masResponse.getMessage());
+  }
+
+  @SneakyThrows
+  private String resourceToString(String path) {
+    var io = this.getClass().getClassLoader().getResourceAsStream(path);
+    try (Reader reader = new InputStreamReader(io)) {
+      return FileCopyUtils.copyToString(reader);
+    }
   }
 
   @SneakyThrows
