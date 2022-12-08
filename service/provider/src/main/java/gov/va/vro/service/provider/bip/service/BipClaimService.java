afc4fd7261ac81b83a06cb292e983541f1563570
dimitri-amida <108086155+dimitri-amida@users.noreply.github.com>
2022-12-08 11:25:49 -0800

    MCP-2085 Mock BIP service (#758)
    * Add place holders for BIP claim update codes.
    
    * Update BIP api service code.
    
    * Add test
    
    * update test
    
    * Remove incorrect committed stuff, add comments.
    
    * Update BIP api service code to make naming convention checking happy.
    
    * Add BIP endpoint, and update BIP claim status update service.
    
    * Add code for more BIP endpoint calls
    
    * Add objects for BIP service
    
    * Adjust BIP api service code.
    
    * Temporary fix error.
    
    * Add BIP controller code for additional endpoints
    
    * Fix swagger and lint error for BIP controller.
    
    * MCP-2085: removed classes I don't need
    
    * MCP-2085: removed classes I don't need
    
    * MCP-2085: added anchor check and tests
    
    * MCP-2085: method to remove special issue
    
    * MCP-2085: added methods whose requirements are unclear
    
    * MCP-2085: moved test that's tripping up jacoco
    
    * MCP-2085: removed classes I don't need
    
    * MCP-2085: removed classes I don't need
    
    * MCP-2085: removed classes I don't need
    
    * MCP-2085: implemented "complete processing" logic
    
    * MCP-2085: ignore case when matching strings
    
    * MCP-2085: don't need createContention
    
    * Keep machine user active (#762)
    
    * MCP-2085: refactored MAS controller
    
    * MCP-2085: started hooking up mock
    
    * MCP-2093: started complete-processing route
    
    * MCP-2093: completed putting together routes
    
    * MCP-2093: merged
    
    * MCP-2093: refactored for better readability
    
    * MCP-2093: connected all routes together
    
    * MCP-2093: fixed test
    
    * MCP-2093: bypass "order exam" failure
    
    * MCP-2085: added code comments
    
    Co-authored-by: wlin2019 <wlin305@gmail.com>
    Co-authored-by: Yoom Lam <yoom@navapbc.com>

---
+++ service/provider/src/main/java/gov/va/vro/service/provider/bip/service/BipClaimService.java
@@ -0,0 +1,139 @@
+package gov.va.vro.service.provider.bip.service;
+
+import gov.va.vro.model.bip.ClaimContention;
+import gov.va.vro.model.bip.UpdateContentionReq;
+import gov.va.vro.model.mas.MasAutomatedClaimPayload;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Slf4j
+@Service
+public class BipClaimService {
+
+  public static final String TSOJ = "398";
+  public static final String SPECIAL_ISSUE_1 = "rating decision review - level 1";
+  public static final String SPECIAL_ISSUE_2 = "rrd";
+  public static final String STATUS_READY = "RFD";
+  public static final String STATUS_DECISION_COMPLETE = "Rating Decision Complete";
+
+  private final IBipApiService bipApiService;
+
+  /**
+   * Check if all the anchors for fast-tracking are satisfied
+   *
+   * @param collectionId collection id identifying the claim
+   * @return true if the anchors are satisfied, false otherwise
+   */
+  public boolean hasAnchors(int collectionId) {
+    var claimDetails = bipApiService.getClaimDetails(collectionId);
+    if (claimDetails == null) {
+      log.warn("Claim with collection Id {} not found in BIP", collectionId);
+      return false;
+    }
+    if (!TSOJ.equals(claimDetails.getTempStationOfJurisdiction())) {
+      log.info("Claim with collection Id {} does not have TSOJ = {}", collectionId, TSOJ);
+      return false;
+    }
+    int claimId = Integer.parseInt(claimDetails.getClaimId());
+    var contentions = bipApiService.getClaimContentions(claimId);
+    if (contentions == null) {
+      log.info("Claim with collection Id {} does not have contentions.", collectionId);
+      return false;
+    }
+
+    // collect all special issues
+    var specialIssues =
+        contentions.stream()
+            .map(ClaimContention::getSpecialIssueCodes)
+            .flatMap(Collection::stream)
+            .map(String::toLowerCase) // Ignore case
+            .collect(Collectors.toSet());
+    return specialIssues.contains(SPECIAL_ISSUE_1) && specialIssues.contains(SPECIAL_ISSUE_2);
+  }
+
+  /**
+   * Remove special issue contention
+   *
+   * @param payload the claim payload
+   * @return the claim payload
+   */
+  public MasAutomatedClaimPayload removeSpecialIssue(MasAutomatedClaimPayload payload) {
+    var claimId = payload.getClaimId();
+    log.info("Attempting to remove special issue for claim id = {}", claimId);
+    var contentions = bipApiService.getClaimContentions(claimId);
+    if (ObjectUtils.isEmpty(contentions)) {
+      log.warn("Claim id = {} has no contentions.", claimId);
+      return payload;
+    }
+
+    List<ClaimContention> updatedContentions = new ArrayList<>();
+    for (ClaimContention contention : contentions) {
+      var codes =
+          contention.getSpecialIssueCodes().stream()
+              .map(String::toLowerCase)
+              .collect(Collectors.toSet());
+      if (codes.contains(SPECIAL_ISSUE_1)) {
+        // remove string from contention
+        List<String> updatedCodes =
+            contention.getSpecialIssueCodes().stream()
+                .filter(code -> !SPECIAL_ISSUE_1.equalsIgnoreCase(code))
+                .collect(Collectors.toList());
+        var update = contention.toBuilder().specialIssueCodes(updatedCodes).build();
+        updatedContentions.add(update);
+      }
+    }
+    if (updatedContentions.isEmpty()) {
+      log.info("Special issue for claim id = {} not found. Nothing to update.", claimId);
+      return payload; // nothing to update
+    }
+    log.info("Removing special issue for claim id = {}", claimId);
+    var request = new UpdateContentionReq(updatedContentions);
+    bipApiService.updateClaimContention(claimId, request);
+    return payload;
+  }
+
+  /**
+   * Update claim status
+   *
+   * @param payload the claim payload
+   * @return the claim payload
+   */
+  public MasAutomatedClaimPayload markAsRFD(MasAutomatedClaimPayload payload) {
+    int collectionId = payload.getCollectionId();
+    log.info("Marking claim with collectionId = {} as Ready For Decision", collectionId);
+    var response = bipApiService.updateClaimStatus(collectionId, STATUS_READY);
+    // TODO: check response, catch exceptions etc
+    return payload;
+  }
+
+  /**
+   * Check if claim is still eligible for fast tracking, and if so, update status
+   *
+   * @param payload the claim payload
+   * @return true if the status is updated, false otherwise
+   */
+  public boolean completeProcessing(MasAutomatedClaimPayload payload) {
+    int collectionId = payload.getCollectionId();
+    // check again if TSOJ. If not, abandon route
+    var claim = bipApiService.getClaimDetails(collectionId);
+    if (!TSOJ.equals(claim.getTempStationOfJurisdiction())) {
+      log.info(
+          "Claim with collection Id = {} is in state {}. Not updating status",
+          collectionId,
+          claim.getTempStationOfJurisdiction());
+      return false;
+    }
+    // otherwise, update claim
+    log.info("Updating claim status for claim with collection id = {}", collectionId);
+    bipApiService.updateClaimStatus(collectionId, STATUS_DECISION_COMPLETE);
+    return true;
+  }
+}
