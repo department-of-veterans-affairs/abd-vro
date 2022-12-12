92eb521e5d456457682d7b3ecbcc8a4d3d7fc07a
dimitri-amida <108086155+dimitri-amida@users.noreply.github.com>
2022-12-12 07:50:05 -0800

    MCP-2094: test controller for synchronous processing

---
+++ controller/src/main/java/gov/va/vro/controller/IntegrationTestController.java
@@ -0,0 +1,23 @@
+package gov.va.vro.controller;
+
+import gov.va.vro.api.resources.IntegrationTestResource;
+import gov.va.vro.model.mas.MasAutomatedClaimPayload;
+import gov.va.vro.service.provider.CamelEntrance;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.RestController;
+
+@Profile("!qa & !sandbox & !prod")
+@RestController
+@RequiredArgsConstructor
+public class IntegrationTestController implements IntegrationTestResource {
+
+  private final CamelEntrance camelEntrance;
+
+  @Override
+  public void automatedClaimSync(@RequestBody MasAutomatedClaimPayload payload) {
+    String response = camelEntrance.processClaim(payload);
+    System.out.println(response);
+  }
+}
