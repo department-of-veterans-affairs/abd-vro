bbe83784ea4127baf581565378ad88f04b5d3c9b
dimitri-amida <108086155+dimitri-amida@users.noreply.github.com>
2022-12-15 09:57:08 -0800

    MCP-2039: test coverage

--- app/src/test/java/gov/va/vro/config/MasAuthTest.java
+++ app/src/test/java/gov/va/vro/config/MasAuthTest.java
@@ -1,14 +1,11 @@
 package gov.va.vro.config;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import gov.va.vro.controller.BaseControllerTest;
 import gov.va.vro.security.ApiAuthKeyManager;
-import gov.va.vro.security.InvalidTokenException;
 import gov.va.vro.security.JwtValidator;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -51,30 +48,5 @@ public class MasAuthTest extends BaseControllerTest {
     when(authentication.getPrincipal()).thenReturn(authorizationHdr);
     apiAuthKeyManager.authenticate(authentication);
     verify(authentication).setAuthenticated(true);
-  }
-
-  // TODO: Must find a way to force this test to go via LH API
-  // @Test
-  void testAuthenticateLhApi() {
-    String sampleJwt =
-        """
-               eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImMwOTI5NTJlLTM4ZDYtNDNjNi0\
-               5MzBlLWZmOTNiYTUxYjA4ZiJ9.eyJleHAiOjk5OTk5OTk5OTksImlhdCI6MTY0MTA2Nzk0O\
-               SwianRpIjoiNzEwOTAyMGEtMzlkOS00MWE4LThlNzgtNTllZjAwYTlkNDJlIiwiaXNzIjoi\
-               aHR0cHM6Ly9zYW5kYm94LWFwaS52YS5nb3YvaW50ZXJuYWwvYXV0aC92Mi92YWxpZGF0aW9\
-               uIiwiYXVkIjoibWFzX2RldiIsInN1YiI6IjhjNDkyY2NmLTk0OGYtNDQ1Zi05NmY4LTMxZT\
-               dmODU5MDlkMiIsInR5cCI6IkJlYXJlciIsImF6cCI6Im1hc19kZXYiLCJzY29wZSI6Im9wZ\
-               W5pZCB2cm9fbWFzIiwiY2xpZW50SWQiOiJtYXNfZGV2In0.Qb41CR1JIGGRlryi-XVtqyeN\
-               W73cU1YeBVqs9Bps3TA""";
-
-    String authorizationHdr = "Bearer " + sampleJwt;
-    when(authentication.getPrincipal()).thenReturn(authorizationHdr);
-    //    when(lhApiPropsMock.getValidateToken()).thenReturn("yes");
-    //    when(lhApiPropsMock.getTokenValidatorUrl()).thenReturn("https://placeholder.com/");
-
-    Throwable exception =
-        assertThrows(
-            InvalidTokenException.class, () -> jwtValidator.validateTokenUsingLh(sampleJwt));
-    assertEquals("Could not validate token against LightHouse API", exception.getMessage());
   }
 }
