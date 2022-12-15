405089e4977aa6e038ebfed17ad460ae9ea625d5
dimitri-amida <108086155+dimitri-amida@users.noreply.github.com>
2022-12-15 15:36:12 -0800

    MCP-2039: fixed checkstyle

--- api/src/main/java/gov/va/vro/api/resources/BipResource.java
+++ api/src/main/java/gov/va/vro/api/resources/BipResource.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
