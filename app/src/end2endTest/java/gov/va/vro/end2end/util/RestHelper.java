fb7024c0924089e285ef1dc651bec292edc720be
Yoom Lam <yoom@navapbc.com>
2022-12-19 23:22:59 -0600

    add attemptWithoutApiKey

--- app/src/end2endTest/java/gov/va/vro/end2end/util/RestHelper.java
+++ app/src/end2endTest/java/gov/va/vro/end2end/util/RestHelper.java
@@ -3,18 +3,12 @@ package gov.va.vro.end2end.util;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Assertions;
-import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 @Getter
 @Setter
@@ -40,52 +34,41 @@ public class RestHelper {
    * Generates the health assessment.
    *
    * @param setup test settings
-   * @return health assessment response
+   * @return ResponseEntity containing health assessment
    * @throws Exception any error to fail the test
    */
-  public String getAssessment(TestSetup setup) throws Exception {
-    String req = setup.getAssessmentRequest();
-    HttpHeaders headers = buildHeaders();
-    HttpEntity<String> requestEntity = new HttpEntity<>(req, headers);
+  public ResponseEntity<String> getAssessment(TestSetup setup) {
+    HttpEntity<String> requestEntity = new HttpEntity<>(setup.getAssessmentRequest(), buildHeaders());
     String url = BASE_URL + ASSESSMENT_END_POINT;
     log.info("POST to {} with request: {}", url, requestEntity);
     ResponseEntity<String> response = restTemplate.postForEntity(url, requestEntity, String.class);
     log.info("Got response: {}", response);
-
-    Assertions.assertEquals(HttpStatus.CREATED, response.getStatusCode());
-
-    return response.getBody();
+    return response;
   }
 
   /**
    * Generates the evidence pdf.
    *
    * @param setup test settings
-   * @return generate pdf response
+   * @return ResponseEntity containing generate pdf metadata
    * @throws Exception any error to fail the test
    */
-  public String generatePdf(TestSetup setup) throws Exception {
-    String req = setup.getGeneratePdfRequest();
-    HttpHeaders headers = buildHeaders();
-    HttpEntity<String> requestEntity = new HttpEntity<>(req, headers);
+  public ResponseEntity<String> generatePdf(TestSetup setup) {
+    HttpEntity<String> requestEntity = new HttpEntity<>(setup.getGeneratePdfRequest(), buildHeaders());
     String url = BASE_URL + PDF_END_POINT;
     log.info("POST to {} with request: {}", url, requestEntity);
     ResponseEntity<String> response = restTemplate.postForEntity(url, requestEntity, String.class);
     log.info("Got response: {}", response);
-
-    Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
-
-    return response.getBody();
+    return response;
   }
 
   /**
    * Gets the evidence pdf as a byte array.
    *
    * @param setup test settings
-   * @return evidence pdf as byte array
+   * @return ResponseEntity containing evidence pdf as byte array
    */
-  public byte[] getPdf(TestSetup setup) {
-    String cd = setup.getContentDispositionFilename();
+  public ResponseEntity<byte[]> getPdf(TestSetup setup) {
     String claimSubmissionId = setup.getClaimSubmissionId();
 
     HttpHeaders headers = buildHeaders();
@@ -95,28 +78,6 @@ public class RestHelper {
     ResponseEntity<byte[]> response =
         restTemplate.exchange(url, HttpMethod.GET, requestEntity, byte[].class);
     log.info("Got response of type: {}", response.getBody().getClass().getSimpleName());
-
-    if(Boolean.parseBoolean(System.getenv("VRO_SAVE_PDF"))) {
-      savePdfFile(response.getBody(), setup.getName() + "-" + setup.getContentDispositionFilename());
-    }
-
-    Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
-
-    HttpHeaders responseHeaders = response.getHeaders();
-    log.info("Response headers: {}", responseHeaders);
-    ContentDisposition actualCd = responseHeaders.getContentDisposition();
-    String filename = actualCd.getFilename();
-    Assertions.assertEquals(cd, filename);
-
-    return response.getBody();
-  }
-
-  private void savePdfFile(byte[] pdfContents, String filename) {
-    try (FileOutputStream outputStream = new FileOutputStream(filename)) {
-      outputStream.write(pdfContents);
-      log.info("Saved pdf to: {}", filename);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    return response;
   }
 }
