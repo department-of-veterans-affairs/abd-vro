5578cfd03d9c19a32c2f8d987a55fbab36478503
ianwolson <ian@amida.com>
2023-01-05 16:12:02 -0500

    spotless

--- service/provider/src/main/java/gov/va/vro/service/provider/services/SufficientEvidenceProcessor.java
+++ service/provider/src/main/java/gov/va/vro/service/provider/services/SufficientEvidenceProcessor.java
@@ -1,7 +1,5 @@
 package gov.va.vro.service.provider.services;
 
-import gov.va.vro.model.AbdEvidenceWithSummary;
-import gov.va.vro.service.provider.mas.MasException;
 import gov.va.vro.service.provider.mas.MasProcessingObject;
 import gov.va.vro.service.spi.db.SaveToDbService;
 import lombok.RequiredArgsConstructor;
@@ -15,15 +13,13 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class SufficientEvidenceProcessor implements Processor {
 
-    private final SaveToDbService saveToDbService;
+  private final SaveToDbService saveToDbService;
 
-    @Override
-    public void process(Exchange exchange) {
-       MasProcessingObject payload = (MasProcessingObject) exchange.getProperty("payload");
-       Boolean flag = (Boolean) exchange.getProperty("sufficientForFastTracking");
-       String claimSubmissionId = payload.getClaimId();
-       saveToDbService.updateSufficientEvidenceFlag(claimSubmissionId, flag);
-
-    }
-
+  @Override
+  public void process(Exchange exchange) {
+    MasProcessingObject payload = (MasProcessingObject) exchange.getProperty("payload");
+    Boolean flag = (Boolean) exchange.getProperty("sufficientForFastTracking");
+    String claimSubmissionId = payload.getClaimId();
+    saveToDbService.updateSufficientEvidenceFlag(claimSubmissionId, flag);
+  }
 }
