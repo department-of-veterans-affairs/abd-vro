63f1b0b02a372faba539b7b55414498cf06343cc
Yoom Lam <yoom@navapbc.com>
2023-01-10 11:34:07 -0600

    add notifications to deploy-app.sh

--- scripts/deploy-app.sh
+++ scripts/deploy-app.sh
@@ -71,9 +71,12 @@ COMMON_HELM_ARGS="--set-string environment=${ENV} \
 # K8s namespace
 NAMESPACE="${TEAMNAME}-${ENV}"
 
+source scripts/notify-slack.src "\`$0\`: Uninstalling \`${HELM_APP_NAME}\` from \`${NAMESPACE}\`"
 helm del $HELM_APP_NAME -n ${NAMESPACE}
-echo "Allowing time for helm to delete $HELM_APP_NAME before creating a new one"
-#sleep 60 # wait for Persistent Volume Claim to be deleted
+# echo "Allowing time for helm to delete $HELM_APP_NAME before creating a new one"
+# sleep 60 # wait for Persistent Volume Claim to be deleted
+
+source scripts/notify-slack.src "\`$0\`: Deploying new \`${HELM_APP_NAME}\` ENV=\`${ENV}\` IMAGE_TAG=\`${IMAGE_TAG}\`"
 helm upgrade --install $HELM_APP_NAME helm-app \
               ${COMMON_HELM_ARGS} ${VRO_IMAGE_ARGS} \
               --debug \
