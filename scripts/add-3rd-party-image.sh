bb2873d4ee85a57310348ef939368433883f7049
Yoom Lam <yoom@navapbc.com>
2023-01-13 09:11:43 -0600

    add add-3rd-party-image.sh

---
+++ scripts/add-3rd-party-image.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# To work around https://www.docker.com/increase-rate-limits/, this script publishes the
+# specified image to GHCR and optionally creates a helm configuration for the service.
+
+# ${{ github.repository }}
+: ${REPO:=department-of-veterans-affairs/abd-vro}
+
+# Image to publish
+IMAGE="rabbitmq"
+IMG_TAG="3"
+
+# Target image; renamed to avoid uncertainty of image being used
+TARGET_IMAGE="vro-$IMAGE"
+# Target path where the image will be pushed
+# https://github.com/orgs/department-of-veterans-affairs/packages?repo_name=abd-vro
+GHCR_PATH="ghcr.io/${REPO}/$TARGET_IMAGE:$IMG_TAG"
+
+# Pull, tag, and push these third-party images
+docker pull "$IMAGE:$IMG_TAG"
+docker tag "$IMAGE:$IMG_TAG" "$GHCR_PATH"
+docker push "$GHCR_PATH"
