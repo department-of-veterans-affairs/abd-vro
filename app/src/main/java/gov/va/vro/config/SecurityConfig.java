08d5c5df0579eec5780b0d85a66b8697c232ac1e
dimitri-amida <108086155+dimitri-amida@users.noreply.github.com>
2023-01-18 08:02:24 -0800

    MCP-2247: fix NPE in BipClaimService (#951)
    * MCP-2247: fix NPE
    
    * MCP-2247: use BipException for consistency

--- app/src/main/java/gov/va/vro/config/SecurityConfig.java
+++ app/src/main/java/gov/va/vro/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package gov.va.vro.config;
 import gov.va.vro.security.ApiAuthKeyFilter;
 import gov.va.vro.security.ApiAuthKeyManager;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +15,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
-@Slf4j
 @Configuration
 @EnableWebSecurity
 @Order(Ordered.HIGHEST_PRECEDENCE)
