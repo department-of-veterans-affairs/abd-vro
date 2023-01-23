d205cf509e6a49e186b88b63193c6001251537ee
ianwolson <ian@amida.com>
2023-01-23 12:45:19 -0500

    Fix spotless

--- service/provider/src/main/java/gov/va/vro/service/provider/camel/MasIntegrationRoutes.java
+++ service/provider/src/main/java/gov/va/vro/service/provider/camel/MasIntegrationRoutes.java
@@ -75,7 +75,6 @@ public class MasIntegrationRoutes extends RouteBuilder {
 
   private final EvidenceSummaryDocumentProcessor evidenceSummaryDocumentProcessor;
 
-
   @Override
   public void configure() {
     configureAuditing();
