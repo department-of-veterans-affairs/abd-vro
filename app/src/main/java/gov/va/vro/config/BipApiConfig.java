3574ba9a11052efe57bc0b942609f66ae8393191
Afsin Ustundag <afsin@amida-tech.com>
2023-01-25 10:37:09 -0500

    BIP Claims Evidence Final Integration (#992)
    bip ce final integration
    
    Co-authored-by: Afsin Ustundag <afsin@amida.com>
    Co-authored-by: dimitri-amida <108086155+dimitri-amida@users.noreply.github.com>

--- app/src/main/java/gov/va/vro/config/BipApiConfig.java
+++ app/src/main/java/gov/va/vro/config/BipApiConfig.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -66,7 +65,6 @@ public class BipApiConfig {
     return keyStore;
   }
 
-  @Primary
   @Bean(name = "bipCERestTemplate")
   public RestTemplate getHttpsRestTemplate(RestTemplateBuilder builder) throws BipException {
     try {
--- service/provider/src/main/java/gov/va/vro/service/provider/BipApiProps.java
+++ service/provider/src/main/java/gov/va/vro/service/provider/BipApiProps.java
@@ -1,11 +1,19 @@
 package gov.va.vro.service.provider;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * Properties used in BIP API service.
+ * Properties used in BIP API services.
  *
  * @author warren @Date 10/31/22
  */
@@ -37,4 +45,27 @@ public class BipApiProps {
   private String applicationId;
 
   private String applicationName;
+
+  /**
+   * Creates common Jwt claims.
+   *
+   * @return Claims
+   */
+  public Claims toCommonJwtClaims() {
+    Calendar cal = Calendar.getInstance();
+    cal.add(Calendar.MINUTE, 30);
+    Date expired = cal.getTime();
+    Claims claims = Jwts.claims();
+    claims.put("jti", jti);
+    claims.put("applicationID", applicationId);
+    claims.put("stationID", stationId);
+    claims.put("userID", claimClientId);
+    Date now = cal.getTime();
+    claims.put("iat", now.getTime());
+    claims.put("expires", expired.getTime());
+    Map<String, Object> headerType = new HashMap<>();
+    headerType.put("typ", Header.JWT_TYPE);
+
+    return claims;
+  }
 }
