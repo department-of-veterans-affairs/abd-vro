7bdec6c590d90e40f8868cda3080ca7735d6eacb
julianbregstone <julian>
2023-01-26 14:54:41 -0500

    change location of date setting

--- service/provider/src/main/java/gov/va/vro/service/provider/camel/MasIntegrationProcessors.java
+++ service/provider/src/main/java/gov/va/vro/service/provider/camel/MasIntegrationProcessors.java
@@ -67,7 +67,6 @@ public class MasIntegrationProcessors {
                     .claimSubmissionId(payload.getClaimId())
                     .diagnosticCode(payload.getDiagnosticCode())
                     .veteranIcn(payload.getVeteranIcn())
-                    .claimSubmissionDateTime(payload.getClaimSubmissionDateTime())
                     .build());
   }
 
