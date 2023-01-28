745d3831ebe20f1dae46c98a977d910783a858ba
Afsin Ustundag <afsin@amida.com>
2023-01-27 21:50:13 -0500

    remove swagger artifacts

--- mock-bip-ce-api/src/main/java/gov/va/vro/mockbipce/Application.java
+++ mock-bip-ce-api/src/main/java/gov/va/vro/mockbipce/Application.java
@@ -8,15 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication(
     exclude = {
         org.springdoc.hateoas.SpringDocHateoasConfiguration.class
-    },
-    excludeName = {
-        "org.apache.camel.spring.boot.CamelAutoConfiguration"
-    })
-@ComponentScan(
-    basePackages = {
-      "org.openapitools",
-      "gov.va.vro.mockbipce.api",
-      "org.openapitools.configuration"
     })
 public class Application {
   public static void main(String[] args) {
