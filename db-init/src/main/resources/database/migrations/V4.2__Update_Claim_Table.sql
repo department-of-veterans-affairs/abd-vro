386ff76899b3b88cf5979e56aae10c189916ef98
alan-amida <alan.thompson@amida.com>
2023-02-02 21:57:05 -0500

    Adjust presumptive_flag data type to boolean

--- db-init/src/main/resources/database/migrations/V4.2__Update_Claim_Table.sql
+++ db-init/src/main/resources/database/migrations/V4.2__Update_Claim_Table.sql
@@ -1,5 +1,5 @@
 ALTER TABLE claim 
 ADD COLUMN vbms_id VARCHAR, -- uuid?
 ADD COLUMN off_ramp_reason VARCHAR, 
-ADD COLUMN presumptive_flag VARCHAR, 
+ADD COLUMN presumptive_flag BOOLEAN, 
 ADD COLUMN disability_action_type VARCHAR;
