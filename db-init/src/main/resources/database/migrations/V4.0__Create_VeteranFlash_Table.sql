7efe3110d332c5c1cfcf5df4e6cabcfb5e49a734
alan-amida <alan.thompson@amida.com>
2023-02-03 23:39:30 -0500

    Change schema migrations based on Dimitri's feedback

--- db-init/src/main/resources/database/migrations/V4.0__Create_VeteranFlash_Table.sql
+++ db-init/src/main/resources/database/migrations/V4.0__Create_VeteranFlash_Table.sql
@@ -1,7 +1,8 @@
+-- collection_id (claim) has many flash_ids
 CREATE TABLE IF NOT EXISTS order_exam (
     id uuid DEFAULT uuid_generate_v4 () NOT NULL,
-    collecton_id VARCHAR,
-    status VARCHAR,
+    collecton_id VARCHAR NOT NULL,
+    flash_id INTEGER NOT NULL,
     PRIMARY KEY(id),
     CONSTRAINT fk_collecton_id
         FOREIGN KEY (collecton_id)
--- db-init/src/main/resources/database/migrations/V4.3__Update_Veteran_Table.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE veteran 
-ADD COLUMN icn_timestamp timestamp NOT NULL;
--- flashIds... list of integers... relationship table??? VeteranFlashEntity?
--- otherwise, there is the integer[] column type... but it is Postgres-specific,
--- other DBMSs might not support this
