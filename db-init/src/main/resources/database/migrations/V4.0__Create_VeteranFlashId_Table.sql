5f65e5f629d3a476d10d90ad388abf0ddec72e29
alan-amida <alan.thompson@amida.com>
2023-02-05 14:00:02 -0500

    Change veteranFlashId field FK to veteran_icn and remove collection_id field

--- db-init/src/main/resources/database/migrations/V4.0__Create_VeteranFlashId_Table.sql
+++ db-init/src/main/resources/database/migrations/V4.0__Create_VeteranFlashId_Table.sql
@@ -1,7 +1,10 @@
--- create association table: a collection_id (claim) can have many flash_ids
+-- create association table: a veteran can have many flash_ids
 CREATE TABLE IF NOT EXISTS veteran_flash_id (
     id uuid DEFAULT uuid_generate_v4 () NOT NULL,
-    collection_id VARCHAR NOT NULL,
+    veteran_icn VARCHAR NOT NULL,
     flash_id INTEGER NOT NULL,
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
+    CONSTRAINT fk_veteran_icn
+    FOREIGN KEY (veteran_icn)
+        REFERENCES veteran(icn)
 );
