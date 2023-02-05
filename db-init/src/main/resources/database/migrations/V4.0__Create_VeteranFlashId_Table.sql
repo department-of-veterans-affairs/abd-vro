78e93a730f69bbaee14f9555f96e0289cd8c538e
alan-amida <alan.thompson@amida.com>
2023-02-04 23:20:46 -0500

    Fix typo for collection_id in migration scripts

--- db-init/src/main/resources/database/migrations/V4.0__Create_VeteranFlashId_Table.sql
+++ db-init/src/main/resources/database/migrations/V4.0__Create_VeteranFlashId_Table.sql
@@ -1,10 +1,10 @@
 -- create association table: a collection_id (claim) can have many flash_ids
 CREATE TABLE IF NOT EXISTS veteran_flash_id (
     id uuid DEFAULT uuid_generate_v4 () NOT NULL,
-    collecton_id VARCHAR NOT NULL,
+    collection_id VARCHAR NOT NULL,
     flash_id INTEGER NOT NULL,
     PRIMARY KEY(id),
-    CONSTRAINT fk_collecton_id
-        FOREIGN KEY (collecton_id)
-            REFERENCES claim(collecton_id)
+    CONSTRAINT fk_collection_id
+        FOREIGN KEY (collection_id)
+            REFERENCES claim(collection_id)
 );
--- db-init/src/main/resources/database/migrations/V4.1__Create_OrderExam_Table.sql
+++ db-init/src/main/resources/database/migrations/V4.1__Create_OrderExam_Table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS order_exam (
     id uuid DEFAULT uuid_generate_v4 () NOT NULL,
-    collecton_id VARCHAR,
+    collection_id VARCHAR,
     status VARCHAR,
     PRIMARY KEY(id),
-    CONSTRAINT fk_collecton_id
-        FOREIGN KEY (collecton_id)
-            REFERENCES claim(collecton_id)
+    CONSTRAINT fk_collection_id
+        FOREIGN KEY (collection_id)
+            REFERENCES claim(collection_id)
 );
