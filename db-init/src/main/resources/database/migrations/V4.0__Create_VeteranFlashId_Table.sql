e7ea9c60a635463035c595caadcffea1f2fcebaa
alan-amida <alan.thompson@amida.com>
2023-02-04 23:40:54 -0500

    Remove FK ref to collection_id since it is not unique in claim table

--- db-init/src/main/resources/database/migrations/V4.0__Create_VeteranFlashId_Table.sql
+++ db-init/src/main/resources/database/migrations/V4.0__Create_VeteranFlashId_Table.sql
@@ -3,8 +3,5 @@ CREATE TABLE IF NOT EXISTS veteran_flash_id (
     id uuid DEFAULT uuid_generate_v4 () NOT NULL,
     collection_id VARCHAR NOT NULL,
     flash_id INTEGER NOT NULL,
-    PRIMARY KEY(id),
-    CONSTRAINT fk_collection_id
-        FOREIGN KEY (collection_id)
-            REFERENCES claim(collection_id)
+    PRIMARY KEY(id)
 );
--- db-init/src/main/resources/database/migrations/V4.1__Create_OrderExam_Table.sql
+++ db-init/src/main/resources/database/migrations/V4.1__Create_OrderExam_Table.sql
@@ -2,8 +2,5 @@ CREATE TABLE IF NOT EXISTS order_exam (
     id uuid DEFAULT uuid_generate_v4 () NOT NULL,
     collection_id VARCHAR,
     status VARCHAR,
-    PRIMARY KEY(id),
-    CONSTRAINT fk_collection_id
-        FOREIGN KEY (collection_id)
-            REFERENCES claim(collection_id)
+    PRIMARY KEY(id)
 );
