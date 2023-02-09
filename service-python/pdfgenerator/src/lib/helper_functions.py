136238d45aa2b48cb296e472c25cb41dc48fe592
julianbregstone <julian>
2023-02-09 09:59:38 -0500

    move helper back

--- service-python/pdfgenerator/src/lib/helper_functions.py
+++ service-python/pdfgenerator/src/lib/helper_functions.py
@@ -7,10 +7,6 @@ from dateutil.relativedelta import relativedelta
 
 def pdf_helper_asthma(data):
 
-    if "evidence" in data:
-        for medication_info in data["evidence"]["medications"]:
-            medication_info["authoredOn"] = parser.parse(medication_info["authoredOn"])
-
     return data
 
 
@@ -21,6 +17,10 @@ def pdf_helper_all(data):
     data["evidence"] = data["evidence"]
     if data["veteranInfo"]["birthdate"] != "":
         data["veteranInfo"]["birthdate"] = parser.parse(data["veteranInfo"]["birthdate"])
+
+    if "evidence" in data:
+        for medication_info in data["evidence"]["medications"]:
+            medication_info["authoredOn"] = parser.parse(medication_info["authoredOn"])
 
     return data
 
