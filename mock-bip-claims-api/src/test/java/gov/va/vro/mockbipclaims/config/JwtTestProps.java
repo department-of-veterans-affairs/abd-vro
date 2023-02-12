6661e896ad772417c9d391a17ee72385f6f405aa
Afsin Ustundag <afsin@amida.com>
2023-02-12 15:59:43 -0500

    code cleaning

--- mock-bip-claims-api/src/test/java/gov/va/vro/mockbipclaims/config/JwtTestProps.java
+++ mock-bip-claims-api/src/test/java/gov/va/vro/mockbipclaims/config/JwtTestProps.java
@@ -1,31 +1,52 @@
 package gov.va.vro.mockbipclaims.config;
 
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.DefaultClaims;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import javax.crypto.spec.SecretKeySpec;
 
 @Getter
 @Setter
-@NoArgsConstructor
-public class JwtTestProps {
-  private String userId;
+public class JwtTestProps extends DefaultClaims {
   private String secret;
-  private String issuer;
-  private String stationId;
-  private String applicationId;
-  private int expirationSeconds;
 
   /**
-   * Test Jwt properties that are use to generate Jwt for tests.
+   * Test Jwt properties that are used to generate Jwt for tests.
    *
    * @param props Test properties
    */
   public JwtTestProps(JwtProps props) {
-    userId = props.getUserId();
     secret = props.getSecret();
-    issuer = props.getIssuer();
-    stationId = props.getStationId();
-    applicationId = props.getApplicationId();
-    expirationSeconds = props.getExpirationSeconds();
+    setSubject("Claim");
+    setIssuer(props.getIssuer());
+    put("applicationID", props.getApplicationId());
+    put("userID", props.getUserId());
+    put("stationID", props.getStationId());
+
+    Calendar currentTime = GregorianCalendar.getInstance();
+    Date now = currentTime.getTime();
+    setIssuedAt(now);
+
+    Calendar expiration = GregorianCalendar.getInstance();
+    expiration.setTime(now);
+    expiration.add(Calendar.SECOND, props.getExpirationSeconds());
+    setExpiration(expiration.getTime());
+  }
+
+  public SignatureAlgorithm getSignatureAlgorithm() {
+    return SignatureAlgorithm.HS256;
+  }
+
+  public Key getSigningKey() {
+    SignatureAlgorithm signatureAlgorithm = getSignatureAlgorithm();
+    byte[] secretBytes = getSecret().getBytes(StandardCharsets.UTF_8);
+    return new SecretKeySpec(secretBytes, signatureAlgorithm.getJcaName());
   }
 }
