350498e836423422b2181f364f1a63c7958a4531
Afsin Ustundag <afsin@amida.com>
2023-02-12 20:03:47 -0500

    code cleaning

--- app/src/end2endTest/java/gov/va/vro/end2end/VroV2Tests.java
+++ app/src/end2endTest/java/gov/va/vro/end2end/VroV2Tests.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gov.va.vro.api.responses.MasResponse;
-import gov.va.vro.end2end.util.ModifyingActionsResponse;
+import gov.va.vro.end2end.util.UpdatesResponse;
 import gov.va.vro.end2end.util.PdfTextV2;
 import gov.va.vro.model.mas.request.MasAutomatedClaimRequest;
 import lombok.SneakyThrows;
@@ -31,6 +31,9 @@ public class VroV2Tests {
   private static final String BASE_URL = "http://localhost:8080/v2";
   private static final String EXAM_ORDERING_STATUS_URL = BASE_URL + "/examOrderingStatus";
   private static final String AUTOMATED_CLAIM_URL = BASE_URL + "/automatedClaim";
+
+  private static final String UPDATES_URL = "http://localhost:8099/updates/";
+  private static final String RECEIVED_FILES_URL = "http://localhost:8096/received-files/";
 
   private static final String JWT_TOKEN =
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImMwOTI5NTJlLTM4ZDYtNDNjNi05MzBlLWZmOTNiYTUxYjA4ZiJ9.eyJleHAiOjk5OTk5OTk5OTksImlhdCI6MTY0MTA2Nzk0OSwianRpIjoiNzEwOTAyMGEtMzlkOS00MWE4LThlNzgtNTllZjAwYTlkNDJlIiwiaXNzIjoiaHR0cHM6Ly9zYW5kYm94LWFwaS52YS5nb3YvaW50ZXJuYWwvYXV0aC92Mi92YWxpZGF0aW9uIiwiYXVkIjoibWFzX2RldiIsInN1YiI6IjhjNDkyY2NmLTk0OGYtNDQ1Zi05NmY4LTMxZTdmODU5MDlkMiIsInR5cCI6IkJlYXJlciIsImF6cCI6Im1hc19kZXYiLCJzY29wZSI6Im9wZW5pZCB2cm9fbWFzIiwiY2xpZW50SWQiOiJtYXNfZGV2In0.Qb41CR1JIGGRlryi-XVtqyeNW73cU1YeBVqs9Bps3TA";
@@ -87,10 +90,10 @@ public class VroV2Tests {
   private boolean getFoundStatus(String claimId, String type) {
     for (int pollNumber = 0; pollNumber < 10; ++pollNumber) {
       Thread.sleep(10);
-      String url = "http://localhost:8099/updates/" + claimId + "/" + type;
-      var testResponse = restTemplate.getForEntity(url, ModifyingActionsResponse.class);
+      String url = UPDATES_URL + claimId + "/" + type;
+      var testResponse = restTemplate.getForEntity(url, UpdatesResponse.class);
       assertEquals(HttpStatus.OK, testResponse.getStatusCode());
-      ModifyingActionsResponse body = testResponse.getBody();
+      UpdatesResponse body = testResponse.getBody();
       if (body.isFound()) {
         log.info("{} is updated.", type);
         return true;
@@ -114,8 +117,8 @@ public class VroV2Tests {
 
     if ("end2end-test".equals(System.getenv("ENV"))) {
       log.info("Reset data in the mock servers.");
-      restTemplate.delete("http://localhost:8099/updates/" + claimId);
-      restTemplate.delete("http://localhost:8096/received-files/" + fileNumber);
+      restTemplate.delete(UPDATES_URL + claimId);
+      restTemplate.delete(RECEIVED_FILES_URL + fileNumber);
     }
 
     var requestEntity = getEntity(content);
@@ -133,7 +136,7 @@ public class VroV2Tests {
     boolean successUploading = false;
     for (int pollNumber = 0; pollNumber < 15; ++pollNumber) {
       Thread.sleep(20000);
-      String url = "http://localhost:8096/received-files/" + fileNumber;
+      String url = RECEIVED_FILES_URL + fileNumber;
       try {
         ResponseEntity<byte[]> testResponse = restTemplate.getForEntity(url, byte[].class);
         assertEquals(HttpStatus.OK, testResponse.getStatusCode());
--- app/src/end2endTest/java/gov/va/vro/end2end/util/UpdatesResponse.java
+++ app/src/end2endTest/java/gov/va/vro/end2end/util/UpdatesResponse.java
@@ -5,6 +5,6 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class ModifyingActionsResponse {
+public class UpdatesResponse {
   private boolean found;
 }
