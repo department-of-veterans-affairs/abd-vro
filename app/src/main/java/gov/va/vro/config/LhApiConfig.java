520bc6892286d0343a23140afdb5b66705176e6c
Afsin Ustundag <afsin@amida-tech.com>
2023-02-21 19:39:54 -0500

    Service nnotation to mock data (#1149)
    * add service to mock
    
    * log message
    
    ---------
    
    Co-authored-by: Afsin Ustundag <afsin@amida.com>

--- app/src/main/java/gov/va/vro/config/LhApiConfig.java
+++ app/src/main/java/gov/va/vro/config/LhApiConfig.java
@@ -1,11 +1,13 @@
 package gov.va.vro.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 // Configure LH APIs Related Attributes
 @Configuration
+@Slf4j
 public class LhApiConfig {
 
   @Bean
@@ -14,6 +16,7 @@ public class LhApiConfig {
       @Value("${lhAPIProvider.vroAudURL}") String vroAudUrl,
       @Value("${lhAPIProvider.apiKey}") String apiKey,
       @Value("${lhAPIProvider.validateToken}") String validateToken) {
+    log.info("VRO token/Aud URL " + tokenValidatorUrl + "/" + vroAudUrl);
     return new LhApiProps(tokenValidatorUrl, vroAudUrl, apiKey, validateToken);
   }
 }
