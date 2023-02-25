16752a9f84cd1917a990e76608b7531cb904308d
Yoom Lam <yoom@navapbc.com>
2023-02-25 00:06:14 -0600

    remove image-names-db.sh and image_vars_db.src

--- scripts/deploy-db.sh
+++ scripts/deploy-db.sh
@@ -23,8 +23,7 @@ else
   VERSION=${GIT_SHA:0:7}
 fi
 
-source scripts/image_vars_db.src
-generateImageArgs(){
+generateDbImageArgs(){
   local _IMAGE_TAG=$2
 
   # sandbox (in nonprod cluster) and prod and prod-test (in the prod cluster) requires signed-images from SecRel
@@ -40,14 +39,12 @@ generateImageArgs(){
     echo "--set-string images.repo=abd-vro-internal "
   fi
 
-  for PREFIX in "${VAR_PREFIXES_ARR[@]}"; do
-    local HELM_KEY=$(getVarValue "${PREFIX}" _HELM_KEY)
-    local IMAGE_NAME=${IMG_NAME_PREFIX}$(getVarValue "${PREFIX}" _IMG)
-    echo "--set-string images.$HELM_KEY.tag=${_IMAGE_TAG} "
-    echo "--set-string images.$HELM_KEY.imageName=${IMAGE_NAME} "
-  done
+  local HELM_KEY="db"
+  local IMAGE_NAME="${IMG_NAME_PREFIX}vro-postgres"
+  echo "--set-string images.$HELM_KEY.tag=${_IMAGE_TAG} "
+  echo "--set-string images.$HELM_KEY.imageName=${IMAGE_NAME} "
 }
-VRO_IMAGE_ARGS=$(generateImageArgs "${ENV}" "${IMAGE_TAG}")
+VRO_IMAGE_ARGS=$(generateDbImageArgs "${ENV}" "${IMAGE_TAG}")
 
 COMMON_HELM_ARGS="--set-string environment=${ENV} \
 --set-string info.version=${IMAGE_TAG} \
