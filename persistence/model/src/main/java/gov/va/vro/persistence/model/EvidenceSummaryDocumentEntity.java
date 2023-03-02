225ece5e5a31ac3f80da65c6f9a18e90f0a4513b
ianwolson <109168960+ianwolson@users.noreply.github.com>
2023-03-02 15:44:19 -0500

    MCP-2389 : Add eFolder ID to ESDs in VRO DB + MCP-2403: Fix V2 ESDs not saving (#1155)
    * MCP-2389 Add Veteran File ID to Evidence Summary Documents
    
    * jacoco test coverage fixes
    
    * MCP-2403 fix v2 not saving esds
    
    * Adding folder ID to esd
    
    * Adding folder ID to esd
    
    * Adding folder ID to esd
    
    * IBipCeApiService
    
    * EvidenceSummaryDocumentRepository
    
    * ClaimRepositoryTest
    
    * adjusted logic
    
    * spotless
    
    * spotless
    
    * fix folder id persisting issue
    
    * Addressing PR comments
    
    * Adding comment for conditional to be removed
    
    * Addressing Hayley's PR comments
    
    ---------
    
    Co-authored-by: Alan Thompson <119633398+alan-amida@users.noreply.github.com>

--- persistence/model/src/main/java/gov/va/vro/persistence/model/EvidenceSummaryDocumentEntity.java
+++ persistence/model/src/main/java/gov/va/vro/persistence/model/EvidenceSummaryDocumentEntity.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 
 import java.util.Map;
+import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
@@ -27,4 +28,6 @@ public class EvidenceSummaryDocumentEntity extends BaseEntity {
   private Map<String, String> evidenceCount;
 
   private String documentName;
+
+  private UUID folderId;
 }
