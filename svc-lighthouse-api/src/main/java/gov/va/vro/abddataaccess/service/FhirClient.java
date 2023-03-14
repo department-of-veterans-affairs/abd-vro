5cb96eb22e2214b99b9c2125bb8cb8000a0fcc8c
warren <wlin305@gmail.com>
2023-03-14 13:45:05 -0400

    Remove some testing logs.

--- svc-lighthouse-api/src/main/java/gov/va/vro/abddataaccess/service/FhirClient.java
+++ svc-lighthouse-api/src/main/java/gov/va/vro/abddataaccess/service/FhirClient.java
@@ -305,9 +305,7 @@ public class FhirClient {
    * @throws AbdException error occurs.
    */
   public AbdEvidence getMedicalEvidence(AbdClaim claim) throws AbdException {
-    log.info("===Get LH FHIR data");
     Map<AbdDomain, List<BundleEntryComponent>> components = getDomainBundles(claim);
-    log.info("===Received LH FHIR data");
     if (components == null) {
       return null;
     }
@@ -322,7 +320,6 @@ public class FhirClient {
    */
   @NotNull
   public AbdEvidence getAbdEvidence(Map<AbdDomain, List<BundleEntryComponent>> components) {
-    log.info("========converting LH Fhir data");
     AbdEvidence result = new AbdEvidence();
     for (Map.Entry<AbdDomain, List<BundleEntryComponent>> entryComponent : components.entrySet()) {
       List<BundleEntryComponent> entries = entryComponent.getValue();
@@ -347,7 +344,6 @@ public class FhirClient {
         }
       }
     }
-    log.info("========got converted LH Fhir data");
     return result;
   }
 }
