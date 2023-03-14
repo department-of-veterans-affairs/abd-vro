33f4910d1368f611e505ab3bedd04f1ab2e05630
Afsin Ustundag <afsin@amida.com>
2023-03-14 00:15:29 -0400

    merge develop fixes

--- mocks/mock-slack/src/main/java/gov/va/vro/mockslack/MockSlackApplication.java
+++ mocks/mock-slack/src/main/java/gov/va/vro/mockslack/MockSlackApplication.java
@@ -4,11 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(
-    exclude = {
-      org.springdoc.hateoas.SpringDocHateoasConfiguration.class,
-      DataSourceAutoConfiguration.class
-    })
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MockSlackApplication {
   public static void main(String[] args) {
     SpringApplication.run(MockSlackApplication.class, args);
