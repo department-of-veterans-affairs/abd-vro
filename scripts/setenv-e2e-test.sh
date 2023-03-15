1cec1809f1722352cbfd96fe393c2d9851a2c71b
Afsin Ustundag <afsin@amida.com>
2023-03-15 09:25:32 -0400

    move mock spec to actions

---
+++ scripts/setenv-e2e-test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This script is for automated end2end testing.
+# It differs from setenv.sh.
+
+if [[ $0 == $BASH_SOURCE ]]; then
+  echo "Usage: At the root of the abd-vro folder, run:
+    source scripts/setenv-e2e-test.sh [path/to/abd-vro-dev-secrets]"
+  exit 1
+fi
+
+export ENV=end2end-test
+source scripts/setenv.sh
+export SLACK_EXCEPTION_WEBHOOK=http://mock-slack:9008/slack-messages
