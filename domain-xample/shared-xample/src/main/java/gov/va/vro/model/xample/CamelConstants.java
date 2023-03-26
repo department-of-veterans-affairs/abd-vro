20fcee721833e8df0eda93bf026e1e0c556a68e8
Yoom Lam <yoom@navapbc.com>
2023-03-26 00:08:08 -0500

    quickfix test

--- domain-xample/shared-xample/src/main/java/gov/va/vro/model/xample/CamelConstants.java
+++
@@ -1,7 +0,0 @@
-package gov.va.vro.model.xample;
-
-public class CamelConstants {
-  public static final String V3_EXCHANGE = "v3";
-  public static final String POST_RESOURCE_QUEUE = "postResource";
-  public static final String GET_RESOURCE_QUEUE = "getResource";
-}
--- domain-xample/shared-xample/src/main/java/gov/va/vro/model/xample/StatusValue.java
+++
@@ -1,9 +0,0 @@
-package gov.va.vro.model.xample;
-
-public enum StatusValue {
-  ERROR,
-  RECEIVED,
-  PROCESSING,
-  DONE,
-  NOT_FOUND
-}
