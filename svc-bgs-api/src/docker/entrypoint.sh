3964cdf67f9eb8925864d7d89bfa99bd74117ce7
Yoom Lam <yoom@navapbc.com>
2023-03-27 16:54:20 -0500

    Add svc-bgs-api

---
+++ svc-bgs-api/src/docker/entrypoint.sh
@@ -0,0 +1,6 @@
+# This will be called by entrypoint-default.sh,
+# after it sets environment variables for secrets
+
+# Uncomment when ready; in the meantime just run bash
+# rails server -b 0.0.0.0
+bash
