e19fc8f045c5ab5b1ce919f49ccae56ba73f6b49
julianbregstone <julian>
2023-03-27 10:16:04 -0400

    spectral

--- shared/domain/src/main/java/gov/va/vro/model/AbdEncounter.java
+++ shared/domain/src/main/java/gov/va/vro/model/AbdEncounter.java
@@ -13,16 +13,15 @@ import lombok.Setter;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class AbdEncounter {
-    @EqualsAndHashCode.Include private String text;
-    @EqualsAndHashCode.Include private String code;
-    @EqualsAndHashCode.Include private String serviceProvider;
-    @EqualsAndHashCode.Include private String location;
-    @EqualsAndHashCode.Include private String reasonCode;
-    @EqualsAndHashCode.Include private String status;
-    @EqualsAndHashCode.Include private String periodEnd;
-    @EqualsAndHashCode.Include private String encounterClass;
+  @EqualsAndHashCode.Include private String text;
+  @EqualsAndHashCode.Include private String code;
+  @EqualsAndHashCode.Include private String serviceProvider;
+  @EqualsAndHashCode.Include private String location;
+  @EqualsAndHashCode.Include private String reasonCode;
+  @EqualsAndHashCode.Include private String status;
+  @EqualsAndHashCode.Include private String periodEnd;
+  @EqualsAndHashCode.Include private String encounterClass;
 
-    @Schema(description = "Source of this data", example = "LH")
-    private String dataSource = "LH";
-
+  @Schema(description = "Source of this data", example = "LH")
+  private String dataSource = "LH";
 }
