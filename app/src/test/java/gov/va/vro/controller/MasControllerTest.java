d71271296551ca9a5f8dd5ddab3efde1c21d2a06
Anthony Velazquez <anthonyhvelazquez@gmail.com>
2023-03-28 11:10:12 -0400

    spotless

--- app/src/test/java/gov/va/vro/controller/MasControllerTest.java
+++ app/src/test/java/gov/va/vro/controller/MasControllerTest.java
@@ -2,9 +2,9 @@ package gov.va.vro.controller;
 
 import static org.apache.camel.builder.AdviceWith.adviceWith;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gov.va.vro.BipServiceTestConfiguration;
