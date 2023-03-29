a0b68ad020823cb36d3d574c2012f35c9c44fbdf
Hayley Dawson <hayley.dawson@amida.com>
2023-03-29 16:37:59 -0400

    set offramp on the claim payload and use that in completion

--- service/provider/src/main/java/gov/va/vro/service/provider/camel/MasIntegrationProcessors.java
+++ service/provider/src/main/java/gov/va/vro/service/provider/camel/MasIntegrationProcessors.java
@@ -141,6 +141,14 @@ public class MasIntegrationProcessors {
     };
   }
 
+  public static Processor setOffRampReasonProcessor(String offRampReason) {
+    return exchange -> {
+      MasProcessingObject mpoOfframp = exchange.getMessage().getBody(MasProcessingObject.class);
+      mpoOfframp.getClaimPayload().setOffRampReason(offRampReason);
+      exchange.getMessage().setBody(mpoOfframp);
+    };
+  }
+
   /**
    * At the conclusion of automated claim processing this processor updates claims and contentions
    * using BIP Claims API.
@@ -149,24 +157,20 @@ public class MasIntegrationProcessors {
    * @return Processor completion camel processor
    */
   public static Processor completionProcessor(
-      String routeId, BipClaimService bipClaimService, MasProcessingService masProcessingService) {
+      BipClaimService bipClaimService, MasProcessingService masProcessingService) {
     return exchange -> {
       MasProcessingObject payload = exchange.getIn().getBody(MasProcessingObject.class);
       MasCamelStage origin = payload.getOrigin();
       Boolean sufficient = exchange.getProperty("sufficientForFastTracking", Boolean.class);
-      String offRampError = exchange.getProperty("offRampError", String.class);
       String offRampErrorPayload = payload.getOffRampReason();
 
       // Update our database with offramp reason.
-      if (offRampError != null || offRampErrorPayload != null) {
-        masProcessingService.offRampClaimForError(payload, offRampError);
+      if (offRampErrorPayload != null) {
+        masProcessingService.offRampClaimForError(payload, offRampErrorPayload);
       }
       MasCompletionStatus completionStatus =
-          MasCompletionStatus.of(origin, sufficient, offRampError);
+          MasCompletionStatus.of(origin, sufficient, offRampErrorPayload);
 
-      if (offRampErrorPayload != null) {
-        exchange.setProperty("completionSlackMessage", offRampErrorPayload);
-      }
       try {
         BipUpdateClaimResult result = bipClaimService.updateClaim(payload, completionStatus);
         if (result.hasMessage()) {
@@ -197,20 +201,6 @@ public class MasIntegrationProcessors {
       String message = exchange.getProperty(exchangeProperty, "Unidentified message", String.class);
       MasProcessingObject masProcessingObject =
           exchange.getMessage().getBody(MasProcessingObject.class);
-      exchange
-          .getIn()
-          .setBody(
-              AuditEvent.fromAuditable(
-                  masProcessingObject, routeId, getSlackMessage(masProcessingObject, message)));
-    };
-  }
-
-  public static Processor slackOffRampProcessor() {
-    return exchange -> {
-      MasProcessingObject masProcessingObject =
-          exchange.getMessage().getBody(MasProcessingObject.class);
-      String routeId = exchange.getProperty("sourceRoute", String.class);
-      String message = exchange.getProperty("offRampError", String.class);
       exchange
           .getIn()
           .setBody(
