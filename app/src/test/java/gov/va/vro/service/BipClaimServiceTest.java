acf0c390c7500cb24a3523891f524c94745301f6
warren <wlin305@gmail.com>
2023-03-30 12:07:48 -0400

    Update BipClaimService.updateClaim test.

--- app/src/test/java/gov/va/vro/service/BipClaimServiceTest.java
+++ app/src/test/java/gov/va/vro/service/BipClaimServiceTest.java
@@ -2,7 +2,10 @@ package gov.va.vro.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyString;
 
 import gov.va.vro.MasTestData;
 import gov.va.vro.model.bip.BipClaim;
@@ -11,11 +14,13 @@ import gov.va.vro.model.bip.ClaimContention;
 import gov.va.vro.model.bip.ClaimStatus;
 import gov.va.vro.model.bip.UpdateContention;
 import gov.va.vro.model.bip.UpdateContentionReq;
+import gov.va.vro.model.mas.ClaimDetail;
 import gov.va.vro.model.mas.MasAutomatedClaimPayload;
 import gov.va.vro.model.mas.response.FetchPdfResponse;
 import gov.va.vro.service.provider.ClaimProps;
 import gov.va.vro.service.provider.bip.BipException;
 import gov.va.vro.service.provider.bip.service.BipClaimService;
+import gov.va.vro.service.provider.bip.service.BipUpdateClaimResult;
 import gov.va.vro.service.provider.bip.service.IBipApiService;
 import gov.va.vro.service.provider.bip.service.IBipCeApiService;
 import gov.va.vro.service.provider.mas.MasCamelStage;
@@ -35,6 +40,7 @@ import java.util.Base64;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
@@ -46,14 +52,17 @@ class BipClaimServiceTest {
 
   private static final long CONTENTION_ID = 1000L;
   private static final long CONTENTION_ID2 = 1001L;
-  private static final List<String> SPECIAL_ISSUES = Arrays.asList("RRD", "RRD1");
-  private static final ClaimStatus CONTENTION_STATUS = ClaimStatus.OPEN;
-  private static final boolean AUTU_IND = false;
+  private static final List<String> SPECIAL_ISSUES_1 = Arrays.asList("RRD", "RRD1");
+  private static final List<String> SPECIAL_ISSUES_2 = new ArrayList<>();
+  private static final ClaimStatus LIFE_CYCLE_STATUS_1 = ClaimStatus.OPEN;
+  private static final ClaimStatus LIFE_CYCLE_STATUS_2 = ClaimStatus.RFD;
+  private static final boolean AUTOMATION_IND_1 = false;
+  private static final boolean AUTOMATION_IND_2 = false;
 
   private ClaimProps claimProps;
 
-  private Map<Long, BipClaim> bipClaims = new ConcurrentHashMap<>();
-  private Map<Long, List<ClaimContention>> bipContentions = new ConcurrentHashMap<>();
+  private final Map<Long, BipClaim> bipClaims = new ConcurrentHashMap<>();
+  private final Map<Long, List<ClaimContention>> bipContentions = new ConcurrentHashMap<>();
 
   class BipApiTestService implements IBipApiService {
 
@@ -81,7 +90,6 @@ class BipClaimServiceTest {
 
     @Override
     public List<ClaimContention> getClaimContentions(long claimId) throws BipException {
-      //      BipClaim claim = getBipClaim(claimId);
       List<ClaimContention> contentions = bipContentions.get(claimId);
       if (contentions == null) {
         return new ArrayList<>();
@@ -107,7 +115,7 @@ class BipClaimServiceTest {
       }
       Map<Long, UpdateContention> updtContentions =
           contention.getUpdateContentions().stream()
-              .collect(Collectors.toMap(c -> c.getContentionId(), c -> c));
+              .collect(Collectors.toMap(UpdateContention::getContentionId, c -> c));
       contentions.forEach(
           c -> {
             UpdateContention updt = updtContentions.get(c.getContentionId());
@@ -129,26 +137,26 @@ class BipClaimServiceTest {
   private void initializeBipClaims() {
     BipClaim claim = new BipClaim();
     claim.setClaimId(CLAIM_ID1);
-    claim.setClaimLifecycleStatus(ClaimStatus.OPEN.getDescription());
+    claim.setClaimLifecycleStatus(LIFE_CYCLE_STATUS_1.getDescription());
     bipClaims.put(Long.parseLong(CLAIM_ID1), claim);
 
     claim = new BipClaim();
     claim.setClaimId(CLAIM_ID2);
-    claim.setClaimLifecycleStatus(ClaimStatus.RFD.getDescription());
+    claim.setClaimLifecycleStatus(LIFE_CYCLE_STATUS_2.getDescription());
     bipClaims.put(Long.parseLong(CLAIM_ID2), claim);
 
     ClaimContention contention = new ClaimContention();
     contention.setContentionId(CONTENTION_ID);
-    contention.setSpecialIssueCodes(SPECIAL_ISSUES);
-    contention.setLifecycleStatus(ClaimStatus.OPEN.getDescription());
-    contention.setAutomationIndicator(false);
+    contention.setSpecialIssueCodes(SPECIAL_ISSUES_1);
+    contention.setLifecycleStatus(LIFE_CYCLE_STATUS_1.getDescription());
+    contention.setAutomationIndicator(AUTOMATION_IND_1);
     bipContentions.put(Long.parseLong(CLAIM_ID1), Collections.singletonList(contention));
 
     contention = new ClaimContention();
     contention.setContentionId(CONTENTION_ID2);
-    contention.setSpecialIssueCodes(SPECIAL_ISSUES);
-    contention.setLifecycleStatus(ClaimStatus.RFD.getDescription());
-    contention.setAutomationIndicator(true);
+    contention.setSpecialIssueCodes(SPECIAL_ISSUES_2);
+    contention.setLifecycleStatus(LIFE_CYCLE_STATUS_2.getDescription());
+    contention.setAutomationIndicator(AUTOMATION_IND_2);
     bipContentions.put(Long.parseLong(CLAIM_ID2), Collections.singletonList(contention));
   }
 
@@ -293,6 +301,77 @@ class BipClaimServiceTest {
     fetchPdfResponse.setPdfData(new String(data));
     var payload = MasTestData.getMasAutomatedClaimPayload();
     claimService.uploadPdf(payload, fetchPdfResponse);
+  }
+
+  @Test
+  void testUpdateClaim() {
+    initializeBipClaims();
+    BipApiTestService bipApiService = new BipApiTestService();
+
+    ClaimProps claimProp = new ClaimProps();
+    claimProp.setSpecialIssue1(SPECIAL_ISSUES_1.get(0));
+    claimProp.setSpecialIssue2(SPECIAL_ISSUES_1.get(1));
+
+    SaveToDbService saveToDbService = Mockito.mock(SaveToDbService.class);
+    Mockito.doNothing().when(saveToDbService).updateRfdFlag(anyString(), anyBoolean());
+
+    BipClaimService claimService =
+        new BipClaimService(claimProp, bipApiService, null, saveToDbService);
+
+    for (long claimId : bipClaims.keySet()) {
+      for (MasCamelStage stage : MasCamelStage.values()) { // TODO: Is stage matters here?
+        MasProcessingObject payload = getMasPayload(claimId, stage);
+        for (MasCompletionStatus status : MasCompletionStatus.values()) {
+          BipUpdateClaimResult result = claimService.updateClaim(payload, status);
+          Set<String> specialIssueCodes = status.getSpecialIssuesToRemove(claimProps);
+          verifyUpdateClaimResult(result, payload, status, stage, specialIssueCodes);
+        }
+      }
+    }
+  }
+
+  private void verifyUpdateClaimResult(
+      BipUpdateClaimResult result,
+      MasProcessingObject payload,
+      MasCompletionStatus status,
+      MasCamelStage stage,
+      Set<String> specialIssueCodes) {
+    assertTrue(result.isSuccess());
+    long claimId = Long.parseLong(payload.getBenefitClaimId());
+    BipClaim claim = bipClaims.get(claimId);
+    ClaimStatus expectedStatus = status.getClaimStatus();
+
+    // Check claim status
+    assertEquals(expectedStatus.getDescription(), claim.getClaimLifecycleStatus());
+
+    // Check contention
+    List<ClaimContention> contentions = bipContentions.get(claimId);
+    // TODO: There might be a problem for handling multiple contentions in BipClaimService.
+    for (ClaimContention contention : contentions) {
+      // Check automation indicator.
+      if (status.isAutomationIndicator()) {
+        assertTrue(contention.isAutomationIndicator());
+      }
+
+      // Check special issue codes
+      if (!specialIssueCodes.isEmpty()) {
+        assertFalse(
+            contention.getSpecialIssueCodes().stream()
+                .anyMatch(c -> specialIssueCodes.contains(c)));
+      }
+
+      // Check contention life cycle status
+      // TODO: Should life cycle status be changed only for the contention with special codes?
+      assertEquals(status.getClaimStatus().getDescription(), contention.getLifecycleStatus());
+    }
+  }
+
+  private static MasProcessingObject getMasPayload(long claimId, MasCamelStage stage) {
+    ClaimDetail claimDetail = new ClaimDetail();
+    claimDetail.setBenefitClaimId(claimId + "");
+    MasAutomatedClaimPayload claimPayload =
+        MasAutomatedClaimPayload.builder().claimDetail(claimDetail).build();
+    return new MasProcessingObject(claimPayload, stage);
   }
 
   private ClaimContention createContention(List<String> codes) {
