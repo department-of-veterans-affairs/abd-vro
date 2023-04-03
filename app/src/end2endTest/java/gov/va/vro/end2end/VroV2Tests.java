41f28fe1851331106ab098781509fdcd9d17e0bd
Afsin Ustundag <afsin@amida.com>
2023-04-02 23:02:06 -0400

    addl e2e test for lh exceptions

--- app/src/end2endTest/java/gov/va/vro/end2end/VroV2Tests.java
+++ app/src/end2endTest/java/gov/va/vro/end2end/VroV2Tests.java
@@ -882,7 +882,7 @@ public class VroV2Tests {
 
   /**
    * This is an end-to-end test for an increase case based on 375. It is used to test lh 500
-   * exceptions.
+   * exceptions. This is on Observation retrieval.
    */
   @Test
   void testAutomatedClaimLh500Exception() {
@@ -902,10 +902,24 @@ public class VroV2Tests {
   void testAutomatedClaimLhTimeoutException() {
     String collectionId = "366";
     AutomatedClaimTestSpec spec = specFor200(collectionId);
-    spec.setExtraSleep(125000); // expected sleep time
+    spec.setExtraSleep(250000); // expected sleep time
 
     // enable the rest of the lines to activate the test.
     // testAutomatedClaimFullPositive(spec);
+    // boolean slackResult = testSlackMessage(collectionId);
+    // assertTrue(slackResult, "No or unexpected slack messages received by slack server");
+  }
+
+  /**
+   * This is an end-to-end test for an increase case based on 375. It is used to test lh 504
+   * exceptions. This is on Condition retrieval.
+   */
+  @Test
+  void testAutomatedClaimLh504Exception() {
+    String collectionId = "367";
+    AutomatedClaimTestSpec spec = specFor200(collectionId);
+    testAutomatedClaimFullPositive(spec);
+    // enable when sack messaging is fixed
     // boolean slackResult = testSlackMessage(collectionId);
     // assertTrue(slackResult, "No or unexpected slack messages received by slack server");
   }
