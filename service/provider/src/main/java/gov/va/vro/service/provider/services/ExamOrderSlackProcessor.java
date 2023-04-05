fb14ea7270ac5caabb8de7686b69f37c24337db1
ianwolson <ian@amida.com>
2023-04-05 15:57:27 -0400

    change slack message

--- service/provider/src/main/java/gov/va/vro/service/provider/services/ExamOrderSlackProcessor.java
+++ service/provider/src/main/java/gov/va/vro/service/provider/services/ExamOrderSlackProcessor.java
@@ -3,6 +3,7 @@ package gov.va.vro.service.provider.services;
 import gov.va.vro.model.claimmetrics.ExamOrdersInfo;
 import gov.va.vro.model.claimmetrics.response.ExamOrderInfoResponse;
 import gov.va.vro.model.event.AuditEvent;
+import gov.va.vro.service.provider.mas.MasException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
@@ -21,17 +22,19 @@ public class ExamOrderSlackProcessor implements Processor {
     StringBuilder msg = new StringBuilder();
     if (exams != null) {
       for (ExamOrderInfoResponse exam : exams.getExamOrderInfoList()) {
-        msg.append("collection ID: ")
+        msg.append("[ExamOrder")
+            .append(" collection ID: ")
             .append(exam.getCollectionId())
-            .append(" ")
-            .append("Created At: ")
+            .append(" createdAt: ")
             .append(exam.getCreatedAt())
-            .append(" ")
-            .append("Status: ")
-            .append(exam.getStatus());
+            .append(" status: ")
+            .append(exam.getStatus())
+            .append("], ");
       }
+    } else {
+      log.error("No exam orders were available to slack.");
+      throw new MasException("No exam orders were available to slack.");
     }
-    log.error("MESSAGE: " + msg);
     return msg.toString();
   }
 }
