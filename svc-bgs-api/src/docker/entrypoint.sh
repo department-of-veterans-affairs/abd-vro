51a12d168fa46157f8c931954d7826ff9c4eb47b
Yoom Lam <yoom@navapbc.com>
2023-04-06 09:09:57 -0500

    reenable svc-bgs-api in Gradle

--- svc-bgs-api/src/docker/entrypoint.sh
+++ svc-bgs-api/src/docker/entrypoint.sh
@@ -1,9 +1,10 @@
+#!/bin/sh
 # This will be called by entrypoint-wrapper.sh,
 # after it sets environment variables for secrets
 
 : ${CERTS_PATH:=$(pwd)/certs}
 mkdir -p "$CERTS_PATH"
-pushd "$CERTS_PATH"
+cd "$CERTS_PATH"
 
 [ "$BIP_KEYSTORE" ]   || { echo "BIP_KEYSTORE is not set!"; exit 2; }
 [ "$BIP_TRUSTSTORE" ] || { echo "BIP_TRUSTSTORE is not set!"; exit 3; }
@@ -17,6 +18,6 @@ openssl pkcs12 ${PWD_ARG} -in keystore.p12   -out tls_bip.crt -nokeys
 openssl pkcs12 ${PWD_ARG} -in keystore.p12   -out tls.key -nocerts -nodes
 openssl pkcs12 ${PWD_ARG} -in truststore.p12 -out va_all.crt
 
-popd
+cd -
 
 exec bundle exec ruby main_consumer.rb
