777d4351e665d9e3a43c8598c3ab72ee5674181e
Afsin Ustundag <afsin@amida.com>
2023-04-06 10:04:24 -0400

    load files in a list.

--- mocks/mock-lighthouse-api/src/main/java/gov/va/vro/mocklh/config/AppConfig.java
+++ mocks/mock-lighthouse-api/src/main/java/gov/va/vro/mocklh/config/AppConfig.java
@@ -3,13 +3,20 @@ package gov.va.vro.mocklh.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gov.va.vro.mocklh.model.MockBundleStore;
 import gov.va.vro.mocklh.model.MockBundles;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
+@Slf4j
 public class AppConfig {
 
   @Bean
@@ -29,56 +36,24 @@ public class AppConfig {
    * @throws IOException Missing files
    */
   @Bean
+  @SneakyThrows
   public MockBundleStore muckBundleStore() throws IOException {
     MockBundleStore store = new MockBundleStore();
+    String rootName = "mock-bundles";
 
-    MockBundles mb1 = MockBundles.of("mock-bundles/mock1012666073V986297");
-    store.put("mock1012666073V986297", mb1);
+    var resource = this.getClass().getClassLoader().getResource(rootName);
+    List<String> directoryNames =
+        Files.walk(Paths.get(resource.toURI()), 1)
+            .filter(Files::isDirectory)
+            .map(p -> p.getFileName().toString())
+            .filter(r -> r.startsWith("mock") && !r.equals(rootName))
+            .collect(Collectors.toList());
 
-    MockBundles mb2 = MockBundles.of("mock-bundles/mock1012666073V986377");
-    store.put("mock1012666073V986377", mb2);
-
-    MockBundles mb3 = MockBundles.of("mock-bundles/mock1012666073V986378");
-    store.put("mock1012666073V986378", mb3);
-
-    MockBundles mb4 = MockBundles.of("mock-bundles/mock1012666073V986380");
-    store.put("mock1012666073V986380", mb4);
-
-    MockBundles mb5 = MockBundles.of("mock-bundles/mock1012666073V986500");
-    store.put("mock1012666073V986500", mb5);
-
-    MockBundles mb6 = MockBundles.of("mock-bundles/mock1012666073V986400");
-    store.put("mock1012666073V986400", mb6);
-
-    MockBundles mb7 = MockBundles.of("mock-bundles/mock1012666073V986401");
-    store.put("mock1012666073V986401", mb7);
-
-    MockBundles mb8 = MockBundles.of("mock-bundles/mock1012666073V986390");
-    store.put("mock1012666073V986390", mb8);
-
-    MockBundles mb9 = MockBundles.of("mock-bundles/mock1012666073V986391");
-    store.put("mock1012666073V986391", mb9);
-
-    MockBundles mb10 = MockBundles.of("mock-bundles/mock1012666073V986392");
-    store.put("mock1012666073V986392", mb10);
-
-    MockBundles mb11 = MockBundles.of("mock-bundles/mock1012666073V986385");
-    store.put("mock1012666073V986385", mb11);
-
-    MockBundles mb12 = MockBundles.of("mock-bundles/mock1012666073V986386");
-    store.put("mock1012666073V986386", mb12);
-
-    MockBundles mb13 = MockBundles.of("mock-bundles/mock1012666073V986369");
-    store.put("mock1012666073V986369", mb13);
-
-    MockBundles mb14 = MockBundles.of("mock-bundles/mock1012666073V986365");
-    store.put("mock1012666073V986365", mb14);
-
-    MockBundles mb15 = MockBundles.of("mock-bundles/mock1012666073V986366");
-    store.put("mock1012666073V986366", mb15);
-
-    MockBundles mb16 = MockBundles.of("mock-bundles/mock1012666073V986367");
-    store.put("mock1012666073V986367", mb16);
+    for (String icn : directoryNames) {
+      log.info("Loading icn: ", icn);
+      MockBundles mb = MockBundles.of("mock-bundles/" + icn);
+      store.put(icn, mb);
+    }
 
     return store;
   }
--- mocks/mock-mas-api/src/main/java/gov/va/vro/mockmas/config/MockMasConfig.java
+++ mocks/mock-mas-api/src/main/java/gov/va/vro/mockmas/config/MockMasConfig.java
@@ -6,66 +6,25 @@ import gov.va.vro.mockmas.model.CollectionStore;
 import gov.va.vro.mockmas.model.ExamOrderStore;
 import gov.va.vro.model.mas.MasCollectionAnnotation;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
+@Slf4j
 public class MockMasConfig {
-
-  @Value("classpath:annotations/collection-375.json")
-  private Resource collection375Resource;
-
-  @Value("classpath:annotations/collection-376.json")
-  private Resource collection376Resource;
-
-  @Value("classpath:annotations/collection-377.json")
-  private Resource collection377Resource;
-
-  @Value("classpath:annotations/collection-378.json")
-  private Resource collection378Resource;
-
-  @Value("classpath:annotations/collection-380.json")
-  private Resource collection380Resource;
-
-  @Value("classpath:annotations/collection-400.json")
-  private Resource collection400Resource;
-
-  @Value("classpath:annotations/collection-401.json")
-  private Resource collection401Resource;
-
-  @Value("classpath:annotations/collection-500.json")
-  private Resource collection500Resource;
-
-  @Value("classpath:annotations/collection-390.json")
-  private Resource collection390Resource;
-
-  @Value("classpath:annotations/collection-391.json")
-  private Resource collection391Resource;
-
-  @Value("classpath:annotations/collection-392.json")
-  private Resource collection392Resource;
-
-  @Value("classpath:annotations/collection-385.json")
-  private Resource collection385Resource;
-
-  @Value("classpath:annotations/collection-386.json")
-  private Resource collection386Resource;
-
-  @Value("classpath:annotations/collection-365.json")
-  private Resource collection365Resource;
-
-  @Value("classpath:annotations/collection-366.json")
-  private Resource collection366Resource;
-
-  @Value("classpath:annotations/collection-367.json")
-  private Resource collection367Resource;
-
   /** Creates and provides the common instance of RestTemplate as a bean for the application. */
   @Bean
   public RestTemplate restTemplate() {
@@ -78,8 +37,8 @@ public class MockMasConfig {
   }
 
   @SneakyThrows
-  private List<MasCollectionAnnotation> readFromResource(Resource resource) {
-    InputStream stream = resource.getInputStream();
+  private List<MasCollectionAnnotation> readFromResource(String path) {
+    InputStream stream = this.getClass().getClassLoader().getResourceAsStream(path);
     ObjectMapper mapper = objectMapper();
     return mapper.readValue(stream, new TypeReference<>() {});
   }
@@ -90,57 +49,28 @@ public class MockMasConfig {
    * @return CollectionStore
    */
   @Bean
+  @SneakyThrows
   public CollectionStore collectionStore() {
     CollectionStore store = new CollectionStore();
 
-    List<MasCollectionAnnotation> collection375 = readFromResource(collection375Resource);
-    store.put(375, collection375);
+    String rootName = "annotations";
+    var resource = this.getClass().getClassLoader().getResource(rootName);
+    List<String> filenames =
+        Files.walk(Paths.get(resource.toURI()), 1)
+            .filter(Files::isRegularFile)
+            .map(p -> p.getFileName().toString())
+            .filter(r -> r.startsWith("collection") && r.endsWith(".json"))
+            .map(r -> rootName + "/" + r)
+            .collect(Collectors.toList());
 
-    List<MasCollectionAnnotation> collection376 = readFromResource(collection376Resource);
-    store.put(376, collection376);
-
-    List<MasCollectionAnnotation> collection377 = readFromResource(collection377Resource);
-    store.put(377, collection377);
-
-    List<MasCollectionAnnotation> collection378 = readFromResource(collection378Resource);
-    store.put(378, collection378);
-
-    List<MasCollectionAnnotation> collection380 = readFromResource(collection380Resource);
-    store.put(380, collection380);
-
-    List<MasCollectionAnnotation> collection400 = readFromResource(collection400Resource);
-    store.put(400, collection400);
-
-    List<MasCollectionAnnotation> collection401 = readFromResource(collection401Resource);
-    store.put(401, collection401);
-
-    List<MasCollectionAnnotation> collection500 = readFromResource(collection500Resource);
-    store.put(500, collection500);
-
-    List<MasCollectionAnnotation> collection390 = readFromResource(collection390Resource);
-    store.put(390, collection390);
-
-    List<MasCollectionAnnotation> collection391 = readFromResource(collection391Resource);
-    store.put(391, collection391);
-
-    List<MasCollectionAnnotation> collection392 = readFromResource(collection392Resource);
-    store.put(392, collection392);
-
-    List<MasCollectionAnnotation> collection385 = readFromResource(collection385Resource);
-    store.put(385, collection385);
-
-    List<MasCollectionAnnotation> collection386 = readFromResource(collection386Resource);
-    store.put(386, collection386);
-
-    List<MasCollectionAnnotation> collection365 = readFromResource(collection365Resource);
-    store.put(365, collection365);
-
-    List<MasCollectionAnnotation> collection366 = readFromResource(collection366Resource);
-    store.put(366, collection366);
-
-    List<MasCollectionAnnotation> collection367 = readFromResource(collection367Resource);
-    store.put(367, collection367);
-
+    for (String filename: filenames) {
+      log.info("Loading file: ", filename);
+      String filepath = rootName + "/" + filename;
+      String idAsString = filename.substring(10, 13);
+      Integer id = Integer.valueOf(idAsString);
+      List<MasCollectionAnnotation> collection = readFromResource(filepath);
+      store.put(id, collection);
+    }
     return store;
   }
 
