dca95162369f99e09189771261b0189453a67599
Afsin Ustundag <afsin@amida.com>
2023-04-09 16:37:03 -0400

    use gradle to create an info object

--- mocks/mock-lighthouse-api/src/main/java/gov/va/vro/mocklh/config/AppConfig.java
+++ mocks/mock-lighthouse-api/src/main/java/gov/va/vro/mocklh/config/AppConfig.java
@@ -1,21 +1,30 @@
 package gov.va.vro.mocklh.config;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gov.va.vro.mocklh.model.MockBundleStore;
 import gov.va.vro.mocklh.model.MockBundles;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
 
 @Configuration
 @Slf4j
 public class AppConfig {
+
+  @Value("classpath:mock-info-file.json")
+  private Resource resource;
 
   @Bean
   public RestTemplate restTemplate() {
@@ -37,14 +46,14 @@ public class AppConfig {
   @SneakyThrows
   public MockBundleStore muckBundleStore() throws IOException {
     MockBundleStore store = new MockBundleStore();
-    String rootName = "mock-bundles";
 
-    PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-
-    Resource[] resources = resolver.getResources("mock-bundles/mock*");
-    for (Resource resource : resources) {
-      String icn = resource.getFilename();
-      log.info("Loading icn: ", icn);
+    Reader reader = new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8);
+    String listAsJson = FileCopyUtils.copyToString(reader);
+    ObjectMapper mapper = objectMapper();
+    JsonNode array = mapper.readTree(resource.getInputStream());
+    for (Iterator<JsonNode> it = array.elements(); it.hasNext(); ) {
+      String icn = it.next().asText();
+      log.info("Loading icn: {}", icn);
       MockBundles mb = MockBundles.of("mock-bundles/" + icn);
       store.put(icn, mb);
     }
--- mocks/mock-mas-api/src/main/java/gov/va/vro/mockmas/config/MockMasConfig.java
+++ mocks/mock-mas-api/src/main/java/gov/va/vro/mockmas/config/MockMasConfig.java
@@ -1,24 +1,34 @@
 package gov.va.vro.mockmas.config;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gov.va.vro.mockmas.model.CollectionStore;
 import gov.va.vro.mockmas.model.ExamOrderStore;
 import gov.va.vro.model.mas.MasCollectionAnnotation;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
 import java.util.List;
 
 @Configuration
 @Slf4j
 public class MockMasConfig {
+
+  @Value("classpath:mock-info-file.json")
+  private Resource resource;
+
   /** Creates and provides the common instance of RestTemplate as a bean for the application. */
   @Bean
   public RestTemplate restTemplate() {
@@ -31,8 +41,8 @@ public class MockMasConfig {
   }
 
   @SneakyThrows
-  private List<MasCollectionAnnotation> readFromResource(Resource resource) {
-    InputStream stream = resource.getInputStream();
+  private List<MasCollectionAnnotation> readFromResource(String path) {
+    InputStream stream = this.getClass().getClassLoader().getResourceAsStream(path);
     ObjectMapper mapper = objectMapper();
     return mapper.readValue(stream, new TypeReference<>() {});
   }
@@ -47,15 +57,17 @@ public class MockMasConfig {
   public CollectionStore collectionStore() {
     CollectionStore store = new CollectionStore();
 
-    PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-
-    Resource[] resources = resolver.getResources("annotations/collection-*.json");
+    Reader reader = new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8);
+    String listAsJson = FileCopyUtils.copyToString(reader);
+    ObjectMapper mapper = objectMapper();
+    JsonNode array = mapper.readTree(resource.getInputStream());
     int start = "collection-".length();
-    for (Resource resource : resources) {
-      String filename = resource.getFilename();
+    for (Iterator<JsonNode> it = array.elements(); it.hasNext(); ) {
+      String filename = it.next().asText();
       String idAsString = filename.substring(start, start + 3);
       Integer id = Integer.valueOf(idAsString);
-      List<MasCollectionAnnotation> collection = readFromResource(resource);
+      log.info("Creating mock collection {}", idAsString);
+      List<MasCollectionAnnotation> collection = readFromResource("annotations/" + filename);
       store.put(id, collection);
     }
     return store;
