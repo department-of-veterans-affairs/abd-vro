5289dc8e24b15b4f969ed46bf3549aca3a011edc
Yoom Lam <yoom@navapbc.com>
2023-04-21 08:47:55 -0500

    revert private ARSD_UPLOADED_NOTE

--- domain-rrd/rrd-workflows/src/main/java/gov/va/vro/service/provider/bgs/service/BgsVeteranNote.java
+++ domain-rrd/rrd-workflows/src/main/java/gov/va/vro/service/provider/bgs/service/BgsVeteranNote.java
@@ -7,7 +7,7 @@ class BgsVeteranNote {
 
   private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
   // The date should be in MM/DD/YYYY format
-  static final String ARSD_UPLOADED_NOTE =
+  private static final String ARSD_UPLOADED_NOTE =
       "Hypertension automated review summary document uploaded %s";
 
   static String getArsdUploadedNote(OffsetDateTime docUploadedAt) {
