c659dee6c0542b00f98377bccae5be651b43f067
Yoom Lam <yoom@navapbc.com>
2023-04-21 16:03:51 -0500

    add MasProcessingObjectTestData

--- domain-rrd/rrd-workflows/src/test/java/gov/va/vro/service/provider/bgs/service/BgsApiClientTest.java
+++ domain-rrd/rrd-workflows/src/test/java/gov/va/vro/service/provider/bgs/service/BgsApiClientTest.java
@@ -2,23 +2,22 @@ package gov.va.vro.service.provider.bgs.service;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import gov.va.vro.model.rrd.bgs.BgsApiClientRequest;
 import gov.va.vro.model.rrd.event.EventReason;
-import gov.va.vro.model.rrd.mas.ClaimDetail;
-import gov.va.vro.model.rrd.mas.MasAutomatedClaimPayload;
-import gov.va.vro.model.rrd.mas.VeteranIdentifiers;
 import gov.va.vro.persistence.model.EvidenceSummaryDocumentEntity;
 import gov.va.vro.persistence.repository.EvidenceSummaryDocumentRepository;
+import gov.va.vro.service.provider.MasProcessingObjectTestData;
 import gov.va.vro.service.provider.mas.MasCamelStage;
 import gov.va.vro.service.provider.mas.MasProcessingObject;
 import org.jetbrains.annotations.NotNull;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.OffsetDateTime;
@@ -31,58 +30,38 @@ import java.util.stream.Collectors;
 @ExtendWith(MockitoExtension.class)
 public class BgsApiClientTest {
 
-  @Mock EvidenceSummaryDocumentRepository esDocRepository;
-  BgsApiClient client;
+  static EvidenceSummaryDocumentRepository esDocRepository;
+  static BgsApiClient client;
 
-  @BeforeEach
-  void setup() {
+  @BeforeAll
+  static void setupStatics() {
+    esDocRepository = Mockito.mock(EvidenceSummaryDocumentRepository.class);
+    // The client is stateless, so a single instance can handle multiple tests
     client = new BgsApiClient(esDocRepository);
   }
 
-  static final String claimId = "0613";
-  static final String participantId = "vetPid20230421";
-
   static final OffsetDateTime uploadedAt = OffsetDateTime.now();
 
-  void mockRepoFindById() {
+  private void mockEsdRepoFindById() {
     EvidenceSummaryDocumentEntity entity = new EvidenceSummaryDocumentEntity();
     entity.setUploadedAt(uploadedAt);
-    Optional<EvidenceSummaryDocumentEntity> foundDoc = Optional.of(entity);
-    when(esDocRepository.findById(any())).thenReturn(foundDoc);
+    when(esDocRepository.findById(any())).thenReturn(Optional.of(entity));
   }
 
-  // TODO: move this method to a factory class for tests to use
-  static MasProcessingObject createMasPayload(MasCamelStage stage) {
-    ClaimDetail claimDetail = new ClaimDetail();
-    claimDetail.setBenefitClaimId(claimId);
-    VeteranIdentifiers veteranIdentifiers = createVeteranIdentifiers();
-    MasAutomatedClaimPayload claimPayload =
-        MasAutomatedClaimPayload.builder()
-            .claimDetail(claimDetail)
-            .veteranIdentifiers(veteranIdentifiers)
-            .build();
-    return new MasProcessingObject(claimPayload, stage);
-  }
-
-  // TODO: move this method to a factory class for tests to use
-  static VeteranIdentifiers createVeteranIdentifiers() {
-    VeteranIdentifiers veteranIdentifiers = new VeteranIdentifiers();
-    veteranIdentifiers.setEdipn("X");
-    veteranIdentifiers.setParticipantId(participantId);
-    veteranIdentifiers.setIcn("X");
-    veteranIdentifiers.setSsn("X");
-    veteranIdentifiers.setVeteranFileId("X");
-    return veteranIdentifiers;
+  private MasProcessingObject mpoWithMasCamelStage(MasCamelStage stage) {
+    return MasProcessingObjectTestData.builder().masCamelStage(stage).build().create();
   }
 
   @Test
   void buildRequestTest_ReadyForDecision() {
-    mockRepoFindById();
-    var mpo = createMasPayload(MasCamelStage.DURING_PROCESSING);
+    mockEsdRepoFindById();
+    var mpo = mpoWithMasCamelStage(MasCamelStage.DURING_PROCESSING);
     mpo.setSufficientForFastTracking(true);
     var requests = client.buildRequest(mpo).getPendingRequests();
 
+    // Should be 2 separate requests -- one for veteran note, another for claim notes
     assertEquals(2, requests.size());
+    requests.forEach(request -> assertTrue(request.isConstraintSatisfied()));
 
     var claimNotes = extractClaimNotesFrom(requests);
     assertThat(claimNotes)
@@ -97,12 +76,14 @@ public class BgsApiClientTest {
 
   @Test
   void buildRequestTest_ExamOrder() {
-    mockRepoFindById();
-    var mpo = createMasPayload(MasCamelStage.DURING_PROCESSING);
+    mockEsdRepoFindById();
+    var mpo = mpoWithMasCamelStage(MasCamelStage.DURING_PROCESSING);
     mpo.setSufficientForFastTracking(false);
     var requests = client.buildRequest(mpo).getPendingRequests();
 
+    // Should be 2 separate requests -- one for veteran note, another for claim notes
     assertEquals(2, requests.size());
+    requests.forEach(request -> assertTrue(request.isConstraintSatisfied()));
 
     var claimNotes = extractClaimNotesFrom(requests);
     assertThat(claimNotes).hasSize(1).containsExactlyInAnyOrder(BgsClaimNotes.EXAM_REQUESTED_NOTE);
@@ -155,15 +136,15 @@ public class BgsApiClientTest {
     assertEquals(expectedClaimNote, claimNotes.get(0));
   }
 
-  static MasProcessingObject mpoWithOfframpReason(EventReason reason) {
-    var mpo = createMasPayload(masCamelStageForOfframp());
+  MasProcessingObject mpoWithOfframpReason(EventReason reason) {
+    var mpo = mpoWithMasCamelStage(masCamelStageForOfframp());
     mpo.getClaimPayload().setOffRampReason(reason.getCode());
     return mpo;
   }
 
   private static final Random random = new Random();
 
-  static MasCamelStage masCamelStageForOfframp() {
+  MasCamelStage masCamelStageForOfframp() {
     // stage doesn't matter as long as there is an setOffRampReason
     MasCamelStage[] stages = {MasCamelStage.START_COMPLETE, MasCamelStage.DURING_PROCESSING};
     return stages[random.nextInt(stages.length)];
