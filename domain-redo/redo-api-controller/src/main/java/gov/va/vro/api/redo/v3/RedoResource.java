89b17b0d64b18c6a58b34a2df096b53b8377014a
verdanceluke <luke@verdance.co>
2023-04-24 18:00:51 -0700

    slim down redo significantly

--- domain-redo/redo-api-controller/src/main/java/gov/va/vro/api/redo/v3/RedoResource.java
+++ domain-redo/redo-api-controller/src/main/java/gov/va/vro/api/redo/v3/RedoResource.java
@@ -2,12 +2,6 @@ package gov.va.vro.api.redo.v3;
 
 import gov.va.vro.api.redo.ResourceException;
 import io.micrometer.core.annotation.Timed;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -22,93 +16,10 @@ import javax.validation.Valid;
 @Timed("redo") // See https://www.baeldung.com/micrometer
 @Tag(name = "Redo Domain")
 public interface RedoResource {
-  @PostMapping("/redo-resource")
-  @Timed(value = "resource.post")
-  @Operation(
-      summary = "Create Redo Resource",
-      description =
-          "Generates a Redo Resource for a specific payload."
-              + "The created resource will be available using the GET endpoint.")
-  @ApiResponses(
-      value = {
-        @ApiResponse(responseCode = "200", description = "Successful Request"),
-        @ApiResponse(
-            responseCode = "400",
-            description = "Redo Resource: Bad Request",
-            content = @Content(schema = @Schema(hidden = true))),
-        @ApiResponse(
-            responseCode = "401",
-            description = "Redo Resource: Unauthorized",
-            content = @Content(schema = @Schema(hidden = true))),
-        @ApiResponse(
-            responseCode = "500",
-            description = "Redo Resource: Server Error",
-            content = @Content(schema = @Schema(hidden = true)))
-      })
-  ResponseEntity<ResourceResponse> postResource(
-      @Parameter(
-              description = "payload for resource",
-              required = true,
-              schema = @Schema(implementation = ResourceRequest.class))
-          @Valid
-          @RequestBody
-          ResourceRequest request)
-      throws MethodArgumentNotValidException, ResourceException;
-
-  @PostMapping("/domain-redo/redo-post-nothing")
-  @Timed(value = "resource.post")
-  @Operation(summary = "Create Redo Resource", description = "Generates nothing")
-  @ApiResponses(
-      value = {
-        @ApiResponse(responseCode = "200", description = "Successful Request"),
-        @ApiResponse(
-            responseCode = "400",
-            description = "Redo Resource: Bad Request",
-            content = @Content(schema = @Schema(hidden = true))),
-        @ApiResponse(
-            responseCode = "401",
-            description = "Redo Resource: Unauthorized",
-            content = @Content(schema = @Schema(hidden = true))),
-        @ApiResponse(
-            responseCode = "500",
-            description = "Redo Resource: Server Error",
-            content = @Content(schema = @Schema(hidden = true)))
-      })
-  ResponseEntity<ResourceResponse> postNothing(
-      @Parameter(
-              description = "payload for resource",
-              required = true,
-              schema = @Schema(implementation = ResourceRequest.class))
-          @Valid
-          @RequestBody
-          ResourceRequest request)
-      throws MethodArgumentNotValidException, ResourceException;
-
   @PostMapping("/domain-redo/cmd/{endpoint}")
   @Timed(value = "resource.post")
-  @Operation(summary = "Call Redo Endpoint", description = "nothing really")
-  @ApiResponses(
-          value = {
-                  @ApiResponse(responseCode = "200", description = "Successful Request"),
-                  @ApiResponse(
-                          responseCode = "400",
-                          description = "Redo Resource: Bad Request",
-                          content = @Content(schema = @Schema(hidden = true))),
-                  @ApiResponse(
-                          responseCode = "401",
-                          description = "Redo Resource: Unauthorized",
-                          content = @Content(schema = @Schema(hidden = true))),
-                  @ApiResponse(
-                          responseCode = "500",
-                          description = "Redo Resource: Server Error",
-                          content = @Content(schema = @Schema(hidden = true)))
-          })
   ResponseEntity<ResourceResponse> callEndpoint(
           @PathVariable(value = "endpoint") String endpoint,
-          @Parameter(
-                  description = "payload for cmd",
-                  required = true,
-                  schema = @Schema(implementation = ResourceRequest.class))
           @Valid
           @RequestBody
           ResourceRequest request)
