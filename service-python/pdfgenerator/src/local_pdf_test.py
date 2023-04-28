fc508905e07ed39e1745f7877948c617b186a546
Anthony Velazquez <anthonyhvelazquez@gmail.com>
2023-04-28 16:50:20 -0400

    merge cells

--- service-python/pdfgenerator/src/local_pdf_test.py
+++ service-python/pdfgenerator/src/local_pdf_test.py
@@ -2,8 +2,32 @@ from lib.pdf_generator import PDFGenerator
 from lib.settings import pdf_options
 
 if __name__ == '__main__':
-    diagnosis_name = "hypertension-v2"
-    message = {}
+    diagnosis_name = "hypertension-v2-cell-merge"
+    message = {
+        "evidence": {
+            "bp_readings": [],
+            "conditions": [],
+            "medications": [
+                {"receiptDate": "1/2/11", "description": "med1", "dateFormatted": "1/1/11", "organization": "org1", "page": "1", "document": "doc1"},
+                {"receiptDate": "2/1/11", "description": "med1", "dateFormatted": "1/1/11", "organization": "org1", "page": "1", "document": "doc1"},
+                {"receiptDate": "2/1/11", "description": "med1", "dateFormatted": "1/1/11", "organization": "org1", "page": "1", "document": "doc1"},
+                {"receiptDate": "2/1/11", "description": "med1", "dateFormatted": "1/1/11", "organization": "org1", "page": "1", "document": "doc1"},
+                {"receiptDate": "2/1/11", "description": "med1", "dateFormatted": "1/1/11", "organization": "org1", "page": "1", "document": "doc1"},
+                {"receiptDate": "3/1/11", "description": "med1", "dateFormatted": "1/1/11", "organization": "org1", "page": "1", "document": "doc1"},
+                {"receiptDate": "4/1/11", "description": "med1", "dateFormatted": "1/1/11", "organization": "org1", "page": "1", "document": "doc1"},
+                {"receiptDate": "5/1/11", "description": "med1", "dateFormatted": "1/1/11", "organization": "org1", "page": "1", "document": "doc1"},
+                {"receiptDate": "5/1/11", "description": "med1", "dateFormatted": "1/1/11", "organization": "org1", "page": "1", "document": "doc1"},
+                {"receiptDate": "5/1/11", "description": "med1", "dateFormatted": "1/1/11", "organization": "org1", "page": "1", "document": "doc1"},
+                {"receiptDate": "6/1/11", "description": "med1", "dateFormatted": "1/1/11", "organization": "org1", "page": "1", "document": "doc1"},
+                {"receiptDate": "7/1/11", "description": "med1", "dateFormatted": "1/1/11", "organization": "org1", "page": "1", "document": "doc1"},
+                {"receiptDate": "7/1/11", "description": "med1", "dateFormatted": "1/1/11", "organization": "org1", "page": "1", "document": "doc1"},
+
+            ],
+            "procedures": [],
+            "serviceLocations": [],
+            "documentsWithoutAnnotationsChecked": []
+        }
+    }
     pdf_generator = PDFGenerator(pdf_options)
     variables = pdf_generator.generate_template_variables(diagnosis_name, message)
     # print("Variables: ", variables)
