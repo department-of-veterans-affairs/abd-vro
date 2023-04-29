1e535b1ca524b9313219939fc47f0fd77198cf8d
verdanceluke <luke@verdance.co>
2023-04-29 08:28:41 -0700

    integrate Yang's hoppy class, call fastapi endpoint on_message

--- domain-cc/python_src/api.py
+++ domain-cc/python_src/api.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World2"}
+
+@app.post("/get_classification")
+def get_classification():
+    return {"classification_code": 6602, "classification_name": "asthma"}
