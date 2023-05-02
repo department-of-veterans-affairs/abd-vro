4bda4df2424633cffb7f06761bb54b504055fd30
verdanceluke <luke@verdance.co>
2023-05-02 16:24:22 -0700

    formatting

--- domain-cc/svc-cc-j/api.py
+++ domain-cc/svc-cc-j/api.py
@@ -5,11 +5,10 @@ app = FastAPI()
 from pydantic_models import ClaimForIncrease, PredictedClassification
 from typing import Optional
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World2"}
-
 
 @app.post("/get_classification")
-def get_classification(claim_for_increase: ClaimForIncrease) -> Optional[PredictedClassification]:
+def get_classification(
+    claim_for_increase: ClaimForIncrease,
+) -> Optional[PredictedClassification]:
+    print(f'received diagnostic code: {claim_for_increase["diagnostic_code"]}')
     return {"classification_code": 6602, "classification_name": "asthma"}
