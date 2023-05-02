934bca40d90376bbb9fcc6d53b6164f9e4df9aad
Hayley Dawson <hayley.dawson@amida.com>
2023-05-02 12:48:03 -0400

    spotless

--- domain-rrd/rrd-workflows/src/test/java/gov/va/vro/service/provider/camel/processor/AssessmentResultProcessorTest.java
+++ domain-rrd/rrd-workflows/src/test/java/gov/va/vro/service/provider/camel/processor/AssessmentResultProcessorTest.java
@@ -1,5 +1,7 @@
 package gov.va.vro.service.provider.camel.processor;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.read.ListAppender;
@@ -15,73 +17,75 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.LoggerFactory;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 import java.util.UUID;
 
 @ExtendWith(MockitoExtension.class)
 public class AssessmentResultProcessorTest {
 
-    private SaveToDbService saveToDbService;
-    private AssessmentResultProcessor processor;
+  private SaveToDbService saveToDbService;
+  private AssessmentResultProcessor processor;
 
-    private final CamelContext ctx = new DefaultCamelContext();
-    private Exchange testExchange;
+  private final CamelContext ctx = new DefaultCamelContext();
+  private Exchange testExchange;
 
-    private final static String diagnosticCode = "7101";
-    private final static UUID claimId = UUID.randomUUID();
+  private static final String diagnosticCode = "7101";
+  private static final UUID claimId = UUID.randomUUID();
 
-    private final static AbdEvidenceWithSummary evidence = new AbdEvidenceWithSummary();
-    private ListAppender<ILoggingEvent> listAppender;
+  private static final AbdEvidenceWithSummary evidence = new AbdEvidenceWithSummary();
+  private ListAppender<ILoggingEvent> listAppender;
 
-    @BeforeEach
-    void setup() {
-        saveToDbService = Mockito.mock(SaveToDbService.class);
-        processor = new AssessmentResultProcessor(saveToDbService);
-        testExchange = new DefaultExchange(ctx);
-        final var log = (Logger) LoggerFactory.getLogger(AssessmentResultProcessor.class);
-        listAppender = new ListAppender<>();
-        listAppender.start();
-        log.addAppender(listAppender);
-    }
+  @BeforeEach
+  void setup() {
+    saveToDbService = Mockito.mock(SaveToDbService.class);
+    processor = new AssessmentResultProcessor(saveToDbService);
+    testExchange = new DefaultExchange(ctx);
+    final var log = (Logger) LoggerFactory.getLogger(AssessmentResultProcessor.class);
+    listAppender = new ListAppender<>();
+    listAppender.start();
+    log.addAppender(listAppender);
+  }
 
-    @Test
-    void testMissingClaimId(){
-        testExchange.setProperty("diagnosticCode", diagnosticCode);
-        testExchange.getIn().setBody(evidence);
-        processor.process(testExchange);
-        Mockito.verify(saveToDbService, Mockito.never()).insertAssessmentResult(Mockito.any(), Mockito.any(), Mockito.any());
-        String message = listAppender.list.get(0).getFormattedMessage();
-        assertThat(message).contains("Claim Id was empty, exiting");
-    }
+  @Test
+  void testMissingClaimId() {
+    testExchange.setProperty("diagnosticCode", diagnosticCode);
+    testExchange.getIn().setBody(evidence);
+    processor.process(testExchange);
+    Mockito.verify(saveToDbService, Mockito.never())
+        .insertAssessmentResult(Mockito.any(), Mockito.any(), Mockito.any());
+    String message = listAppender.list.get(0).getFormattedMessage();
+    assertThat(message).contains("Claim Id was empty, exiting");
+  }
 
-    @Test
-    void testMissingDiagnosticCode(){
-        testExchange.setProperty("claim-id", UUID.randomUUID());
-        testExchange.getIn().setBody(evidence);
-        processor.process(testExchange);
-        Mockito.verify(saveToDbService, Mockito.never()).insertAssessmentResult(Mockito.any(), Mockito.any(), Mockito.any());
-        String message = listAppender.list.get(0).getFormattedMessage();
-        assertThat(message).contains("Diagnostic Code was empty, exiting.");
-    }
+  @Test
+  void testMissingDiagnosticCode() {
+    testExchange.setProperty("claim-id", UUID.randomUUID());
+    testExchange.getIn().setBody(evidence);
+    processor.process(testExchange);
+    Mockito.verify(saveToDbService, Mockito.never())
+        .insertAssessmentResult(Mockito.any(), Mockito.any(), Mockito.any());
+    String message = listAppender.list.get(0).getFormattedMessage();
+    assertThat(message).contains("Diagnostic Code was empty, exiting.");
+  }
 
-    @Test
-    void testEmptyEvidence(){
-        testExchange.setProperty("claim-id", claimId);
-        testExchange.setProperty("diagnosticCode", diagnosticCode);
-        processor.process(testExchange);
-        Mockito.verify(saveToDbService, Mockito.never()).insertAssessmentResult(Mockito.any(), Mockito.any(), Mockito.any());
-        String message = listAppender.list.get(0).getFormattedMessage();
-        assertThat(message).contains("Evidence was empty, exiting");
-    }
+  @Test
+  void testEmptyEvidence() {
+    testExchange.setProperty("claim-id", claimId);
+    testExchange.setProperty("diagnosticCode", diagnosticCode);
+    processor.process(testExchange);
+    Mockito.verify(saveToDbService, Mockito.never())
+        .insertAssessmentResult(Mockito.any(), Mockito.any(), Mockito.any());
+    String message = listAppender.list.get(0).getFormattedMessage();
+    assertThat(message).contains("Evidence was empty, exiting");
+  }
 
-    @Test
-    void testSuccessfulSave(){
-        testExchange.setProperty("claim-id", claimId);
-        testExchange.setProperty("diagnosticCode", diagnosticCode);
-        testExchange.getIn().setBody(evidence);
-        processor.process(testExchange);
-        Mockito.verify(saveToDbService, Mockito.times(1)).insertAssessmentResult(claimId, evidence, diagnosticCode);
-    }
-
-
+  @Test
+  void testSuccessfulSave() {
+    testExchange.setProperty("claim-id", claimId);
+    testExchange.setProperty("diagnosticCode", diagnosticCode);
+    testExchange.getIn().setBody(evidence);
+    processor.process(testExchange);
+    Mockito.verify(saveToDbService, Mockito.times(1))
+        .insertAssessmentResult(claimId, evidence, diagnosticCode);
+  }
 }
