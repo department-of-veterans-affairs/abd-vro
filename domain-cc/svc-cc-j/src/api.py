00e0a4225d91faae2821972a5366bdddc35354b1
verdanceluke <luke@verdance.co>
2023-05-03 13:29:17 -0700

    check in the rest of the WIP python files for getting CC docs

--- domain-cc/svc-cc-j/src/api.py
+++ domain-cc/svc-cc-j/src/api.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World2"}
+from pydantic_models import ClaimForIncrease, PredictedClassification
+from typing import Optional
 
 
 @app.post("/get_classification")
-def get_classification():
+def get_classification(
+        claim_for_increase: ClaimForIncrease,
+) -> Optional[PredictedClassification]:
+    print(f'received diagnostic code: {claim_for_increase["diagnostic_code"]}')
     return {"classification_code": 6602, "classification_name": "asthma"}
-
