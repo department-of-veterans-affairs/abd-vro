229f8c17a914b925ee3acd999ac343a812eae65f
verdanceluke <luke@verdance.co>
2023-05-08 09:38:00 -0700

    update VRO docs w/ description + link for CC docs

--- domain-cc/cc-api-controller/src/main/java/gov/va/vro/api/cc/v3/CCResource.java
+++ domain-cc/cc-api-controller/src/main/java/gov/va/vro/api/cc/v3/CCResource.java
@@ -3,14 +3,11 @@ package gov.va.vro.api.cc.v3;
 import com.fasterxml.jackson.databind.JsonNode;
 import gov.va.vro.api.cc.ResourceException;
 import io.micrometer.core.annotation.Timed;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -19,6 +16,10 @@ import javax.validation.Valid;
 @Tag(name = "CC Domain")
 public interface CCResource {
   @PostMapping("/contention-classification/{endpoint}")
+  @Operation(
+      summary = "Invoke contention classification python endpoint",
+      description =
+              "See more detailed documentation here: https://github.com/department-of-veterans-affairs/abd-vro/blob/develop/domain-cc/README.md#building-docs")
   @Timed(value = "resource.post")
   @ResponseBody
   ResponseEntity<ResourceResponse> callEndpoint(
