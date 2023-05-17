a9157fa4e24f4c11f656f066f4bb3625173fd379
Yoom Lam <yoom@navapbc.com>
2023-05-17 10:21:18 -0500

    clean up

--- api-gateway/src/main/java/gov/va/vro/GatewayApplication.java
+++ api-gateway/src/main/java/gov/va/vro/GatewayApplication.java
@@ -7,17 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
-//@SpringBootConfiguration
-//@ConfigurationPropertiesScan(basePackages = {"gov.va.vro.config"})
-//@EnableAutoConfiguration(exclude = {OpenApiConfiguration.class})
 @SpringBootApplication
-//@ComponentScan(
-//    basePackages = {"gov.va.vro", "gov.va.starter.boot"},
-//    excludeFilters = {
-//      @ComponentScan.Filter(
-//          type = FilterType.ASSIGNABLE_TYPE,
-//          value = {OpenApiConfiguration.class, WebSecurityConfigurerAdapter.class})
-//    })
 @OpenAPIDefinition(info = @Info(title = "VRO Gateway API", version = "1.0", description = "API for the Gateway"))
 public class GatewayApplication {
   public static void main(String[] args) {
