274a37ca867a0f400b5892e17cb421e11c1e2ddf
greene858 <30325260+greene858@users.noreply.github.com>
2023-05-26 16:09:41 -0700

    Release version: 3.0.0

--- api-gateway/src/main/java/gov/va/vro/propmodel/Info.java
+++ api-gateway/src/main/java/gov/va/vro/propmodel/Info.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 public class Info {
   private String title = "API";
   private String description = "Description";
-  private String version = "v0.0.0";
+  private String version = "v3.0.0";
 
   private final Contact contact = new Contact();
 
