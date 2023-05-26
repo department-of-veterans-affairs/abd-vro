02bc43a3b5ea1d0f02f81f2c91ce90270e0b0c55
verdanceluke <luke@verdance.co>
2023-05-25 17:41:23 -0700

    hook API endpoint into a lookup table

--- domain-cc/svc-cc-app/src/api.py
+++ domain-cc/svc-cc-app/src/api.py
@@ -3,6 +3,9 @@ from typing import Optional
 
 from fastapi import FastAPI
 from pydantic_models import ClaimForIncrease, PredictedClassification
+from util.lookup_table import get_lookup_table, get_classification_name
+
+LOOKUP_TABLE = get_lookup_table()
 
 app = FastAPI(
     servers=[
@@ -20,14 +23,24 @@ logging.basicConfig(
 )
 
 
+def diagnostic_code_lookup(diagnostic_code) -> Optional[int]:
+    return LOOKUP_TABLE.get(diagnostic_code, None)
+
+
 @app.post("/classifier")
 def get_classification(
     claim_for_increase: ClaimForIncrease,
 ) -> Optional[PredictedClassification]:
-    classification = {
-        "classification_code": 6602,
-        "classification_name": "asthma",
-    }  # replace this line w/ lookup table call
+    classification_code = LOOKUP_TABLE.get(claim_for_increase.diagnostic_code, None)
+    if classification_code:
+        classification_name = get_classification_name(classification_code)
+        classification = {
+            "classification_code": classification_code,
+            "classification_name": classification_name,
+        }
+    else:
+        classification = None
+
     logging.info(
         f"claim_id: {claim_for_increase.claim_id}, diagnostic_code: {claim_for_increase.diagnostic_code}, form526_submission_id: {claim_for_increase.form526_submission_id}"
     )
---
+++ domain-cc/svc-cc-app/src/util/lookup_table.py
@@ -0,0 +1,44 @@
+import json
+import os
+
+# lookup table generated from code in vagov-claim-classification-data/create_mdeo_dc_to_cc_mapping.py
+LOOKUP_TABLE_PATH = os.path.join(os.path.dirname(__file__), "data", "dc_lookup_table_mvp.json")
+# sourced from Lighthouse Benefits Reference Data /disabilities endpoint:
+# https://developer.va.gov/explore/benefits/docs/benefits_reference_data?version=current
+BRD_CLASSIFICATIONS_PATH = os.path.join(
+    os.path.dirname(__file__), "data", "lh_brd_classification_ids.json"
+)
+
+
+def get_classification_names_by_code():
+    name_by_code = {}
+    with open(BRD_CLASSIFICATIONS_PATH, "r") as fh:
+        print(f'BRD_CLASSIFICATIONS_PATH fh: {fh}')
+        disability_items = json.load(fh)["items"]
+        for item in disability_items:
+            name_by_code[item["id"]] = item["name"]
+    return name_by_code
+
+
+CLASSIFICATION_NAMES_BY_CODE = get_classification_names_by_code()
+
+
+def get_lookup_table():
+    print(f'LOOKUP_TABLE_PATH: {LOOKUP_TABLE_PATH}')
+    with open(LOOKUP_TABLE_PATH, "r") as fh:
+        print(f'fh: {fh}')
+        print('idk')
+        lookup_table = json.load(fh)
+        print(f'lookup_table: {lookup_table}')
+        lookup_table = {
+            int(diagnostic_code): classification_id
+            for diagnostic_code, classification_id in lookup_table.items()
+        }
+        return lookup_table
+
+
+def get_classification_name(classification_code):
+    try:
+        return CLASSIFICATION_NAMES_BY_CODE[classification_code]
+    except KeyError:
+        return None
