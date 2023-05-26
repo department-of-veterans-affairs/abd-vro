a4e258f4ccdeb6cb9719a0ccc694fcd8da3f53fd
verdanceluke <luke@verdance.co>
2023-05-25 22:18:09 -0700

    add test for unmapped diagnostic code

--- domain-cc/svc-cc-app/tests/test_api.py
+++ domain-cc/svc-cc-app/tests/test_api.py
@@ -1,5 +1,9 @@
 from fastapi.testclient import TestClient
 
+RESPIRATORY_CLASSIFICATION = {
+    "classification_code": 9012,
+    "classification_name": 'Respiratory',
+}
 
 def test_classification(client: TestClient):
     json_post_dict = {
@@ -10,12 +14,28 @@ def test_classification(client: TestClient):
 
     response = client.post("/classifier", json=json_post_dict)
     assert response.status_code == 201
+    assert response.json()["classification_code"] == RESPIRATORY_CLASSIFICATION["classification_code"]
+    assert response.json()["classification_name"] == RESPIRATORY_CLASSIFICATION["classification_name"]
+
 
 def test_missing_params(client: TestClient):
-    """ should fail if all required params are not present """
+    """should fail if all required params are not present"""
     json_post_dict = {
         "diagnostic_code": 6510,
     }
 
     response = client.post("/classifier", json=json_post_dict)
     assert response.status_code == 422
+
+
+def test_unmapped_diagnostic_code(client: TestClient):
+    """should return null"""
+    json_post_dict = {
+        "diagnostic_code": 7,
+        "claim_id": 700,
+        "form526_submission_id": 777,
+    }
+
+    response = client.post("/classifier", json=json_post_dict)
+    assert response.status_code == 201
+    assert response.json() is None
