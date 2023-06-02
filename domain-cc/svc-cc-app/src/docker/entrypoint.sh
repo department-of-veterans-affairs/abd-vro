29feba06a25b8d64c565fb5a303f978ea98197f2
verdanceluke <luke@verdance.co>
2023-06-02 14:30:07 -0700

    fix uvicorn command - remove reload, update path

--- domain-cc/svc-cc-app/src/docker/entrypoint.sh
+++ domain-cc/svc-cc-app/src/docker/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # TODO: I don't think --reload is needed when running in a container
-exec uvicorn python_src:api:app --reload --host 0.0.0.0 --port 8120
+exec uvicorn python_src.api:app --host 0.0.0.0 --port 8120
