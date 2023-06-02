a93dac0e49024545716e294037780d04af262765
Yoom Lam <yoom@navapbc.com>
2023-06-02 16:04:47 -0500

    clean up: delete Dockerfile; dockerComposeUp works

--- domain-cc/svc-cc-app/src/docker/entrypoint.sh
+++ domain-cc/svc-cc-app/src/docker/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-# Sets environment variable secrets
-[ -e set-env-secrets.src ] && . ./set-env-secrets.src
-
+# TODO: I don't think --reload is needed when running in a container
 exec uvicorn python_src:api:app --reload --host 0.0.0.0 --port 8120
