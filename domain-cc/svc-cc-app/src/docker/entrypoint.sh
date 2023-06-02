730b8e664099ffc8f02efe73b8ca500c45c040f0
Yoom Lam <yoom@navapbc.com>
2023-06-02 15:43:42 -0500

    bash isn't installed in the base image

--- domain-cc/svc-cc-app/src/docker/entrypoint.sh
+++ domain-cc/svc-cc-app/src/docker/entrypoint.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
 # Sets environment variable secrets
 [ -e set-env-secrets.src ] && . ./set-env-secrets.src
 
 exec uvicorn python_src:api:app --reload --host 0.0.0.0 --port 8120
-
