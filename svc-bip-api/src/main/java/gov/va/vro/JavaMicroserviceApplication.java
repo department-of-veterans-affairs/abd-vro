4a2cfcee2661f669c7b27ed3fa726dabf8ac182b
Ilya Makedon <ilya.makedon@agile6.com>
2023-06-30 10:52:52 -0600

    now responds to rmq messages

---
+++ svc-bip-api/src/main/java/gov/va/vro/JavaMicroserviceApplication.java
@@ -0,0 +1,96 @@
+package gov.va.vro;
+
+import gov.va.vro.model.xample.SomeDtoModel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.listener.api.RabbitListenerErrorHandler;
+import org.springframework.amqp.rabbit.support.ListenerExecutionFailedException;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import java.util.concurrent.CountDownLatch;
+
+@SpringBootApplication
+@Slf4j
+public class JavaMicroserviceApplication {
+  // Also see https://spring.io/guides/gs/messaging-rabbitmq/
+
+  @Bean
+  public CountDownLatch shutdownLatch() {
+    return new CountDownLatch(1);
+  }
+
+  public static void main(String[] args) throws InterruptedException {
+    var ctx = SpringApplication.run(JavaMicroserviceApplication.class, args);
+
+    // Keep this application running
+    final CountDownLatch closeLatch = ctx.getBean(CountDownLatch.class);
+    Runtime.getRuntime()
+        .addShutdownHook(
+            new Thread() {
+              @Override
+              public void run() {
+                closeLatch.countDown();
+              }
+            });
+    closeLatch.await();
+  }
+
+  static final String exchangeName = "xample";
+
+  static final String queueName = "serviceJ";
+
+  static final String routingKey = "serviceJ";
+
+  // Spring AMQP requires that the Queue, the DirectExchange/TopicExchange, and the Binding be
+  // declared as top-level Spring beans in order to be set up properly.
+
+  @Bean
+  Queue queue1() {
+    return new Queue(queueName, true, false, true);
+  }
+
+  @Bean
+  DirectExchange exchange1() {
+    return new DirectExchange(exchangeName, true, true);
+  }
+
+  @Bean
+  Binding binding1() {
+    return BindingBuilder.bind(queue1()).to(exchange1()).with(routingKey);
+  }
+
+  @Bean
+  public MessageConverter jackson2MessageConverter() {
+    var converter = new Jackson2JsonMessageConverter();
+    // converter.setAlwaysConvertToInferredType(true);
+    return converter;
+  }
+
+  @Bean
+  RabbitListenerErrorHandler xampleErrorHandler() {
+    RabbitListenerErrorHandler handler =
+        new RabbitListenerErrorHandler() {
+          @Override
+          public Object handleError(
+              Message amqpMessage,
+              org.springframework.messaging.Message<?> message,
+              ListenerExecutionFailedException exception)
+              throws Exception {
+            log.info("Oh no!", exception);
+
+            if (message != null && message.getHeaders().getReplyChannel() != null) {
+              var errorModel = SomeDtoModel.builder().resourceId("").diagnosticCode("").build();
+              errorModel.header(500, exception.toString());
+
+              return errorModel;
+            }
+
+            return null;
+          }
+        };
+    return handler;
+  }
+}
