35af87f5c95787b1c7e0fff01240b4f76f6c96f4
Ilya Makedon <ilya.makedon@agile6.com>
2023-07-03 09:02:35 -0600

    the error handler returns BipClaim now

--- svc-bip-api/src/main/java/gov/va/vro/bip/config/RMQErrorHandler.java
+++ svc-bip-api/src/main/java/gov/va/vro/bip/config/RMQErrorHandler.java
@@ -1,5 +1,8 @@
 package gov.va.vro.bip.config;
-
+import java.util.List;
+import gov.va.vro.bip.model.BipClaim;
+import gov.va.vro.bip.model.BipUpdateClaimResp;
+import gov.va.vro.bip.model.ClaimContention;
 import gov.va.vro.model.xample.SomeDtoModel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
@@ -9,10 +12,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Configuration
 public class RMQErrorHandler {
+    public static Map<String, Class> qNameMapReturnType;
+    static {
+        qNameMapReturnType = new HashMap<>();
+        qNameMapReturnType.put("getClaimDetailsQueue", BipClaim.class);
+        qNameMapReturnType.put("setClaimToRfdStatusQueue", BipUpdateClaimResp.class);
+        qNameMapReturnType.put("updateClaimStatusQueue", BipUpdateClaimResp.class);
+        //articleMapOne.put("getClaimContentionsQueue", List<ClaimContention>.class);
+        qNameMapReturnType.put("updateClaimContentionQueue", BipUpdateClaimResp.class);
+
+    }
     /**
      * Boilerplate that regiesters error handler method with the system
      */
@@ -31,22 +46,18 @@ public class RMQErrorHandler {
 
     /**
      * Handles uncaught exceptions that occur during processing of messages from queue.
-     * todo: vary the message set to client depending on the cause of exception.  For example
-     * if the cause is MessageConversionException, we should inform the client of expected format.
      */
     public static Object handleError(
             Message amqpMessage,
             org.springframework.messaging.Message<?> message,
             ListenerExecutionFailedException exception) throws Exception {
 
-        log.error("ListenerExecutionFailedException occurred. ", exception.getCause());
-        String correlationId = amqpMessage.getMessageProperties().getCorrelationId().toString();
-        String messageStr = "{\"msg\":\"There was a system error while processing your request with correlationId " + correlationId +
-                ".  Please contact VRO support if the problem persists.\"}";
-        Message response = new Message(messageStr.getBytes(StandardCharsets.UTF_8));
-        response.getMessageProperties().setCorrelationId(correlationId);
-        response.getMessageProperties().getHeaders().put("status", "500");
-        log.info("Responding with: {}", response.toString());
-        return response;
+        log.error("ListenerExecutionFailedException occurred because of:{}.  " +
+                "And the fialed message was {}. ", exception.getCause(), message.toString());
+        String dstQ = exception.getFailedMessage().getMessageProperties().getConsumerQueue();
+        String messageStr = "There was a system error while processing your request.  "+
+                ".  Please contact VRO support if the problem persists.";
+        return new BipClaim(500, messageStr);
+        //todo: this method needs to return whatever type the client expects.
     }
 }
