8bfb5f855b9fa466683c7f590da909ebf117dbc1
Yoom Lam <yoom@navapbc.com>
2023-07-13 19:34:28 -0500

    specify store-type PKCS12

--- svc-bie-kafka/src/docker/entrypoint.sh
+++ svc-bie-kafka/src/docker/entrypoint.sh
@@ -15,14 +15,14 @@ done
 
 export KEYSTORE_FILE="$PWD/keystore.p12"
 echo "$BIE_KAFKA_KEYSTORE_INBASE64" | base64 -d > "$KEYSTORE_FILE"
-echo -e "\nVerifying keystore and its password..."
+echo -e "\nVerifying keystore ($KEYSTORE_FILE) and its password..."
 if ! keytool -list -v -keystore "$KEYSTORE_FILE" -storepass "$BIE_KAFKA_KEYSTORE_PASSWORD" | grep "kafka-keystore"; then
   >&2 echo "ERROR: with keystore"
 fi
 
 export TRUSTSTORE_FILE="$PWD/truststore.p12"
 echo "$BIE_KAFKA_TRUSTSTORE_INBASE64" | base64 -d > "$TRUSTSTORE_FILE"
-echo -e "\nVerifying truststore and its password..."
+echo -e "\nVerifying truststore ($TRUSTSTORE_FILE) and its password..."
 if ! keytool -list -v -keystore "$TRUSTSTORE_FILE" -storepass "$BIE_KAFKA_TRUSTSTORE_PASSWORD" | grep "kafka-truststore"; then
   >&2 echo "ERROR: with truststore"
 fi
