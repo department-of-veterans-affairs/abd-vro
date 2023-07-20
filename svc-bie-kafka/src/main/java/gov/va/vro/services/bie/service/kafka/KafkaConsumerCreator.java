047d069b230f3be445bbc0902a101dc8d6d05dc7
Chengjie Yin <89034346+chengjie8@users.noreply.github.com>
2023-07-20 14:57:11 -0400

    test with spring annotation

--- svc-bie-kafka/src/main/java/gov/va/vro/services/bie/service/kafka/KafkaConsumerCreator.java
+++ svc-bie-kafka/src/main/java/gov/va/vro/services/bie/service/kafka/KafkaConsumerCreator.java
@@ -3,12 +3,14 @@ package gov.va.vro.services.bie.service.kafka;
 import gov.va.vro.services.bie.config.BieProperties;
 import gov.va.vro.services.bie.service.AmqpMessageSender;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.listener.AbstractMessageListenerContainer;
 import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.KafkaMessageListenerContainer;
 import org.springframework.kafka.listener.MessageListener;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,35 +22,47 @@ import javax.annotation.PreDestroy;
 @Component
 public class KafkaConsumerCreator {
 
-  private final List<KafkaMessageListenerContainer<?, ?>> listeners = new ArrayList<>();
+//  private final List<KafkaMessageListenerContainer<?, ?>> listeners = new ArrayList<>();
 
   public KafkaConsumerCreator(
-      final ConsumerFactory<?, ?> consumerFactory,
+//      final ConsumerFactory<?, ?> consumerFactory,
       final AmqpMessageSender amqpMessageSender,
-      final BieProperties bieProperties) {
-    setUpListeners(consumerFactory, amqpMessageSender, bieProperties.getKafkaTopicToAmqpQueueMap());
+//      final BieProperties bieProperties
+  ) {
+//    setUpListeners(consumerFactory, amqpMessageSender, bieProperties.getKafkaTopicToAmqpQueueMap());
   }
 
-  private void setUpListeners(
-      final ConsumerFactory<?, ?> consumerFactory,
-      final AmqpMessageSender amqpMessageSender,
-      final Map<String, String> topicMap) {
-    topicMap.forEach(
-        (kafkaTopic, amqpExchange) -> {
-          final ContainerProperties containerProps = new ContainerProperties(kafkaTopic);
-          containerProps.setAckMode(ContainerProperties.AckMode.RECORD);
-          containerProps.setMessageListener(
-              (MessageListener<Integer, String>)
-                  data -> {
-                    log.debug(
-                        "event=messageReceivedFromKafka topic={} msg={}",
-                        data.topic(),
-                        data.value());
-                    amqpMessageSender.send(amqpExchange, data.topic(), data.value());
-                  });
-          listeners.add(new KafkaMessageListenerContainer<>(consumerFactory, containerProps));
-        });
+  @KafkaListener(topics = {"TST_CONTENTION_BIE_CONTENTION_ASSOCIATED_TO_CLAIM_V02",
+          "TST_CONTENTION_BIE_CONTENTION_UPDATED_V02",
+          "TST_CONTENTION_BIE_CONTENTION_CLASSIFIED_V02",
+        "TST_CONTENTION_BIE_CONTENTION_COMPLETED_V02",
+        "TST_CONTENTION_BIE_CONTENTION_DELETED_V02"})
+  public void consume(String message) {
+    log.debug("Consumed message: " + message);
   }
+
+//  private void setUpListeners(
+//      final ConsumerFactory<?, ?> consumerFactory,
+//      final AmqpMessageSender amqpMessageSender,
+//      final Map<String, String> topicMap) {
+//    topicMap.forEach(
+//        (kafkaTopic, amqpExchange) -> {
+//          final ContainerProperties containerProps = new ContainerProperties(kafkaTopic);
+//          containerProps.setAckMode(ContainerProperties.AckMode.RECORD);
+//          containerProps.setMessageListener(
+//              (MessageListener<Integer, String>)
+//                  data -> {
+//                    log.debug(
+//                        "event=messageReceivedFromKafka topic={} msg={}",
+//                        data.topic(),
+//                        data.value());
+//                    amqpMessageSender.send(amqpExchange, data.topic(), data.value());
+//                  });
+//          listeners.add(new KafkaMessageListenerContainer<>(consumerFactory, containerProps));
+//
+//
+//        });
+//  }
 
   public List<KafkaMessageListenerContainer<?, ?>> getListeners() {
     return List.copyOf(listeners);
