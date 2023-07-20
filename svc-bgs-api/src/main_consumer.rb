d6ac7951de0568d1ad9024040719a6d326d90c0e
dfitchett <135860892+dfitchett@users.noreply.github.com>
2023-07-20 09:53:44 -0700

    Remove test code and files.

--- svc-bgs-api/src/main_consumer.rb
+++ svc-bgs-api/src/main_consumer.rb
@@ -5,7 +5,6 @@ require_relative 'config/setup'
 
 require 'rabbit_subscriber'
 require 'bgs_client'
-require 'json'
 
 BUNNY_ARGS = {
   host: ENV['RABBITMQ_PLACEHOLDERS_HOST'].presence || "localhost",
@@ -32,13 +31,11 @@ def initialize_subscriber(bgs_client)
   subscriber
 end
 
-def run(subscriber, bgs_client)
+def run(subscriber)
   begin
     while true do
       $logger.info "Waiting for messages..."
-      # sleep 10.minutes
-      make_request(bgs_client)
-      sleep 1.minutes
+      sleep 10.minutes
     end
   ensure
     $logger.info "Closing queue subscriptions"
@@ -46,44 +43,7 @@ def run(subscriber, bgs_client)
   end
 end
 
-def make_request(bgs_client)
-  # Send claim notes request
-  req = JSON.parse(File.read("claim_notes.txt"))
-  puts "claim_notes_req=#{req}"
-  begin
-    response = bgs_client.handle_request(req)
-  rescue => e
-    puts e.backtrace
-    response = {
-      statusCode: e.is_a?(ArgumentError) ? 400 : 500,
-      statusMessage: "#{e.class}: #{e.message}",
-    }
-  ensure
-    puts "claim_notes_response=#{response}"
-    stringify = JSON.generate(response)
-    File.write("claim_notes_response.txt", stringify)
-  end
-
-  # Send veteran notes request
-  req = JSON.parse(File.read("veteran_note.txt"))
-  puts "veteran_note_req=#{req}"
-  begin
-    response = bgs_client.handle_request(req)
-  rescue => e
-    puts e.backtrace
-    response = {
-      statusCode: e.is_a?(ArgumentError) ? 400 : 500,
-      statusMessage: "#{e.class}: #{e.message}",
-    }
-  ensure
-    puts "veteran_note_response=#{response}"
-    stringify = JSON.generate(response)
-    File.write("veteran_note_response.txt", stringify)
-  end
-end
-
 $logger.info "Initializing subscriber..."
-bgs_client = BgsClient.new
-subscriber = initialize_subscriber(bgs_client)
+subscriber = initialize_subscriber(BgsClient.new)
 $logger.info "Initialized subscriber!"
-run(subscriber, bgs_client)
+run(subscriber)
