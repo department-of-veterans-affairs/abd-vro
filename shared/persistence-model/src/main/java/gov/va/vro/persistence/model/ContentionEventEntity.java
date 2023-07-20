6d9b0f3e98b98eedf7be54467c5bf981249f448b
Derek Fitchett <135860892+dfitchett@users.noreply.github.com>
2023-07-20 09:04:14 -0700

    1677: Implement xample route for saving messages bie contention events to postgres (#1809)
    * 1677: Implement xample route for saving messages from bie queues via rabbitmq to postgres.
    ---------
    Co-authored-by: Teja <teja@coa.solutions>

---
+++ shared/persistence-model/src/main/java/gov/va/vro/persistence/model/ContentionEventEntity.java
@@ -0,0 +1,28 @@
+package gov.va.vro.persistence.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "bie_contention_event")
+public class ContentionEventEntity extends BaseEntity {
+
+  @Column(name = "notified_at")
+  private LocalDateTime notifiedAt;
+
+  @Column(name = "event")
+  private String eventType;
+
+  @Column(name = "event_details")
+  private String eventDetails;
+
+  // TODO: Non-PII event details to be extracted from eventDetails into their own fields.
+  // See ticket #1680 https://github.com/department-of-veterans-affairs/abd-vro/issues/1680
+}
