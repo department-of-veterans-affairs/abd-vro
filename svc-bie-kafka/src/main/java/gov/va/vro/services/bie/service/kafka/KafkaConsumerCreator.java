e83ee778c7a768ea243f1cb122a2cddcf585a614
Chengjie Yin <89034346+chengjie8@users.noreply.github.com>
2023-07-21 00:31:00 -0400

    test message as byte

--- svc-bie-kafka/src/main/java/gov/va/vro/services/bie/service/kafka/KafkaConsumerCreator.java
+++ svc-bie-kafka/src/main/java/gov/va/vro/services/bie/service/kafka/KafkaConsumerCreator.java
@@ -3,12 +3,14 @@ package gov.va.vro.services.bie.service.kafka;
 import gov.va.vro.services.bie.config.BieProperties;
 import gov.va.vro.services.bie.service.AmqpMessageSender;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
 @Slf4j
 @Component
 public class KafkaConsumerCreator {
@@ -23,8 +25,11 @@ public class KafkaConsumerCreator {
         "#{'${kafka.topic.prefix}'}_CONTENTION_BIE_CONTENTION_COMPLETED_V02",
         "#{'${kafka.topic.prefix}'}_CONTENTION_BIE_CONTENTION_DELETED_V02"
       })
-  public void consume(String message, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
-    log.info("Consumed message: " + message);
-    amqpMessageSender.send(bieProperties.getKafkaTopicToAmqpQueueMap().get(topic), topic, message);
+  public void consume(ConsumerRecord<byte[], byte[]> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+    String key = new String(record.key(), StandardCharsets.UTF_8);
+    String value = new String(record.value(), StandardCharsets.UTF_8);
+    log.info("Consumed message key: " + key);
+    log.info("Consumed message value: " + value);
+    amqpMessageSender.send(bieProperties.getKafkaTopicToAmqpQueueMap().get(topic), topic, value);
   }
 }
