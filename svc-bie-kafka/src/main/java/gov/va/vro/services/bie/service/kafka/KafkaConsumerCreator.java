8b181cc96eacc0b5f30727acad695cc88589bec8
Chengjie Yin <89034346+chengjie8@users.noreply.github.com>
2023-07-24 22:40:25 -0400

    test

--- svc-bie-kafka/src/main/java/gov/va/vro/services/bie/service/kafka/KafkaConsumerCreator.java
+++ svc-bie-kafka/src/main/java/gov/va/vro/services/bie/service/kafka/KafkaConsumerCreator.java
@@ -3,13 +3,15 @@ package gov.va.vro.services.bie.service.kafka;
 import gov.va.vro.services.bie.config.BieProperties;
 import gov.va.vro.services.bie.service.AmqpMessageSender;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Slf4j
 @Component
@@ -20,14 +22,13 @@ public class KafkaConsumerCreator {
   @KafkaListener(
       topics = {"#{'${kafka.topic.prefix}'}_CONTENTION_BIE_CONTENTION_ASSOCIATED_TO_CLAIM_V02"})
   public void consume(
-      ConsumerRecord<GenericRecord, GenericRecord> record,
-      @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
-    GenericRecord key = record.key();
-    GenericRecord value = record.value();
-    log.info("Consumed message key: " + key);
-    log.info("Consumed message value: " + value);
-    log.info("Consumed message value (toString): " + value.toString());
+      ConsumerRecord<Long, byte[]> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+    byte[] decodedBytes = Base64.getDecoder().decode(record.value());
+    String decodedString = new String(decodedBytes, StandardCharsets.UTF_8);
+
+    log.info("Consumed message key: " + record.key());
+    log.info("Consumed message value: " + decodedString);
     amqpMessageSender.send(
-        bieProperties.getKafkaTopicToAmqpQueueMap().get(topic), topic, value.toString());
+        bieProperties.getKafkaTopicToAmqpQueueMap().get(topic), topic, decodedString);
   }
 }
