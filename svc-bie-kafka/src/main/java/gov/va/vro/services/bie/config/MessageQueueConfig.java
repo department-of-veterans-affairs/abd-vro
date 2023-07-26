8c369712cae23448d3ce3eddf4bbe3d22743d492
Yoom Lam <yoom@navapbc.com>
2023-07-26 16:34:52 -0500

    remove queue declarations

--- svc-bie-kafka/src/main/java/gov/va/vro/services/bie/config/MessageQueueConfig.java
+++ svc-bie-kafka/src/main/java/gov/va/vro/services/bie/config/MessageQueueConfig.java
@@ -2,11 +2,8 @@ package gov.va.vro.services.bie.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.AbstractDeclarable;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Declarables;
 import org.springframework.amqp.core.FanoutExchange;
-import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -22,7 +19,6 @@ public class MessageQueueConfig {
 
   private static final boolean IS_DURABLE = true;
   private static final boolean IS_AUTO_DELETED = true;
-  private static final boolean IS_EXCLUSIVE = false;
 
   @Bean
   public MessageConverter messageConverter() {
@@ -35,16 +31,10 @@ public class MessageQueueConfig {
         bieProperties.getKafkaTopicToAmqpQueueMap().values().stream()
             .map(
                 topic -> {
-                  final Queue queue = new Queue(topic, IS_DURABLE, IS_EXCLUSIVE, IS_AUTO_DELETED);
                   final FanoutExchange fanoutExchange =
                       new FanoutExchange(topic, IS_DURABLE, IS_AUTO_DELETED);
-                  final Binding binding = BindingBuilder.bind(queue).to(fanoutExchange);
-                  log.info(
-                      "event=setUpMQ queue={} exchange={} binding={}",
-                      queue,
-                      fanoutExchange,
-                      binding);
-                  return List.of(queue, fanoutExchange, binding);
+                  log.info("event=setUpMQ exchange={}", fanoutExchange);
+                  return List.of(fanoutExchange);
                 })
             .flatMap(Collection::stream)
             .collect(Collectors.toList());
--- svc-bie-kafka/src/main/java/gov/va/vro/services/bie/service/AmqpMessageSender.java
+++ svc-bie-kafka/src/main/java/gov/va/vro/services/bie/service/AmqpMessageSender.java
@@ -2,5 +2,5 @@ package gov.va.vro.services.bie.service;
 
 public interface AmqpMessageSender {
 
-  void send(String exchange, String queue, String message);
+  void send(String exchange, String routingKey, String message);
 }
