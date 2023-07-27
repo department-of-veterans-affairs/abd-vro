eefdecf30e7daca48ca5bbc8971c6debc99c336c
Chengjie Yin <89034346+chengjie8@users.noreply.github.com>
2023-07-27 15:34:27 -0400

    merge from develop and minor update

--- svc-bie-kafka/src/main/java/gov/va/vro/services/bie/service/kafka/KafkaConsumer.java
+++ svc-bie-kafka/src/main/java/gov/va/vro/services/bie/service/kafka/KafkaConsumer.java
@@ -6,8 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
@@ -19,19 +17,25 @@ public class KafkaConsumer {
   @Autowired BieProperties bieProperties;
 
   @KafkaListener(
-      topics = {"#{'${kafka.topic.prefix}'}_CONTENTION_BIE_CONTENTION_ASSOCIATED_TO_CLAIM_V02"})
-  public void consume(
-      ConsumerRecord<byte[], byte[]> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+      topics = {
+        "#{'${kafka.topic.prefix}'}_CONTENTION_BIE_CONTENTION_ASSOCIATED_TO_CLAIM_V02",
+        "#{'${kafka.topic.prefix}'}_CONTENTION_BIE_CONTENTION_UPDATED_V02",
+        "#{'${kafka.topic.prefix}'}_CONTENTION_BIE_CONTENTION_CLASSIFIED_V02",
+        "#{'${kafka.topic.prefix}'}_CONTENTION_BIE_CONTENTION_COMPLETED_V02",
+        "#{'${kafka.topic.prefix}'}_CONTENTION_BIE_CONTENTION_DELETED_V02"
+      })
+  public void consume(ConsumerRecord<byte[], byte[]> record) {
     try {
       String messageKey = new String(record.key(), StandardCharsets.UTF_8);
       String messageValue = new String(record.value(), StandardCharsets.UTF_8);
+      String topicName = record.topic();
 
-      log.info("Topic name: " + topic);
+      log.info("Topic name: " + topicName);
       log.info("Consumed message key: " + messageKey);
       log.info("Consumed message value (before) decode: " + messageValue);
 
       amqpMessageSender.send(
-          bieProperties.getKafkaTopicToAmqpQueueMap().get(topic), topic, messageValue);
+          bieProperties.getKafkaTopicToAmqpExchangeMap().get(topicName), topicName, messageValue);
     } catch (Exception e) {
       log.error("Exception occurred while processing message: " + e.getMessage());
     }
