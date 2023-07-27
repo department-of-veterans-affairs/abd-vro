bbaa336757d87291b741e121b159329121e01e59
Chengjie Yin <89034346+chengjie8@users.noreply.github.com>
2023-07-27 14:49:18 -0400

    minor change

--- svc-bie-kafka/src/main/java/gov/va/vro/services/bie/service/kafka/KafkaConsumer.java
+++ svc-bie-kafka/src/main/java/gov/va/vro/services/bie/service/kafka/KafkaConsumer.java
@@ -3,21 +3,13 @@ package gov.va.vro.services.bie.service.kafka;
 import gov.va.vro.services.bie.config.BieProperties;
 import gov.va.vro.services.bie.service.AmqpMessageSender;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericDatumReader;
-import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.io.DatumReader;
-import org.apache.avro.io.Decoder;
-import org.apache.avro.io.DecoderFactory;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import java.io.IOException;
-import java.io.InputStream;
+
 import java.nio.charset.StandardCharsets;
 
 @Slf4j
@@ -25,47 +17,23 @@ import java.nio.charset.StandardCharsets;
 public class KafkaConsumer {
   @Autowired AmqpMessageSender amqpMessageSender;
   @Autowired BieProperties bieProperties;
-  private final Schema schema;
-
-  public KafkaConsumer() throws IOException {
-    // Load the Avro schema
-    try (InputStream schemaStream =
-        getClass().getResourceAsStream("/avro/ContentionAssociatedToClaim.avsc")) {
-      this.schema = new Schema.Parser().parse(schemaStream);
-    }
-  }
 
   @KafkaListener(
       topics = {"#{'${kafka.topic.prefix}'}_CONTENTION_BIE_CONTENTION_ASSOCIATED_TO_CLAIM_V02"})
   public void consume(
-      ConsumerRecord<byte[], byte[]> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic)
-      throws IOException {
+      ConsumerRecord<byte[], byte[]> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
     try {
-      byte[] value = record.value();
+      String messageKey = new String(record.key(), StandardCharsets.UTF_8);
+      String messageValue = new String(record.value(), StandardCharsets.UTF_8);
+
       log.info("Topic name: " + topic);
-      log.info("Consumed message key: " + new String(record.key(), StandardCharsets.UTF_8));
-      log.info(
-          "Consumed message value (before) decode: " + new String(value, StandardCharsets.UTF_8));
+      log.info("Consumed message key: " + messageKey);
+      log.info("Consumed message value (before) decode: " + messageValue);
 
-      // Create a reader for the Avro schema
-      DatumReader<GenericRecord> reader = new GenericDatumReader<>(schema);
-      // Create a decoder for the Avro-encoded data
-      Decoder decoder = DecoderFactory.get().binaryDecoder(value, null);
-      // Deserialize the data
-      GenericRecord genericRecord = reader.read(null, decoder);
-
-      log.info("Consumed message value (after) : " + genericRecord);
-      log.info("Consumed message value(toString): " + genericRecord.toString());
       amqpMessageSender.send(
-          bieProperties.getKafkaTopicToAmqpQueueMap().get(topic), topic, genericRecord.toString());
+          bieProperties.getKafkaTopicToAmqpQueueMap().get(topic), topic, messageValue);
     } catch (Exception e) {
-      handleException(e);
+      log.error("Exception occurred while processing message: " + e.getMessage());
     }
-  }
-
-  @ExceptionHandler
-  public void handleException(Exception e) {
-    // Log the exception and handle it
-    log.error("Exception occurred while processing message: " + e.getMessage());
   }
 }
