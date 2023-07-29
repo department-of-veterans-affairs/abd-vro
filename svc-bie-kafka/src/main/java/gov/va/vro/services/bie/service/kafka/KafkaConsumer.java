4c12d158dda5669e052231e13316c7c1cdbe174d
Cheng <89034346+chengjie8@users.noreply.github.com>
2023-07-28 20:11:20 -0400

    enable kafka broker SSL config and spring kafka annotation (#1859)
    * added/correct kafka properties
    
    * Split VRO_SECRETS_* into individual K8s secrets to improve secret update propagation
    
    * update group name
    
    * Automated commit: Unpin versions of updated images
    
    * minor change
    
    * test with spring annotation
    
    * fix compile issue
    
    * test with dynamic prefix for topics and fetch earliest
    
    * clean up
    
    * minor fix
    
    * minor change
    
    * test message as byte
    
    * try with one topic
    
    * test with GenericRecord
    
    * minor change
    
    * test with new group id
    
    * minor change
    
    * minor change
    
    * add avro serializer for topic message decode
    
    * minor change
    
    * minor change
    
    * test
    
    * minor change
    
    * add simple exception handler
    
    * minor change
    
    * rename and minor change
    
    * minor change
    
    * merge from develop and minor update
    
    * clean up
    
    * minor change
    
    * address code reveiw comments
    
    * fix spotless issue
    
    ---------
    
    Co-authored-by: Yoom Lam <yoom@navapbc.com>
    Co-authored-by: VRO Machine User <abd-vro-machine@users.noreply.github.com>
    Co-authored-by: Teja <teja@coa.solutions>

---
+++ svc-bie-kafka/src/main/java/gov/va/vro/services/bie/service/kafka/KafkaConsumer.java
@@ -0,0 +1,40 @@
+package gov.va.vro.services.bie.service.kafka;
+
+import gov.va.vro.services.bie.config.BieProperties;
+import gov.va.vro.services.bie.service.AmqpMessageSender;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class KafkaConsumer {
+  private final AmqpMessageSender amqpMessageSender;
+  private final BieProperties bieProperties;
+
+  @KafkaListener(
+      topics = {
+        "#{'${kafka.topic.prefix}'}_CONTENTION_BIE_CONTENTION_ASSOCIATED_TO_CLAIM_V02",
+        "#{'${kafka.topic.prefix}'}_CONTENTION_BIE_CONTENTION_UPDATED_V02",
+        "#{'${kafka.topic.prefix}'}_CONTENTION_BIE_CONTENTION_CLASSIFIED_V02",
+        "#{'${kafka.topic.prefix}'}_CONTENTION_BIE_CONTENTION_COMPLETED_V02",
+        "#{'${kafka.topic.prefix}'}_CONTENTION_BIE_CONTENTION_DELETED_V02"
+      })
+  public void consume(ConsumerRecord<byte[], byte[]> record) {
+    String messageKey = new String(record.key(), StandardCharsets.UTF_8);
+    String messageValue = new String(record.value(), StandardCharsets.UTF_8);
+    String topicName = record.topic();
+
+    log.info("Topic name: {}", topicName);
+    log.info("Consumed message key: {}", messageKey);
+    log.info("Consumed message value (before) decode: {}", messageValue);
+
+    amqpMessageSender.send(
+        bieProperties.getKafkaTopicToAmqpExchangeMap().get(topicName), topicName, messageValue);
+  }
+}
