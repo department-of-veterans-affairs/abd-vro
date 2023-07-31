5871e44a16ecf3efa72f4d72deaeeb7e82626e92
Teja <teja@coa.solutions>
2023-07-31 09:38:25 -0400

    workflow route updates and adding tests

--- domain-xample/xample-workflows/src/test/java/gov/va/vro/routes/xample/BieXampleRoutesTest.java
+++ domain-xample/xample-workflows/src/test/java/gov/va/vro/routes/xample/BieXampleRoutesTest.java
@@ -1,16 +1,13 @@
 package gov.va.vro.routes.xample;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import gov.va.vro.model.biekafka.BieMessagePayload;
-import gov.va.vro.model.biekafka.ContentionKafkaEventType;
+import gov.va.vro.model.biekafka.test.BieMessagePayloadFactory;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.AdviceWith;
 import org.apache.camel.test.junit5.CamelTestSupport;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -20,9 +17,10 @@ import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @Slf4j
 @ExtendWith(MockitoExtension.class)
-@Disabled
 class BieXampleRoutesTest extends CamelTestSupport {
 
   private static final String STARTING_URI = "seda:testQueue";
@@ -56,12 +54,7 @@ class BieXampleRoutesTest extends CamelTestSupport {
     if (isUseAdviceWith()) context.start();
   }
 
-  final BieMessagePayload testItem =
-      BieMessagePayload.builder()
-          .eventType(ContentionKafkaEventType.valueOf("testEvent"))
-          //          .eventDetails("testEventDetails")
-          //          .notifiedAt(LocalDateTime.now().toString())
-          .build();
+  final BieMessagePayload testItem = BieMessagePayloadFactory.create();
 
   @Test
   @SneakyThrows
@@ -69,9 +62,16 @@ class BieXampleRoutesTest extends CamelTestSupport {
     final BieMessagePayload response =
         template.requestBody(STARTING_URI, testItem, BieMessagePayload.class);
     assertThat(response.getEventType()).isEqualTo(testItem.getEventType());
-    assertThat(response.getEventDetails()).isEqualTo(testItem.getEventDetails());
+    assertThat(response.getStatusMessage()).isNull();
     assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
-
+    assertThat(response.getClaimId()).isEqualTo(testItem.getClaimId());
+    assertThat(response.getContentionClassificationName())
+        .isEqualTo(testItem.getContentionClassificationName());
+    assertThat(response.getContentionId()).isEqualTo(testItem.getContentionId());
+    assertThat(response.getContentionClassificationCode())
+        .isEqualTo(testItem.getContentionClassificationCode());
+    assertThat(response.getDiagnosticTypeCode()).isEqualTo(testItem.getDiagnosticTypeCode());
+    assertThat(response.getEventDetails()).isEqualTo(testItem.getEventDetails());
     assertMockEndpointsSatisfied();
   }
 
@@ -87,6 +87,7 @@ class BieXampleRoutesTest extends CamelTestSupport {
     assertThat(response.getEventType()).isEqualTo(testItem.getEventType());
     assertThat(response.getEventDetails()).isEqualTo(testItem.getEventDetails());
     assertThat(response.getStatus()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    assertThat(response.getStatusMessage()).isEqualTo(exception.toString());
 
     assertMockEndpointsSatisfied();
   }
