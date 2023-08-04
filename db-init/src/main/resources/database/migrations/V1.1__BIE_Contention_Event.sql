62a458f335eb17f0498dbd6bf6635c62786211de
Teja <teja@coa.solutions>
2023-08-04 12:51:35 -0400

    update sql scripts

--- db-init/src/main/resources/database/migrations/V1.1__BIE_Contention_Event.sql
+++ db-init/src/main/resources/database/migrations/V1.1__BIE_Contention_Event.sql
@@ -1,17 +1,11 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS "bie_contention_event"
-(
-    id                             uuid DEFAULT uuid_generate_v4() NOT NULL,
-    notified_at                    TIMESTAMP NOT NULL,
-    occurred_at                    TIMESTAMP NOT NULL,
-    event_type                     VARCHAR(255),
-    claim_id                       BIGINT,
-    contention_id                  BIGINT,
-    diagnostic_type_code           VARCHAR(255),
-    contention_classification_code VARCHAR(255),
-    event_details                  JSONB,
-    created_at                     TIMESTAMP NOT NULL,
-    updated_at                     TIMESTAMP NOT NULL,
+CREATE TABLE IF NOT EXISTS "bie_contention_event" (
+    id uuid DEFAULT uuid_generate_v4 () NOT NULL,
+    event VARCHAR NOT NULL,
+    event_details VARCHAR NOT NULL,
+    notified_at timestamp NOT NULL,
+    created_at timestamp NOT NULL,
+    updated_at timestamp NOT NULL,
     PRIMARY KEY(id)
-);
+    );
---
+++ db-init/src/main/resources/database/migrations/V1.2__BIE_Contention_Event_Recreate_Table.sql
@@ -0,0 +1,19 @@
+-- Drop the old table
+DROP TABLE IF EXISTS "bie_contention_event";
+
+-- Create the new table
+CREATE TABLE IF NOT EXISTS "bie_contention_event"
+(
+    id                             uuid DEFAULT uuid_generate_v4() NOT NULL,
+    notified_at                    TIMESTAMP NOT NULL,
+    occurred_at                    TIMESTAMP NOT NULL,
+    event_type                     VARCHAR(255),
+    claim_id                       BIGINT,
+    contention_id                  BIGINT,
+    diagnostic_type_code           VARCHAR(255),
+    contention_classification_code VARCHAR(255),
+    event_details                  JSONB,
+    created_at                     TIMESTAMP NOT NULL,
+    updated_at                     TIMESTAMP NOT NULL,
+    PRIMARY KEY(id)
+);
