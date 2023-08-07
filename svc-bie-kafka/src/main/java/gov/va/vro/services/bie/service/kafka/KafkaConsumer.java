e3a04d01fa97d12b59b71f421056609246d86dbf
Teja <teja@coa.solutions>
2023-08-07 10:56:03 -0500

    refactor kafka consumer using generic record avro

--- svc-bie-kafka/src/main/java/gov/va/vro/services/bie/service/kafka/KafkaConsumer.java
+++ svc-bie-kafka/src/main/java/gov/va/vro/services/bie/service/kafka/KafkaConsumer.java
@@ -1,20 +1,19 @@
 package gov.va.vro.services.bie.service.kafka;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import gov.va.vro.model.biekafka.BieMessagePayload;
 import gov.va.vro.model.biekafka.ContentionEvent;
 import gov.va.vro.services.bie.config.BieProperties;
 import gov.va.vro.services.bie.service.AmqpMessageSender;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Set;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,6 +21,14 @@ import java.util.stream.Collectors;
 public class KafkaConsumer {
   private final AmqpMessageSender amqpMessageSender;
   private final BieProperties bieProperties;
+
+  private final String KEY_CLAIM_ID = "ClaimId";
+  private final String KEY_CONTENTION_ID = "ContentionId";
+  private final String KEY_CONTENTION_CLASSIFICATION_NAME = "ContentionClassificationName";
+  private final String KEY_CONTENTION_CLASSIFICATION_CODE = "ContentionClassificationCode";
+  private final String KEY_DIAGNOSTIC_TYPE_CODE = "DiagnosticTypeCode";
+  private final String KEY_EVENT_TIME = "EventTime";
+
 
   public ContentionEvent mapTopicToEvent(String topic) {
 
@@ -41,6 +48,16 @@ public class KafkaConsumer {
     }
   }
 
+  public static Map<String, Object> recordToMapIgnoringFields(GenericRecord record, Set<String> ignoredFields) {
+    Map<String, Object> map = new HashMap<>();
+    record.getSchema().getFields().forEach(field -> {
+      if (!ignoredFields.contains(field.name())) {
+        map.put(field.name(), record.get(field.name()));
+      }
+    });
+    return map;
+  }
+
   @KafkaListener(
       topics = {
         "#{'${kafka.topic.prefix}'}_CONTENTION_BIE_CONTENTION_ASSOCIATED_TO_CLAIM_V02",
@@ -49,42 +66,33 @@ public class KafkaConsumer {
         "#{'${kafka.topic.prefix}'}_CONTENTION_BIE_CONTENTION_COMPLETED_V02",
         "#{'${kafka.topic.prefix}'}_CONTENTION_BIE_CONTENTION_DELETED_V02"
       })
-  public void consume(ConsumerRecord<byte[], byte[]> record) {
+  public void consume(ConsumerRecord<String, GenericRecord> record) {
     try {
-      log.info("Consumed record value: {}", new String(record.value(), StandardCharsets.UTF_8));
-      //      String messageKey = new String(record.key(), StandardCharsets.UTF_8);
-      String messageValue = new String(record.value(), StandardCharsets.UTF_8);
+      GenericRecord messageValue = record.value();
       String topicName = record.topic();
-      //      log.info("Consumed message key: {}", messageKey);
       log.info("Consumed message value (before) decode: {}", messageValue);
-      // convert messageValue to json using jackson
-      final ObjectMapper objectMapper = new ObjectMapper();
-      final Map<String, Object> jsonBody = objectMapper.readValue(messageValue, Map.class);
 
-      log.info("JSONBODY: " + jsonBody);
+      final Set<String> keysToRemove =
+              Set.of(
+                  KEY_CLAIM_ID,
+                  KEY_CONTENTION_ID,
+                  KEY_CONTENTION_CLASSIFICATION_NAME,
+                  KEY_CONTENTION_CLASSIFICATION_CODE,
+                  KEY_DIAGNOSTIC_TYPE_CODE,
+                  KEY_EVENT_TIME
+              );
 
-      final var keysToRemove =
-          Arrays.asList(
-              "ClaimId",
-              "DiagnosticTypeCode",
-              "ContentionId",
-              "ContentionClassificationName",
-              "ContentionClassificationCode",
-              "EventTime");
-      Map<String, Object> eventDetails =
-          jsonBody.entrySet().stream()
-              .filter(entry -> !keysToRemove.contains(entry.getKey()))
-              .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().toString()));
+      Map<String, Object> eventDetails = recordToMapIgnoringFields(messageValue, keysToRemove);
 
       BieMessagePayload payload =
           BieMessagePayload.builder()
               .eventType(ContentionEvent.valueOf(mapTopicToEvent(topicName).toString()))
-              .claimId((long) jsonBody.get("ClaimId"))
-              .contentionId((long) jsonBody.get("ContentionId"))
-              .contentionClassificationName((String) jsonBody.get("ContentionClassificationName"))
-              .contentionClassificationCode((String) jsonBody.get("ContentionClassificationCode"))
-              .diagnosticTypeCode((String) jsonBody.get("DiagnosticTypeCode"))
-              .occurredAt((Long) jsonBody.get("EventTime"))
+              .claimId((long) messageValue.get(KEY_CLAIM_ID))
+              .contentionId((long) messageValue.get(KEY_CONTENTION_ID))
+              .contentionClassificationName((String) messageValue.get(KEY_CONTENTION_CLASSIFICATION_NAME))
+              .contentionClassificationCode((String) messageValue.get(KEY_CONTENTION_CLASSIFICATION_CODE))
+              .diagnosticTypeCode((String) messageValue.get(KEY_DIAGNOSTIC_TYPE_CODE))
+              .occurredAt((Long) messageValue.get(KEY_EVENT_TIME))
               .notifiedAt(record.timestamp())
               .status(200)
               .eventDetails(eventDetails)
