4e5769ff1937c50d9afeecefbdfa022f90f0dae5
Verdance Luke <verdanceluke@Verdances-MacBook-Pro.local>
2023-08-11 16:46:43 -0700

    add vro database url

--- domain-cc/cc-app/src/python_src/database.py
+++ domain-cc/cc-app/src/python_src/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+VRO_POSTGRES_URL = "postgres://vro_user:vro_user_pw@localhost:5432/vro"
 POSTGRES_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/postgres"
 # SQLITE_DATABASE_URL = "sqlite:///./sql_app.db"
 # engine = create_engine(SQLITE_DATABASE_URL, connect_args={"check_same_thread": False})
