989a9421f006a8acd326e53238e9cb5e988231ea
Verdance Luke <verdanceluke@Verdances-MacBook-Pro.local>
2023-08-11 17:16:52 -0700

    debugging use of vro docker postgres

--- domain-cc/cc-app/src/python_src/database.py
+++ domain-cc/cc-app/src/python_src/database.py
@@ -3,10 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 VRO_POSTGRES_URL = "postgres://vro_user:vro_user_pw@localhost:5432/vro"
-POSTGRES_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/postgres"
+# POSTGRES_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/postgres"
 # SQLITE_DATABASE_URL = "sqlite:///./sql_app.db"
 # engine = create_engine(SQLITE_DATABASE_URL, connect_args={"check_same_thread": False})
-engine = create_engine(POSTGRES_DATABASE_URL)
+# engine = create_engine(POSTGRES_DATABASE_URL)
+engine = create_engine(VRO_POSTGRES_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=True)
 Base = declarative_base()
 
