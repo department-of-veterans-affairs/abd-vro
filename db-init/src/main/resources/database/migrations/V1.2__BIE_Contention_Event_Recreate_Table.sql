9b8c8a7c2801e3cb4106d9a5314a955c1752a950
Teja <teja@coa.solutions>
2023-08-17 14:34:08 -0400

    adding comments to db table setup

--- db-init/src/main/resources/database/migrations/V1.2__BIE_Contention_Event_Recreate_Table.sql
+++ db-init/src/main/resources/database/migrations/V1.2__BIE_Contention_Event_Recreate_Table.sql
@@ -1,18 +1,38 @@
 -- Drop the old table
 DROP TABLE IF EXISTS "bie_contention_event";
 
--- Create the new table
 CREATE TABLE IF NOT EXISTS "bie_contention_event"
 (
-    id                             uuid DEFAULT uuid_generate_v4() NOT NULL,
-    notified_at                    TIMESTAMP NOT NULL,
-    occurred_at                    TIMESTAMP NOT NULL,
-    event_type                     VARCHAR(255),
-    claim_id                       BIGINT,
-    contention_id                  BIGINT,
-    diagnostic_type_code           VARCHAR(255),
+    -- Unique identifier for each contention event
+    id uuid DEFAULT uuid_generate_v4() NOT NULL,
+
+    -- Timestamp when the event was notified
+    notified_at TIMESTAMP NOT NULL,
+
+    -- Timestamp when the event actually occurred
+    occurred_at TIMESTAMP NOT NULL,
+
+    -- Type of the Contention event
+    -- Reference (abd-vro/shared/lib-bie-kafka/src/main/java/gov/va/vro/model/biekafka/ContentionEvent.java)
+    event_type VARCHAR(255),
+
+    -- Reference to the claim associated with the contention
+    claim_id BIGINT,
+
+    -- Reference to the specific contention
+    contention_id BIGINT,
+
+    -- Code representing the type of diagnostic related to the contention
+    diagnostic_type_code VARCHAR(255),
+
+    -- Code classifying the contention
     contention_classification_code VARCHAR(255),
-    created_at                     TIMESTAMP NOT NULL,
-    updated_at                     TIMESTAMP NOT NULL,
+
+    -- Timestamp when the record was created in the database
+    created_at TIMESTAMP NOT NULL,
+
+    -- Timestamp when the record was last updated in the database
+    updated_at TIMESTAMP NOT NULL,
+
     PRIMARY KEY(id)
 );
