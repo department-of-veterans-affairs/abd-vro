6e37558f697921fc6eb771959e22aeae356ea45c
Luke Short <lukeshortemail@gmail.com>
2023-08-18 13:58:51 -0700

    basic test to make sure dropdow LUT can be loaded properly

---
+++ domain-cc/cc-app/tests/test_data.py
@@ -0,0 +1,8 @@
+from src.python_src.util.lookup_table import DropdownLookupTable
+
+DROPDOWN_LUT_SIZE = 489
+
+def test_build_dropdown_lookup_table():
+    lookup_table = DropdownLookupTable()
+    assert len(lookup_table) == DROPDOWN_LUT_SIZE
+
