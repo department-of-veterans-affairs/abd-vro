6ae9ad6809786df6f310964cba464a2f1de99c1f
Luke Short <lukeshortemail@gmail.com>
2023-08-21 07:42:18 -0700

    WIP using claim_type attribute to classify

--- domain-cc/cc-app/src/python_src/api.py
+++ domain-cc/cc-app/src/python_src/api.py
@@ -3,6 +3,8 @@ import sys
 from typing import Optional
 
 from fastapi import FastAPI, HTTPException
+from .pydantic_models import Claim, ClaimForIncrease, PredictedClassification
+from .util.lookup_table import DropdownLookupTable, DiagnosticCodeLookupTable
 
 from .pydantic_models import ClaimForIncrease, PredictedClassification
 from .util.brd_classification_codes import get_classification_name
@@ -63,4 +65,32 @@ def get_classification(
         f"claim_id: {claim_for_increase.claim_id}, diagnostic_code: {claim_for_increase.diagnostic_code}, form526_submission_id: {claim_for_increase.form526_submission_id}"
     )
     logging.info(f"classification: {classification}")
+    return classification
+
+
+@app.post("/v2/classifier")
+def get_classification_v2(claim: Claim) -> Optional[PredictedClassification]:
+    classification_code = None
+    if isinstance(claim, ClaimForIncrease):
+        classification_code = dc_lookup_table.get(claim.diagnostic_code, None)
+
+    if not classification_code:
+        classification_code = dropdown_lookup_table.get(claim.contention_text, None)
+
+    if classification_code:
+        classification_name = get_classification_name(classification_code)
+        classification = {
+            "classification_code": classification_code,
+            "classification_name": classification_name,
+        }
+    else:
+        classification = None
+
+    if isinstance(claim, ClaimForIncrease):
+        logging.info(f"claim_id: {claim.claim_id}, diagnostic_code: {claim.diagnostic_code}, form526_submission_id: {claim.form526_submission_id}")
+    else:
+        logging.info(
+            f"claim_id: {claim.claim_id}, form526_submission_id: {claim.form526_submission_id}")
+    logging.info(f"classification: {classification}")
+
     return classification
