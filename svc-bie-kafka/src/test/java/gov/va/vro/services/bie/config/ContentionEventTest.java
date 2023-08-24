1654ddf8db58e410d4fa71f0288b681e47ac864f
Teja <teja@coa.solutions>
2023-08-24 15:47:59 -0400

    test file name change

--- svc-bie-kafka/src/test/java/gov/va/vro/services/bie/config/ContentionEventTest.java
+++ svc-bie-kafka/src/test/java/gov/va/vro/services/bie/config/ContentionEventTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class MessageHelperTest {
+public class ContentionEventTest {
 
   @ParameterizedTest
   @CsvSource({
