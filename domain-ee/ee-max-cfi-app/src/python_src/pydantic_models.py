b57a0e5a8c5a2b53a78812811db484101f5b034d
Derek Fitchett <135860892+dfitchett@users.noreply.github.com>
2023-09-01 07:32:09 -0700

    Loosened the requirements for max-ratings endpoint. (#2006)

--- domain-ee/ee-max-cfi-app/src/python_src/pydantic_models.py
+++ domain-ee/ee-max-cfi-app/src/python_src/pydantic_models.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, conint, conlist
 
 
 class MaxRatingsForClaimForIncreaseRequest(BaseModel):
-    diagnostic_codes: conlist(conint(strict=True), unique_items=True, min_items=1, max_items=100)
+    diagnostic_codes: conlist(conint(strict=True), max_items=1000)
 
 
 class Rating(BaseModel):
