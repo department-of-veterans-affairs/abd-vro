8c83c3cc667192f23b369d40e624e8ee2dd2c90b
Mason Watson <masonwatson@navapbc.com>
2023-09-20 20:11:19 -0700

    rm postgres Dockerfile

--- postgres/src/main/resources/init-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d postgres -v flyuser="$FLYWAY_USER" -v flypass="'$FLYWAY_PASSWORD'" -v flydb="$FLYWAY_DB" <<-EOSQL
-	CREATE ROLE :flyuser PASSWORD :flypass LOGIN CREATEROLE;
-	CREATE DATABASE :flydb;
-	GRANT ALL PRIVILEGES ON DATABASE :flydb TO :flyuser;
-EOSQL
