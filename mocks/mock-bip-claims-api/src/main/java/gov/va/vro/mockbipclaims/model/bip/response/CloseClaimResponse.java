0272d5a2b9967704c5164858499047bc870510b7
Teja <teja@coa.solutions>
2023-10-18 16:02:07 -0400

    updated comment

--- mocks/mock-bip-claims-api/src/main/java/gov/va/vro/mockbipclaims/model/bip/response/CloseClaimResponse.java
+++ mocks/mock-bip-claims-api/src/main/java/gov/va/vro/mockbipclaims/model/bip/response/CloseClaimResponse.java
@@ -3,6 +3,6 @@ package gov.va.vro.mockbipclaims.model.bip.response;
 import gov.va.vro.mockbipclaims.model.bip.ProviderResponse;
 import lombok.Data;
 
-/** ClaimDetailResponse. */
+/** CloseClaimResponse. */
 @Data
 public class CloseClaimResponse extends ProviderResponse {}
