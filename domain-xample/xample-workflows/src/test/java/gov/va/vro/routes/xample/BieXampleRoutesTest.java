02423ea410c08159cab2ad77dc92034d5ba2a6ca
josiahjones <josiah.jones@agile6.com>
2023-10-18 10:57:03 -0700

    Change assertIsSatisfied to the new method

--- domain-xample/xample-workflows/src/test/java/gov/va/vro/routes/xample/BieXampleRoutesTest.java
+++ domain-xample/xample-workflows/src/test/java/gov/va/vro/routes/xample/BieXampleRoutesTest.java
@@ -8,6 +8,7 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.AdviceWith;
+import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit5.CamelTestSupport;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -60,22 +61,21 @@ class BieXampleRoutesTest extends CamelTestSupport {
   @SneakyThrows
   void testSaveContentionEventRoute() {
     final BieMessagePayload response =
-        template.requestBody(STARTING_URI, testItem, BieMessagePayload.class);
+            template.requestBody(STARTING_URI, testItem, BieMessagePayload.class);
     assertThat(response.getEventType()).isEqualTo(testItem.getEventType());
     assertThat(response.getStatusMessage()).isNull();
     assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
     assertThat(response.getClaimId()).isEqualTo(testItem.getClaimId());
     assertThat(response.getContentionClassificationName())
-        .isEqualTo(testItem.getContentionClassificationName());
+            .isEqualTo(testItem.getContentionClassificationName());
     assertThat(response.getContentionId()).isEqualTo(testItem.getContentionId());
     assertThat(response.getContentionTypeCode()).isEqualTo(testItem.getContentionTypeCode());
     assertThat(response.getDiagnosticTypeCode()).isEqualTo(testItem.getDiagnosticTypeCode());
     assertThat(response.getActionName()).isEqualTo(testItem.getActionName());
     assertThat(response.getActionResultName()).isEqualTo(testItem.getActionResultName());
 
-    assertMockEndpointsSatisfied();
+    MockEndpoint.assertIsSatisfied(context);
   }
-
   @Test
   @SneakyThrows
   void testMainRouteOnException() {
@@ -89,6 +89,6 @@ class BieXampleRoutesTest extends CamelTestSupport {
     assertThat(response.getStatus()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
     assertThat(response.getStatusMessage()).isEqualTo(exception.toString());
 
-    assertMockEndpointsSatisfied();
+    MockEndpoint.assertIsSatisfied(context);
   }
 }
