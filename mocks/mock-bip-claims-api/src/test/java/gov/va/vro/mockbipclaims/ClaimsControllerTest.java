0d7734c8876a6aed6a3d51c86becc325260a43fc
Teja <teja@coa.solutions>
2023-10-19 12:22:17 -0400

    fix lint issues

--- mocks/mock-bip-claims-api/src/test/java/gov/va/vro/mockbipclaims/ClaimsControllerTest.java
+++ mocks/mock-bip-claims-api/src/test/java/gov/va/vro/mockbipclaims/ClaimsControllerTest.java
@@ -1,12 +1,10 @@
 package gov.va.vro.mockbipclaims.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
 
 import gov.va.vro.mockbipclaims.model.bip.ClaimDetail;
-import gov.va.vro.mockbipclaims.model.bip.Message;
 import gov.va.vro.mockbipclaims.model.bip.response.ClaimDetailResponse;
 import gov.va.vro.mockbipclaims.model.bip.response.CloseClaimResponse;
 import gov.va.vro.mockbipclaims.model.store.ClaimStore;
@@ -22,65 +20,65 @@ import org.springframework.web.server.ResponseStatusException;
 
 public class ClaimsControllerTest {
 
-    @InjectMocks
-    private ClaimsController claimsController;
+  @InjectMocks private ClaimsController claimsController;
 
-    @Mock
-    private ClaimStore claimStore;
+  @Mock private ClaimStore claimStore;
 
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
+  @BeforeEach
+  public void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  public void testGetClaimById_Success() {
+    Long claimId = 1010L;
+    ClaimDetail claimDetail = new ClaimDetail();
+    ClaimStoreItem item = new ClaimStoreItem();
+    item.setClaimDetail(claimDetail);
+    when(claimStore.get(claimId)).thenReturn(item);
+
+    ResponseEntity<ClaimDetailResponse> response = claimsController.getClaimById(claimId);
+
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    assertEquals(claimDetail, response.getBody().getClaim());
+  }
+
+  @Test
+  public void testGetClaimById_NotFound() {
+    Long claimId = 1010L;
+    when(claimStore.get(claimId)).thenReturn(null);
+
+    try {
+      claimsController.getClaimById(claimId);
+    } catch (ResponseStatusException e) {
+      assertEquals(HttpStatus.NOT_FOUND, e.getStatus());
     }
+  }
 
-    @Test
-    public void testGetClaimById_Success() {
-        Long claimId = 1010L;
-        ClaimDetail claimDetail = new ClaimDetail();
-        ClaimStoreItem item = new ClaimStoreItem();
-        item.setClaimDetail(claimDetail);
-        when(claimStore.get(claimId)).thenReturn(item);
+  @Test
+  public void testCancelClaimById_Success() {
+    Long claimId = 1010L;
+    ClaimStoreItem item = new ClaimStoreItem();
+    when(claimStore.get(claimId)).thenReturn(item);
 
-        ResponseEntity<ClaimDetailResponse> response = claimsController.getClaimById(claimId);
+    ResponseEntity<CloseClaimResponse> response = claimsController.cancelClaimById(claimId);
 
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(claimDetail, response.getBody().getClaim());
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    assertEquals(
+        "Successfully canceled the claim with id: " + claimId,
+        response.getBody().getMessages().get(0).getText());
+    verify(claimStore).cancel(claimId);
+  }
+
+  @Test
+  public void testCancelClaimById_NotFound() {
+    Long claimId = 1010L;
+    when(claimStore.get(claimId)).thenReturn(null);
+
+    try {
+      claimsController.cancelClaimById(claimId);
+    } catch (ResponseStatusException e) {
+      assertEquals(HttpStatus.NOT_FOUND, e.getStatus());
     }
-
-    @Test
-    public void testGetClaimById_NotFound() {
-        Long claimId = 1010L;
-        when(claimStore.get(claimId)).thenReturn(null);
-
-        try {
-            claimsController.getClaimById(claimId);
-        } catch (ResponseStatusException e) {
-            assertEquals(HttpStatus.NOT_FOUND, e.getStatus());
-        }
-    }
-
-    @Test
-    public void testCancelClaimById_Success() {
-        Long claimId = 1010L;
-        ClaimStoreItem item = new ClaimStoreItem();
-        when(claimStore.get(claimId)).thenReturn(item);
-
-        ResponseEntity<CloseClaimResponse> response = claimsController.cancelClaimById(claimId);
-
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals("Successfully canceled the claim with id: " + claimId, response.getBody().getMessages().get(0).getText());
-        verify(claimStore).cancel(claimId);
-    }
-
-    @Test
-    public void testCancelClaimById_NotFound() {
-        Long claimId = 1010L;
-        when(claimStore.get(claimId)).thenReturn(null);
-
-        try {
-            claimsController.cancelClaimById(claimId);
-        } catch (ResponseStatusException e) {
-            assertEquals(HttpStatus.NOT_FOUND, e.getStatus());
-        }
-    }
+  }
 }
