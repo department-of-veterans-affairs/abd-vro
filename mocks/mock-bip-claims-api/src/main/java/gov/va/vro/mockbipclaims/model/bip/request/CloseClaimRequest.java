b7d84ca7255edaf1cd0feb241ce642b1f69bbae5
Teja <teja@coa.solutions>
2023-10-23 10:46:16 -0600

    adding equals and hash code to close claim request

--- mocks/mock-bip-claims-api/src/main/java/gov/va/vro/mockbipclaims/model/bip/request/CloseClaimRequest.java
+++ mocks/mock-bip-claims-api/src/main/java/gov/va/vro/mockbipclaims/model/bip/request/CloseClaimRequest.java
@@ -5,9 +5,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
-
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper=false)
 public class CloseClaimRequest extends ProviderRequest {
 
   @NotNull private String lifecycleStatusReasonCode;
