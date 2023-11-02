114df77531d8780018652b5f5034d9107300c9c2
Chengjie Yin <89034346+chengjie8@users.noreply.github.com>
2023-11-02 10:10:39 -0400

    minor change

--- shared/lib-bie-kafka/src/main/java/gov/va/vro/model/biekafka/BieMessagePayload.java
+++ shared/lib-bie-kafka/src/main/java/gov/va/vro/model/biekafka/BieMessagePayload.java
@@ -20,6 +20,24 @@ public class BieMessagePayload {
   private String diagnosticTypeCode;
   private String actionName;
   private String actionResultName;
+
+
   private long notifiedAt;
   private long occurredAt;
+
+  private long dateAdded;
+  private long dateCompleted;
+  private long dateUpdated;
+
+  private String actorStation;
+  private boolean automationIndicator;
+  private String benefitClaimTypeCode;
+  private String contentionStatusTypeCode;
+  private String currentLifecycleStatus;
+
+  private String details;
+  private long eventTime;
+  private String journalStatusTypeCode;
+  private long veteranParticipantId;
+  private String eventDetails;
 }
