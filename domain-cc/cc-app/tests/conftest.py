bce50a44b26b3aaf091797de6e8209fcace89920
Luke Short <lukeshortemail@gmail.com>
2023-11-01 17:26:02 -0700

    fix tables still get created for sqlite for pytest

--- domain-cc/cc-app/tests/conftest.py
+++ domain-cc/cc-app/tests/conftest.py
@@ -3,6 +3,7 @@
 import pytest
 from fastapi.testclient import TestClient
 from src.python_src.api import app
+from src.python_src.database import Base, engine
 
 TUBERCULOSIS_CLASSIFICATION = {
     "diagnostic_code": 7710,
@@ -20,7 +21,8 @@ DRUG_INDUCED_PULMONARY_PNEMONIA_CLASSIFICATION = {
     "classification_name": "Respiratory",
 }
 
-SQLALCHEMY_DATABASE_URL = "sqlite://"
+# SQLALCHEMY_DATABASE_URL = "sqlite://"
+Base.metadata.create_all(bind=engine)
 
 
 @pytest.fixture
