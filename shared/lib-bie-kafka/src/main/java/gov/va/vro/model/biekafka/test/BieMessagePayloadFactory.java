16619b24a3d167c77e61a543018de86783e9c379
Chengjie Yin <89034346+chengjie8@users.noreply.github.com>
2023-11-05 17:22:24 -0500

    refactor and populate new fields using faker

--- shared/lib-bie-kafka/src/main/java/gov/va/vro/model/biekafka/test/BieMessagePayloadFactory.java
+++ shared/lib-bie-kafka/src/main/java/gov/va/vro/model/biekafka/test/BieMessagePayloadFactory.java
@@ -3,26 +3,71 @@ package gov.va.vro.model.biekafka.test;
 import gov.va.vro.model.biekafka.BieMessagePayload;
 import gov.va.vro.model.biekafka.ContentionEvent;
 import net.datafaker.Faker;
+import java.util.Random;
 
 import java.util.concurrent.TimeUnit;
 
 public class BieMessagePayloadFactory {
-
+  private static final Random RANDOM = new Random();
   private static final Faker faker = new Faker();
 
-  public static BieMessagePayload create() {
+  private static BieMessagePayload createPayload(ContentionEvent eventType) {
     return BieMessagePayload.builder()
-        .eventType(ContentionEvent.CONTENTION_ASSOCIATED_TO_CLAIM)
-        .claimId(faker.random().nextLong())
-        .contentionClassificationName(faker.lorem().word())
-        .contentionTypeCode(faker.lorem().characters(10))
-        .contentionId(faker.random().nextLong())
-        .diagnosticTypeCode(faker.lorem().characters(10))
-        .occurredAt(faker.date().past(60, TimeUnit.DAYS).getTime())
-        .notifiedAt(faker.date().past(60, TimeUnit.DAYS).getTime())
-        .actionName(faker.lorem().characters(10))
-        .actionResultName(faker.lorem().characters(10))
-        .status(200)
-        .build();
+            .eventType(eventType)
+            .claimId(faker.random().nextLong())
+            .contentionClassificationName(faker.lorem().word())
+            .contentionTypeCode(faker.lorem().characters(10))
+            .contentionId(faker.random().nextLong())
+            .diagnosticTypeCode(faker.lorem().characters(10))
+            .occurredAt(faker.date().past(60, TimeUnit.DAYS).getTime())
+            .notifiedAt(faker.date().past(60, TimeUnit.DAYS).getTime())
+            .actionName(faker.lorem().characters(10))
+            .actionResultName(faker.lorem().characters(10))
+            .status(200)
+            .build();
+  }
+
+  private static void setCommonPayloadValues(BieMessagePayload payload) {
+    payload.setBenefitClaimTypeCode(faker.lorem().characters(10));
+    payload.setActorStation(faker.lorem().characters(200));
+    payload.setAutomationIndicator(faker.bool().bool());
+    payload.setContentionStatusTypeCode(faker.lorem().characters(5));
+    payload.setCurrentLifecycleStatus(faker.lorem().characters(8));
+    payload.setDetails(faker.lorem().characters(200));
+    payload.setEventTime(faker.date().past(60, TimeUnit.DAYS).getTime());
+    payload.setVeteranParticipantId(faker.random().nextLong());
+  }
+
+  private static void setDeletedPayloadValues(BieMessagePayload payload) {
+    payload.setAutomationIndicator(faker.bool().bool());
+    payload.setContentionStatusTypeCode(faker.lorem().characters(5));
+    payload.setCurrentLifecycleStatus(faker.lorem().characters(8));
+    payload.setEventTime(faker.date().past(60, TimeUnit.DAYS).getTime());
+  }
+
+  public static BieMessagePayload create() {
+    // generate a random contention classification event type
+    ContentionEvent eventType = faker.options().option(ContentionEvent.values());
+
+    BieMessagePayload payload = createPayload(eventType);
+
+    switch (eventType) {
+      case CONTENTION_ASSOCIATED_TO_CLAIM:
+      case CONTENTION_CLASSIFIED:
+        setCommonPayloadValues(payload);
+        break;
+      case CONTENTION_UPDATED:
+        setCommonPayloadValues(payload);
+        payload.setJournalStatusTypeCode(faker.lorem().characters(5));
+        break;
+      case CONTENTION_DELETED:
+        setDeletedPayloadValues(payload);
+        break;
+      case CONTENTION_COMPLETED:
+        // No special handling for CONTENTION_COMPLETED
+        break;
+    }
+
+    return payload;
   }
 }
