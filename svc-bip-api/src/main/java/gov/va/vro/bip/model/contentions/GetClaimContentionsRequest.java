3885efb1695043450aa59d0a2b88c112c6883d81
Derek Fitchett <135860892+dfitchett@users.noreply.github.com>
2023-11-06 11:53:31 -0800

    Changes BIP API to implement passthrough behavior. (#2165)
    Resolves #2091 - Changes BIP API to implement passthrough behavior

---
+++ svc-bip-api/src/main/java/gov/va/vro/bip/model/contentions/GetClaimContentionsRequest.java
@@ -0,0 +1,16 @@
+package gov.va.vro.bip.model.contentions;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GetClaimContentionsRequest {
+  @JsonProperty("claimId")
+  private long claimId;
+}
