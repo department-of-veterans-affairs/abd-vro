fece4169aa177aa7b76d3f4cee7b2eb0465ab0c9
Teja <teja@coa.solutions>
2023-11-15 12:52:31 -0500

    spotless fix

--- svc-bip-api/src/integrationTest/java/gov/va/vro/bip/RabbitMqIntegrationTest.java
+++ svc-bip-api/src/integrationTest/java/gov/va/vro/bip/RabbitMqIntegrationTest.java
@@ -23,7 +23,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Arrays;
 import java.util.List;
