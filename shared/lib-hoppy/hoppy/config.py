07c8c4c20f67b2addc7e9ef14eab628375fb04af
Mason Watson <32622851+msnwatson@users.noreply.github.com>
2023-11-17 12:57:13 -0500

    Secrel Issues (#2232)
    * force version in gradle-plugins
    
    * force versions
    
    * rm password
    
    * add dep constraint
    
    * run spotless

--- shared/lib-hoppy/hoppy/config.py
+++ shared/lib-hoppy/hoppy/config.py
@@ -3,7 +3,7 @@ import os
 RABBITMQ_CONFIG = {
     "host": os.environ.get("RABBITMQ_PLACEHOLDERS_HOST") or "localhost",
     "username": os.environ.get("RABBITMQ_USERNAME") or "user",
-    "password": os.environ.get("RABBITMQ_PASSWORD") or "bitnami",
+    "password": os.environ.get("RABBITMQ_PASSWORD"),
     "port": int(os.environ.get("RABBITMQ_PORT") or 5672),
     "retry_limit": int(os.environ.get("RABBITMQ_RETRY_LIMIT") or 3),
     "timeout": int(os.environ.get("RABBITMQ_TIMEOUT") or 60 * 60 * 3),  # 3 hours
