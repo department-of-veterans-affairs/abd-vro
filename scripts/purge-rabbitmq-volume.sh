bda4c5e456d4175198c268fdfedb57393fee51a6
Chengjie Yin <89034346+chengjie8@users.noreply.github.com>
2023-11-17 02:45:15 -0500

    minor change

--- scripts/purge-rabbitmq-volume.sh
+++ scripts/purge-rabbitmq-volume.sh
@@ -1,34 +1,23 @@
 #!/bin/bash
 
-# Define the volume name
-volume_name="vro_var_rabbitmq"
+# Define the service and volume names
+service_name="rabbitmq-service"
+volume_name="var_rabbitmq"
 
-# Get container IDs using the volume
-container_ids=$(docker ps -q --filter volume=$volume_name)
+# Stop the RabbitMQ service using Docker Compose
+echo "Stopping the $service_name service using Docker Compose..."
+docker-compose stop $service_name
 
-# Check if any container IDs were found
-if [ -z "$container_ids" ]; then
-    echo "No container found using volume $volume_name."
-else
-    # Iterate over each container ID
-    for id in $container_ids; do
-        # Get container name
-        container_name=$(docker inspect --format='{{.Name}}' $id | sed 's/^\/\+//')
-
-        # Print container name
-        echo "Forcefully stopping container $container_name ($id) using volume $volume_name..."
-
-        # Forcefully stop the container
-        docker kill $id
-    done
-fi
-
-# Remove the volume
+# Remove the existing Docker volume (caution: this will delete all data in the volume)
 echo "Removing volume $volume_name..."
 docker volume rm $volume_name
 
-# Optionally, recreate the volume
+# Recreate the Docker volume
 echo "Recreating volume $volume_name..."
 docker volume create $volume_name
+
+# Restart the RabbitMQ service using Docker Compose
+echo "Restarting the $service_name service..."
+docker-compose up -d $service_name
 
 echo "Script completed."
