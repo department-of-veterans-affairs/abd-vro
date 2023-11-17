f0b19f411487e6276abaa1b2d994013dd52d266a
Chengjie Yin <89034346+chengjie8@users.noreply.github.com>
2023-11-17 03:00:56 -0500

    test

--- scripts/purge-rabbitmq-volume.sh
+++ scripts/purge-rabbitmq-volume.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-# Define the service and volume names
+# Define the service name
 service_name="rabbitmq-service"
-volume_name="vro_var_rabbitmq"
 
 # Stop the RabbitMQ service using Docker Compose
 echo "Stopping the $service_name service using Docker Compose..."
 docker-compose stop $service_name
 
-# Remove the existing Docker volume (caution: this will delete all data in the volume)
-echo "Removing volume $volume_name..."
-docker volume rm $volume_name
-
-# Recreate the Docker volume
-echo "Recreating volume $volume_name..."
-docker volume create $volume_name
+# Remove all unused volumes (caution: this will delete data in all unused volumes)
+echo "Pruning unused Docker volumes..."
+docker volume prune -f
 
 # Restart the RabbitMQ service using Docker Compose
 echo "Restarting the $service_name service..."
