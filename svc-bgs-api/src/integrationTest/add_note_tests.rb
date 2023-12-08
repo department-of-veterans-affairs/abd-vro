d8ef05f4d4f431e14662853a759c08977d825cbe
Mason Watson <masonwatson@navapbc.com>
2023-12-08 13:04:54 -0800

    integration tests as a gradle task

--- svc-bgs-api/src/integrationTest/add_note_tests.rb
+++ svc-bgs-api/src/integrationTest/add_note_tests.rb
@@ -8,28 +8,27 @@ require_relative '../config/constants'
 # Constants used by integration tests
 ADD_NOTE_REPLY_QUEUE = "add-note-response"
 ADD_NOTE_PAYLOAD = "{\\\"veteranNote\\\":\\\"test\\\",\\\"veteranParticipantId\\\":111}"
+CORRELATION_ID = "1234"
 
-def test_successful_add_note()
+
+def test_successful_add_note(ch)
+    x = ch.direct(BGS_EXCHANGE_NAME, CAMEL_MQ_PROPERTIES)
     r = ch.queue(ADD_NOTE_REPLY_QUEUE, CAMEL_MQ_PROPERTIES).bind(x, :routing_key => ADD_NOTE_REPLY_QUEUE)
 
-    q = ch.queue(ADD_NOTE, CAMEL_MQ_PROPERTIES)
-    q.publish(ADD_NOTE_PAYLOAD, :reply_to => ADD_NOTE_REPLY_QUEUE, :delivery_mode => 1, :correlation_id => ${{env.CORRELATION_ID}}, :payload_encoding => string)
+    q = ch.queue(ADD_NOTE_QUEUE, CAMEL_MQ_PROPERTIES)
+    q.publish(ADD_NOTE_PAYLOAD, :reply_to => ADD_NOTE_REPLY_QUEUE, :delivery_mode => 1, :correlation_id => CORRELATION_ID, :payload_encoding => "string")
 
     # TODO: check to make sure there is a message in the reply queue before popping
 
     delivery_info, properties, payload = r.pop
 
     # Validate response from the add-note message processing
+    response_correlation_id = properties.correlation_id
 
-    echo "Checking correlation_id..."
-    echo "correlation_id: ${{ fromJson(steps.addNoteResponse.outputs.response).data[0].properties.correlation_id }}"
+    puts "Checking correlation_id..."
+    puts "correlation_id: #{response_correlation_id}"
 
-    if properties.correlation_id != ${{env.CORRELATION_ID}} then
-        raise "Unexpected correlation_id: Expected ${{env.CORRELATION_ID}}. Found ${{properties.correlation_id}} "
+    if response_correlation_id != CORRELATION_ID then
+        raise "Unexpected correlation_id: Expected #{CORRELATION_ID}. Found #{response_correlation_id} "
     end
-
-    # TODO: validate response payload
-    # if JSON.parse(payload)["statusCode"] != 200 then
-    #     raise "svc-bgs-api healthcheck failed"
-    # end
 end
--- svc-bgs-api/src/integrationTest/before_integration_test.rb
+++ svc-bgs-api/src/integrationTest/before_integration_test.rb
@@ -1,7 +1,8 @@
 require_relative '../config/constants'
 
-conn = Bunny.new(BUNNY_ARGS)
-conn.start
+def before_integration_test()
+    conn = Bunny.new(BUNNY_ARGS)
+    conn.start
 
-ch = conn.create_channel
-x = ch.direct(BGS_EXCHANGE_NAME, CAMEL_MQ_PROPERTIES)
+    return conn.create_channel
+end
