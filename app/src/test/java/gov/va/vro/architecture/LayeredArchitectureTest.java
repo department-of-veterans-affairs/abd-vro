76ac7df5c01a9dc55f34f5365592dad6a49fee9f
Mason Watson <masonwatson@navapbc.com>
2023-12-11 17:33:44 -0800

    run linter

--- app/src/test/java/gov/va/vro/architecture/LayeredArchitectureTest.java
+++ app/src/test/java/gov/va/vro/architecture/LayeredArchitectureTest.java
@@ -1,11 +1,12 @@
 package gov.va.vro.architecture;
 
-import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
-
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.core.importer.ImportOption.Predefined;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 
 public class LayeredArchitectureTest {
   private final String packageName = "gov.va";
@@ -14,38 +15,37 @@ public class LayeredArchitectureTest {
           .withImportOption(Predefined.DO_NOT_INCLUDE_TESTS)
           .importPackages(packageName);
 
-//  @Test
-//  public void layerDependenciesAreRespected() {
-//    layeredArchitecture()
-//        .layer("API")
-//        .definedBy(packageName + "..api..")
-//        .optionalLayer("API-Requests")
-//        .definedBy(packageName + "..api..requests..")
-//        .optionalLayer("API-Responses")
-//        .definedBy(packageName + "..api..responses..")
-////        .layer("Controllers")
-////        .definedBy(packageName + "..controller..")
-//        .optionalLayer("SPI")
-//        .definedBy(packageName + "..service.spi..")
-//        .optionalLayer("Services")
-//        .definedBy(packageName + "..service.provider..", "..service.rrd.db..", "..service.event..")
-//        .layer("Persistence")
-//        .definedBy(packageName + "..persistence..")
-////        .whereLayer("API")
-////        .mayOnlyBeAccessedByLayers("Controllers")
-//        .whereLayer("API-Requests")
-//        .mayOnlyBeAccessedByLayers("API")
-////            .mayOnlyBeAccessedByLayers("Controllers", "API")
-//        .whereLayer("API-Responses")
-//        .mayOnlyBeAccessedByLayers("API")
-////        .mayOnlyBeAccessedByLayers("Controllers", "API")
-////        .whereLayer("Controllers")
-////        .mayNotBeAccessedByAnyLayer()
-//        .whereLayer("SPI")
-//        .mayOnlyBeAccessedByLayers("Services")
-////        .mayOnlyBeAccessedByLayers("Controllers", "Services")
-//        .whereLayer("Persistence")
-//        .mayOnlyBeAccessedByLayers("Services")
-//        .check(classes);
-//  }
+    @Test
+    @Disabled
+    public void layerDependenciesAreRespected() {
+      layeredArchitecture()
+          .layer("API")
+          .definedBy(packageName + "..api..")
+          .optionalLayer("API-Requests")
+          .definedBy(packageName + "..api..requests..")
+          .optionalLayer("API-Responses")
+          .definedBy(packageName + "..api..responses..")
+          .layer("Controllers")
+          .definedBy(packageName + "..controller..")
+          .optionalLayer("SPI")
+          .definedBy(packageName + "..service.spi..")
+          .optionalLayer("Services")
+          .definedBy(packageName + "..service.provider..", "..service.rrd.db..",
+   "..service.event..")
+          .layer("Persistence")
+          .definedBy(packageName + "..persistence..")
+          .whereLayer("API")
+          .mayOnlyBeAccessedByLayers("Controllers")
+          .whereLayer("API-Requests")
+          .mayOnlyBeAccessedByLayers("Controllers", "API")
+          .whereLayer("API-Responses")
+          .mayOnlyBeAccessedByLayers("Controllers", "API")
+          .whereLayer("Controllers")
+          .mayNotBeAccessedByAnyLayer()
+          .whereLayer("SPI")
+          .mayOnlyBeAccessedByLayers("Controllers", "Services")
+          .whereLayer("Persistence")
+          .mayOnlyBeAccessedByLayers("Services")
+          .check(classes);
+    }
 }
