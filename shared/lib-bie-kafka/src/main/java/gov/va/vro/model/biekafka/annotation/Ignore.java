7bb680686cc4b0eb0094408ae1a260b85cbdd9ac
Chengjie Yin <89034346+chengjie8@users.noreply.github.com>
2023-12-15 23:53:34 -0500

    minor change

--- shared/lib-bie-kafka/src/main/java/gov/va/vro/model/biekafka/annotation/Ignore.java
+++ shared/lib-bie-kafka/src/main/java/gov/va/vro/model/biekafka/annotation/Ignore.java
@@ -4,7 +4,10 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
+/**
+ * This annotation is used to mark fields that should be ignored when fetching
+ * from a GenericRecord.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Ignore {
