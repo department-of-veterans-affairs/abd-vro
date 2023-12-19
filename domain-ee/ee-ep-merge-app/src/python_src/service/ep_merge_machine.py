6e5d4bd3c31a3f54912487d65db2d5fa1038bde7
dfitchett <135860892+dfitchett@users.noreply.github.com>
2023-12-19 13:35:41 -0800

    Missed method refactor

--- domain-ee/ee-ep-merge-app/src/python_src/service/ep_merge_machine.py
+++ domain-ee/ee-ep-merge-app/src/python_src/service/ep_merge_machine.py
@@ -91,7 +91,7 @@ class EpMergeMachine(StateMachine):
         if response is not None and response.status_code == 200:
             if response.claim is None or response.claim.end_product_code is None:
                 logging.info(self.job.state)
-                self.log_error(f"Pending claim #{self.job.pending_claim_id} does not have an end product code")
+                self.add_error(f"Pending claim #{self.job.pending_claim_id} does not have an end product code")
             else:
                 self.cancellation_reason = CANCELLATION_REASON_FORMAT.format(ep_code=response.claim.end_product_code,
                                                                              claim_id=self.job.pending_claim_id)
