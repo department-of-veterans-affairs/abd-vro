7d8a3e332c0a576a1eb05093b34f5f767ea43eab
dfitchett <135860892+dfitchett@users.noreply.github.com>
2023-12-28 11:44:30 -0800

    Added postgres schema env var

--- domain-ee/ee-ep-merge-app/src/python_src/model/merge_job.py
+++ domain-ee/ee-ep-merge-app/src/python_src/model/merge_job.py
@@ -1,3 +1,4 @@
+from config import POSTGRES_SCHEMA
 from db.base_class import Base
 from sqlalchemy import Column, DateTime, Integer, String
 from sqlalchemy.dialects.postgresql import ARRAY, JSONB, UUID
@@ -7,7 +8,7 @@ from sqlalchemy.sql import func
 
 class MergeJob(Base):
     __tablename__ = "merge_jobs"
-    __table_args__ = {"schema": "claims"}
+    __table_args__ = {"schema": POSTGRES_SCHEMA}
 
     job_id = Column(UUID(as_uuid=True), primary_key=True, index=True)
     pending_claim_id = Column(Integer, index=True)
