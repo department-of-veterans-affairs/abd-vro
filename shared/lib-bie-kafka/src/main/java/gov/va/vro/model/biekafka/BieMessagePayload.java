13bd3bc229b063535566dcc078de8a3654ea7d76
Cheng <89034346+chengjie8@users.noreply.github.com>
2024-01-03 15:07:11 -0500

    Introduce annotations to handle the fields exclusive to only certain kafka topic names (#2368)
    * added new annotations and introduced additional code in BieMessageUtils class to utilize the annotations
    
    * minor change
    
    * lint and unit test fixes
    
    * update group id to re-fetch all topic message for testing
    
    * added appropriate test coverge for various kafka topics and their exclusive fields
    
    * lint

--- shared/lib-bie-kafka/src/main/java/gov/va/vro/model/biekafka/BieMessagePayload.java
+++ shared/lib-bie-kafka/src/main/java/gov/va/vro/model/biekafka/BieMessagePayload.java
@@ -1,6 +1,8 @@
 package gov.va.vro.model.biekafka;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import gov.va.vro.model.biekafka.annotation.Ignore;
+import gov.va.vro.model.biekafka.annotation.TargetEvents;
 import lombok.*;
 
 @Setter
@@ -10,31 +12,80 @@ import lombok.*;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @EqualsAndHashCode
 public class BieMessagePayload {
-  // these are vro fields
-  private Integer status;
-  private String statusMessage;
-  private ContentionEvent eventType;
-  private Long notifiedAt;
+  // These are VRO fields
+  @Ignore private Integer status;
+  @Ignore private String statusMessage;
+  @Ignore private ContentionEvent eventType;
+  @Ignore private Long notifiedAt;
+  @Ignore private String description;
 
-  // populated from kafka topic payload
-  private String benefitClaimTypeCode;
-  private String actorStation;
-  private String details;
-  private Long veteranParticipantId;
+  // Fields without the @TargetEvents annotation are included in all five Kafka topics
   private Long claimId;
   private Long contentionId;
-  private String contentionTypeCode;
-  private String contentionClassificationName;
-  private String diagnosticTypeCode;
   private String actionName;
   private String actionResultName;
   private Boolean automationIndicator;
+  private String contentionTypeCode;
   private String contentionStatusTypeCode;
   private String currentLifecycleStatus;
   private Long eventTime;
-  private String description;
+
+  // populated from kafka topic payload
+  @TargetEvents({
+    "CONTENTION_BIE_CONTENTION_ASSOCIATED_TO_CLAIM_V02",
+    "CONTENTION_BIE_CONTENTION_CLASSIFIED_V02",
+    "CONTENTION_BIE_CONTENTION_UPDATED_V02"
+  })
+  private String benefitClaimTypeCode;
+
+  @TargetEvents({
+    "CONTENTION_BIE_CONTENTION_ASSOCIATED_TO_CLAIM_V02",
+    "CONTENTION_BIE_CONTENTION_CLASSIFIED_V02",
+    "CONTENTION_BIE_CONTENTION_UPDATED_V02"
+  })
+  private String actorStation;
+
+  @TargetEvents({
+    "CONTENTION_BIE_CONTENTION_ASSOCIATED_TO_CLAIM_V02",
+    "CONTENTION_BIE_CONTENTION_CLASSIFIED_V02",
+    "CONTENTION_BIE_CONTENTION_UPDATED_V02"
+  })
+  private String details;
+
+  @TargetEvents({
+    "CONTENTION_BIE_CONTENTION_ASSOCIATED_TO_CLAIM_V02",
+    "CONTENTION_BIE_CONTENTION_CLASSIFIED_V02",
+    "CONTENTION_BIE_CONTENTION_UPDATED_V02"
+  })
+  private Long veteranParticipantId;
+
+  @TargetEvents({
+    "CONTENTION_BIE_CONTENTION_ASSOCIATED_TO_CLAIM_V02",
+    "CONTENTION_BIE_CONTENTION_CLASSIFIED_V02",
+    "CONTENTION_BIE_CONTENTION_UPDATED_V02"
+  })
+  private String contentionClassificationName;
+
+  @TargetEvents({
+    "CONTENTION_BIE_CONTENTION_ASSOCIATED_TO_CLAIM_V02",
+    "CONTENTION_BIE_CONTENTION_CLASSIFIED_V02",
+    "CONTENTION_BIE_CONTENTION_UPDATED_V02"
+  })
+  private String diagnosticTypeCode;
+
+  @TargetEvents({"CONTENTION_BIE_CONTENTION_UPDATED_V02"})
   private String journalStatusTypeCode;
+
+  @TargetEvents({
+    "CONTENTION_BIE_CONTENTION_ASSOCIATED_TO_CLAIM_V02",
+    "CONTENTION_BIE_CONTENTION_CLASSIFIED_V02",
+    "CONTENTION_BIE_CONTENTION_UPDATED_V02"
+  })
   private Long dateAdded;
+
+  @TargetEvents({"CONTENTION_BIE_CONTENTION_UPDATED_V02"})
   private Long dateCompleted;
+
+  @TargetEvents({"CONTENTION_BIE_CONTENTION_UPDATED_V02"})
   private Long dateUpdated;
 }
