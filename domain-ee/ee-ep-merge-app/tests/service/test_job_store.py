7b55def1f49769196d3b260c08426c1a40ef8857
Derek Fitchett <135860892+dfitchett@users.noreply.github.com>
2024-01-04 15:01:20 -0800

    EP Merge App: paginate get merge jobs endpoint (#2415)
    * Paginate merge endpoint to return paginated jobs by state

--- domain-ee/ee-ep-merge-app/tests/service/test_job_store.py
+++ domain-ee/ee-ep-merge-app/tests/service/test_job_store.py
@@ -1,5 +1,11 @@
+import pytest
 from model.merge_job import MergeJob
+from schema.merge_job import JobState
 from src.python_src.service.job_store import JobStore
+
+DEFAULT_STATES = JobState.incomplete_states()
+DEFAULT_OFFSET = 1
+DEFAULT_LIMIT = 10
 
 
 def test_reinitialize_in_progress_jobs(db):
@@ -48,3 +54,40 @@ def test_update_merge_job(db, merge_job):
     assert updated_merge_job.ep400_claim_id == merge_job.ep400_claim_id
     assert updated_merge_job.state == merge_job.state
     db.update.assert_called_once()
+
+
+@pytest.mark.parametrize("states,offset,limit",
+                         [
+                             pytest.param(None, None, None, id="defaults"),
+                             pytest.param([JobState.PENDING], 1, None, id="first page, default 10 items"),
+                             pytest.param([JobState.PENDING], None, 1, id="first page, default offset, 1 item"),
+                             pytest.param([JobState.PENDING], 1, 10, id="last page, 1 item"),
+                             pytest.param([JobState.PENDING], 1, 2, id="first page, 2 items"),
+                             pytest.param([JobState.PENDING], 6, 2, id="last page, 2 items"),
+                         ])
+def test_query(db, merge_job, states: list, offset: int, limit: int):
+    job_store = JobStore(db)
+    kwargs = {}
+    if states:
+        kwargs["states"] = states
+    if offset:
+        kwargs['offset'] = offset
+    if limit:
+        kwargs['limit'] = limit
+
+    job_store.query(**kwargs)
+
+    query_args = db.query.call_args[0]
+    actual_model = query_args[0]
+    actual_order_by = query_args[1]
+    actual_filter = query_args[2]
+    actual_offset = query_args[3]
+    actual_limit = query_args[4]
+
+    assert actual_model == MergeJob
+    assert actual_order_by == MergeJob.updated_at
+    assert actual_filter.compare(MergeJob.state.in_(states if states else DEFAULT_STATES))
+    assert actual_offset == offset if offset else DEFAULT_OFFSET
+    assert actual_limit == limit if limit else DEFAULT_LIMIT
+
+    db.query.assert_called_once()
--- domain-ee/ee-ep-merge-app/tests/test_api.py
+++ domain-ee/ee-ep-merge-app/tests/test_api.py
@@ -30,7 +30,6 @@ def mock_job_store(mocker):
     return mocker.patch('src.python_src.api.job_store', return_value=Mock())
 
 
-@pytest.fixture(autouse=True)
 def submitted_job():
     return MergeJob(
         job_id=JOB_ID,
@@ -42,8 +41,8 @@ def submitted_job():
 
 
 @pytest.fixture(autouse=True)
-def mock_job_submit(mock_job_store, submitted_job):
-    mock_job_store.submit_merge_job.return_value = submitted_job
+def mock_job_submit(mock_job_store):
+    mock_job_store.submit_merge_job.return_value = submitted_job()
 
 
 def test_health(client: TestClient):
@@ -98,9 +97,9 @@ def test_get_job_by_job_id_job_not_found(client: TestClient, mock_job_store):
     assert response.status_code == 404
 
 
-def test_get_job_by_job_id_job_found(client: TestClient, mock_job_store, submitted_job):
+def test_get_job_by_job_id_job_found(client: TestClient, mock_job_store):
     job_id = make_merge_request(client)
-    mock_job_store.get_merge_job.return_value = submitted_job
+    mock_job_store.get_merge_job.return_value = submitted_job()
 
     response = client.get(MERGE + f'/{job_id}')
     assert response.status_code == 200
@@ -123,20 +122,41 @@ def make_merge_request(client: TestClient):
     return job_id
 
 
-def test_get_all_jobs_in_progress(client: TestClient, mock_job_store, submitted_job):
-    expected_job_ids = [make_merge_request(client), make_merge_request(client)]
-    expected_jobs = [submitted_job, submitted_job]
-    mock_job_store.get_merge_jobs_in_progress = Mock(return_value=expected_jobs)
+@pytest.mark.parametrize("page,size",
+                         [
+                             pytest.param(None, None, id="defaults"),
+                             pytest.param(1, None, id="first page, 10 items"),
+                             pytest.param(1, None, id="last page, 1 item"),
+                             pytest.param(1, 2, id="first page, 2 items"),
+                             pytest.param(6, 2, id="last page, 2 items"),
+                         ])
+def test_get_merge_jobs_pagination(client: TestClient, mock_job_store, page, size):
+    # Set defaults for missing values:
+    expected_page = page if page else 1
+    expected_size = size if size else 10
 
-    response = client.get(MERGE)
+    expected_jobs = [submitted_job() for i in range(expected_size)]
+    mock_job_store.query = Mock(return_value=(expected_jobs, 11))
+
+    params = {}
+    if page:
+        params['page'] = page
+    if size:
+        params['size'] = size
+
+    response = client.get(MERGE, params=params)
     assert response.status_code == 200
 
     response_json = response.json()
+    assert response_json['total'] == 11
+    assert response_json['states'] == JobState.incomplete_states()
+    assert response_json['page'] == expected_page
+    assert response_json['size'] == expected_size
     results = response_json['jobs']
-    assert len(results) == len(expected_job_ids)
+    assert len(results) == len(expected_jobs)
 
     for job in results:
-        assert job['job_id'] in expected_job_ids
+        assert job['job_id'] == str(JOB_ID)
         assert job['pending_claim_id'] == 1
         assert job['ep400_claim_id'] == 2
         assert job['state'] == JobState.PENDING.value
