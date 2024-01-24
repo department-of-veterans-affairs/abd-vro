146fb308a02d5a53caca060ddd0b0e63e4fbb785
Chengjie Yin <89034346+chengjie8@users.noreply.github.com>
2024-01-24 12:26:38 -0500

    use entrypoint.sh file

--- rabbitmq/src/main/resources/entrypoint.sh
+++ rabbitmq/src/main/resources/entrypoint.sh
@@ -2,14 +2,16 @@
 set -e
 
 # Check if the environment variables are set
-if [ -z "${RABBITMQ_USER}" ] || [ -z "${RABBITMQ_PASSWORD}" ]; then
+if [ -z "${RABBITMQ_USERNAME}" ] || [ -z "${RABBITMQ_PASSWORD}" ]; then
     echo "Required environment variables are missing."
     exit 1
 fi
 
 # Update rabbitmq.conf with environment variable values
-echo "default_user = ${RABBITMQ_USER}" >> /etc/rabbitmq/rabbitmq.conf
-echo "default_pass = ${RABBITMQ_PASSWORD}" >> /etc/rabbitmq/rabbitmq.conf
+echo "default_user = ${RABBITMQ_USERNAME}" >> /tmp/rabbitmq.conf
+echo "default_pass = ${RABBITMQ_PASSWORD}" >> /tmp/rabbitmq.conf
+
+cp /tmp/rabbitmq.conf /etc/rabbitmq/rabbitmq.conf
 
 
 # Start RabbitMQ
