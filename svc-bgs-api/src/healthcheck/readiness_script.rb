e9d405c5b6ee15f2b24605fabd6569fc3325ec60
Ponnia <151670616+Ponnia-M@users.noreply.github.com>
2024-01-29 16:20:57 -0500

    Update readiness_script.rb

--- svc-bgs-api/src/healthcheck/readiness_script.rb
+++ svc-bgs-api/src/healthcheck/readiness_script.rb
@@ -7,14 +7,16 @@ require_relative 'lib/bgs_client'
 def rabbitmq_connection_active?
   subscriber = RabbitSubscriber.new(BUNNY_ARGS)
   subscriber.connected?
-rescue
+rescue StandardError => e
+  puts "RabbitMQ connection check failed: #{e.message}"
   false
 end
 
 def bgs_service_available?
   client = BgsClient.new
   client.check_bgs_availability
-rescue
+rescue StandardError => e
+  puts "BGS service availability check failed: #{e.message}"
   false
 end
 
