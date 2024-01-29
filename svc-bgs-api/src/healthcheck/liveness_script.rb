a0691373f89cf6a8b02c44281ede7ae24ec734e9
Ponnia <151670616+Ponnia-M@users.noreply.github.com>
2024-01-29 17:34:41 -0500

    Bgs liveness (#2508)
    * Add Liveness and Readiness for BGS-api
    
    * Add Liveness and Readiness for BGS-api #2417
    
    - Add liveness_script.rb for liveness probe
    - Add readiness_script for readiness probe
    - Update values.yaml
    - Update chart.yaml version
    - Update Dockerfile
    
    * Update path in values.yaml for BGS-api
    
    * Update liveness_script.rb
    
    * Update readiness_script.rb
    
    ---------
    
    Co-authored-by: Teja <teja@coa.solutions>

---
+++ svc-bgs-api/src/healthcheck/liveness_script.rb
@@ -0,0 +1,19 @@
+# This script is used by the K8S Liveness probe to check the connection between RabbitMQ and BGS-api thereby verifying 
+# that the BGS-api application is running and able to perform its basic functions. 
+
+require_relative 'rabbit_subscriber'
+
+def check_rabbitmq_connection
+  subscriber = RabbitSubscriber.new(BUNNY_ARGS)
+  subscriber.connected?
+rescue StandardError => e
+  puts "RabbitMQ connection check failed: #{e.message}"
+  false
+end
+
+if check_rabbitmq_connection
+  exit 0
+else
+  puts "Liveness check failed"
+  exit 1
+end
