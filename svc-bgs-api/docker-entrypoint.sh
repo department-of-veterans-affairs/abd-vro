d2c7f60517c4d035047556c7d7314a2f1f4835dd
Ponnia Muyen <ponnia.muyen@agile6.com>
2024-01-31 18:14:29 -0500

    Commenting openssl command lines

--- svc-bgs-api/docker-entrypoint.sh
+++ svc-bgs-api/docker-entrypoint.sh
@@ -15,18 +15,18 @@ cd "$CERTS_PATH" || exit 10
 [ "$BIP_PASSWORD" ] && { : ${PWD_ARG:=-password pass:$BIP_PASSWORD}; }
 
 # Checks that openssl is installed
-openssl version
+# openssl version
 
 [ "$BIP_KEYSTORE" ] && [ "$BIP_TRUSTSTORE" ] && {
     echo "$BIP_KEYSTORE"   | base64 -d > keystore.p12
     echo "$BIP_TRUSTSTORE" | base64 -d > truststore.p12
 
     # shellcheck disable=SC2086
-    openssl pkcs12 ${PWD_ARG} -in keystore.p12   -out tls_bip.crt -nokeys || exit 2
-    # shellcheck disable=SC2086
-    openssl pkcs12 ${PWD_ARG} -in keystore.p12   -out tls.key -nocerts -nodes || exit 3
-    # shellcheck disable=SC2086
-    openssl pkcs12 ${PWD_ARG} -in truststore.p12 -out va_all.crt || exit 4
+    # openssl pkcs12 ${PWD_ARG} -in keystore.p12   -out tls_bip.crt -nokeys || exit 2
+    # # shellcheck disable=SC2086
+    # openssl pkcs12 ${PWD_ARG} -in keystore.p12   -out tls.key -nocerts -nodes || exit 3
+    # # shellcheck disable=SC2086
+    # openssl pkcs12 ${PWD_ARG} -in truststore.p12 -out va_all.crt || exit 4
 }
 
 cd "$CURR_DIR" || exit 11
