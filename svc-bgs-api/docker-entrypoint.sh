38bd8ba812057a60f3c7fb0e637b4d51ee7a923c
Ponnia <151670616+Ponnia-M@users.noreply.github.com>
2024-02-01 10:52:51 -0500

    Commenting openssl command lines (#2523)
    * Commenting openssl command lines
    
    * Remove openssl command lines

--- svc-bgs-api/docker-entrypoint.sh
+++ svc-bgs-api/docker-entrypoint.sh
@@ -14,19 +14,9 @@ cd "$CERTS_PATH" || exit 10
 # shellcheck disable=SC2223
 [ "$BIP_PASSWORD" ] && { : ${PWD_ARG:=-password pass:$BIP_PASSWORD}; }
 
-# Checks that openssl is installed
-openssl version
-
 [ "$BIP_KEYSTORE" ] && [ "$BIP_TRUSTSTORE" ] && {
     echo "$BIP_KEYSTORE"   | base64 -d > keystore.p12
     echo "$BIP_TRUSTSTORE" | base64 -d > truststore.p12
-
-    # shellcheck disable=SC2086
-    openssl pkcs12 ${PWD_ARG} -in keystore.p12   -out tls_bip.crt -nokeys || exit 2
-    # shellcheck disable=SC2086
-    openssl pkcs12 ${PWD_ARG} -in keystore.p12   -out tls.key -nocerts -nodes || exit 3
-    # shellcheck disable=SC2086
-    openssl pkcs12 ${PWD_ARG} -in truststore.p12 -out va_all.crt || exit 4
 }
 
 cd "$CURR_DIR" || exit 11
