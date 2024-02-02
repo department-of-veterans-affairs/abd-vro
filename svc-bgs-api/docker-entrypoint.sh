4c0a0b1c29276d5bf9d827b9277ab842177599b9
Mason Watson <masonwatson@navapbc.com>
2024-02-02 14:09:28 -0800

    restore openssl commands for testing

--- svc-bgs-api/docker-entrypoint.sh
+++ svc-bgs-api/docker-entrypoint.sh
@@ -17,17 +17,17 @@ cd "$CERTS_PATH" || exit 10
 # Checks that openssl is installed
 openssl version
 
-#[ "$BIP_KEYSTORE" ] && [ "$BIP_TRUSTSTORE" ] && {
-#    echo "$BIP_KEYSTORE"   | base64 -d > keystore.p12
-#    echo "$BIP_TRUSTSTORE" | base64 -d > truststore.p12
-#
-#    # shellcheck disable=SC2086
-#    openssl pkcs12 ${PWD_ARG} -in keystore.p12   -out tls_bip.crt -nokeys || exit 2
-#    # shellcheck disable=SC2086
-#    openssl pkcs12 ${PWD_ARG} -in keystore.p12   -out tls.key -nocerts -nodes || exit 3
-#    # shellcheck disable=SC2086
-#    openssl pkcs12 ${PWD_ARG} -in truststore.p12 -out va_all.crt || exit 4
-#}
+[ "$BIP_KEYSTORE" ] && [ "$BIP_TRUSTSTORE" ] && {
+    echo "$BIP_KEYSTORE"   | base64 -d > keystore.p12
+    echo "$BIP_TRUSTSTORE" | base64 -d > truststore.p12
+
+    # shellcheck disable=SC2086
+    openssl pkcs12 ${PWD_ARG} -in keystore.p12   -out tls_bip.crt -nokeys || exit 2
+    # shellcheck disable=SC2086
+    openssl pkcs12 ${PWD_ARG} -in keystore.p12   -out tls.key -nocerts -nodes || exit 3
+    # shellcheck disable=SC2086
+    openssl pkcs12 ${PWD_ARG} -in truststore.p12 -out va_all.crt || exit 4
+}
 
 cd "$CURR_DIR" || exit 11
 
