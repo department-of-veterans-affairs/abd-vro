060195a921a9053700858298616a9a66b41bcbbe
Luke Short <lukeshortemail@gmail.com>
2024-02-05 15:40:34 -0800

    troubleshoot @root_validator failing

--- domain-cc/cc-app/src/python_src/pydantic_models.py
+++ domain-cc/cc-app/src/python_src/pydantic_models.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import BaseModel, model_validator
+from pydantic import BaseModel, root_validator
 
 
 class Claim(BaseModel):
@@ -10,7 +10,7 @@ class Claim(BaseModel):
     claim_type: str = "claim_for_increase"
     contention_text: Optional[str]  # marked optional to retain compatibility with v1
 
-    @model_validator(mode="before")
+    @root_validator(pre="before")
     def check_dc_for_cfi(cls, values):
         claim_type = values.get("claim_type")
         diagnostic_code = values.get("diagnostic_code")
