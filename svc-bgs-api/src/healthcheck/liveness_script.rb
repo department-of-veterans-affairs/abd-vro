acc1ba93ab0a482e6ce9e57befd51377fca078df
Ponnia <151670616+Ponnia-M@users.noreply.github.com>
2024-02-05 13:42:24 -0500

    Final Fix to svc-bgs-api K8s readiness probe (2417) (#2539)
    * Update gemfile
    
    * Update bgs path in gemfile
    
    * Update gemfile
    
    * Fix readiness probe for svc-bgs-api
    
    * Add K8s startup probe for svc-bgs-api
    
    * Update startup_script.rb
    
    * Update readiness_script.rb

--- svc-bgs-api/src/healthcheck/liveness_script.rb
+++ svc-bgs-api/src/healthcheck/liveness_script.rb
@@ -1,20 +1,33 @@
 # This script is used by the K8S Liveness probe to check the connection between RabbitMQ and BGS-api thereby verifying 
 # that the BGS-api application is running and able to perform its basic functions. 
 
+require 'logger'
 require_relative '../lib/rabbit_subscriber'
 require_relative '../config/constants'
 
-def check_rabbitmq_connection
-  subscriber = RabbitSubscriber.new(BUNNY_ARGS)
-  subscriber.rabbitmq_connected?
-rescue StandardError => e
-  puts "RabbitMQ connection check failed: #{e.message}"
-  false
+# Initializes logger
+$logger = Logger.new(STDOUT)
+$logger.level = Logger::INFO
+$logger.info('Logger initialized')
+
+def log(message)
+  $logger.info("[Liveness Check]: #{message}")
 end
 
+def check_rabbitmq_connection
+  subscriber = RabbitSubscriber.new(BUNNY_ARGS)
+  connected = subscriber.rabbitmq_connected?
+  log "RabbitMQ connectivity check: #{connected ? 'Success' : 'Failure'}"
+  connected
+  rescue => e
+    log "RabbitMQ check failed: #{e.message}"
+    false
+  end
+
 if check_rabbitmq_connection
-  exit 0
+  log "Liveness check passed!"
+  exit(0)
 else
-  puts "Liveness check failed"
-  exit 1
+  log "Liveness check failed!"
+  exit(1)
 end
