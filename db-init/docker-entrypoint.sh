4e865299ee0badf9520defba3d9fd8e73d3ae4f2
Teja <teja@coa.solutions>
2024-02-16 12:00:03 -0500

    export flyway url

--- db-init/docker-entrypoint.sh
+++ db-init/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Assuming POSTGRES_URL is passed in without the jdbc: prefix
 # and construct FLYWAY_URL with the prefix.
-FLYWAY_URL="jdbc:$POSTGRES_URL"
+export FLYWAY_URL="jdbc:$POSTGRES_URL"
 
 # Poll to see if DB instance is ready
 until pg_isready -d $POSTGRES_DB -h $POSTGRES_URL -p 5432 -U $POSTGRES_FLYWAY_USER; do
