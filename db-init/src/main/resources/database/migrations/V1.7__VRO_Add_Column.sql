b1d0bfa72c2431fa3bc64940702792c61866d245
Teja <teja@coa.solutions>
2024-02-21 14:22:37 -0500

    reset columns, image versions

--- db-init/src/main/resources/database/migrations/V1.7__VRO_Add_Column.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "bie_contention_event"
-ADD COLUMN "hello_world" VARCHAR(255) DEFAULT NULL;
--- db-init/src/main/resources/database/migrations/V1.8__VRO_Test_Column.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "bie_contention_event"
-ADD COLUMN "test" VARCHAR(255) DEFAULT NULL;
