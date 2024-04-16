4ade254a2fff0579be2262bd3540d22fe9c67ab3
Mason Watson <masonwatson@navapbc.com>
2024-04-16 11:39:21 -0700

    add back double bracing

--- scripts/secrel-secrel-inputs.sh
+++ scripts/secrel-secrel-inputs.sh
@@ -6,23 +6,23 @@ source scripts/image_vars.src
 IMAGE_LIST="[]"
 
 # This case captures manual dispatches where publishing is disabled
-if [ "${needs.publish-to-ghcr.result}" == 'skipped' ]; then
+if [ "${{needs.publish-to-ghcr.result}}" == 'skipped' ]; then
 
   IMAGE_LIST="["
 
-  if [ "${inputs.image_name}" == 'all' ]; then
+  if [ "${{inputs.image_name}}" == 'all' ]; then
     for PREFIX in "${VAR_PREFIXES_ARR[@]}"; do
       IMG_NAME="$(getVarValue "${PREFIX}" _IMG)"
-      GHCR_PATH="ghcr.io/${github.repository}/${IMG_NAME}"
+      GHCR_PATH="ghcr.io/${{github.repository}}/${IMG_NAME}"
       IMAGE_LIST+="\"${GHCR_PATH}:latest\","
     done
 
     # Remove the trailing comma
     IMAGE_LIST=${IMAGE_LIST%?}
   else
-    IMG_TAG="${inputs.image_tag}"
-    IMG_NAME="$(getVarValue "$(bashVarPrefix "${inputs.image_name}")" _IMG)"
-    GHCR_PATH="ghcr.io/${github.repository}/${IMG_NAME}"
+    IMG_TAG="${{inputs.image_tag}}"
+    IMG_NAME="$(getVarValue "$(bashVarPrefix "${{inputs.image_name}}")" _IMG)"
+    GHCR_PATH="ghcr.io/${{github.repository}}/${IMG_NAME}"
     IMAGE_LIST+="\"${GHCR_PATH}:${IMG_TAG}\""
   fi
 
@@ -30,8 +30,8 @@ if [ "${needs.publish-to-ghcr.result}" == 'skipped' ]; then
 
 # This case captures when the workflow is run automatically on PR merge along
 # with manual dispatches where publishing is disabled
-elif [ "${needs.publish-to-ghcr.result}" == 'success' ]; then
-  IMAGE_LIST=${needs.publish-to-ghcr.outputs.vro-images}
+elif [ "${{needs.publish-to-ghcr.result}}" == 'success' ]; then
+  IMAGE_LIST=${{needs.publish-to-ghcr.outputs.vro-images}}
 fi
 
 # Throw an error if the IMAGE_LIST variable is an empty array
