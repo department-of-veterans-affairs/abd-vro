3e0aea1375c51ee86de64d13ce76fae1200233ef
Tyler Spangler <tylerspangler@Tyler-Spangler.local>
2024-04-18 08:10:53 -0600

    updated schema to call from environment vars, reformatted constants names

--- domain-cc/cc-app/alembic/database_config.py
+++ domain-cc/cc-app/alembic/database_config.py
@@ -1,12 +1,11 @@
 """File sets env vars for database connection and to use in domain-cc/alembic/env.py"""
 import os
 
-username = os.environ.get("DOMAIN_CC_USER") or os.environ.get("POSTGRES_USER")
-password = os.environ.get("DOMAIN_CC_PW") or os.environ.get("POSTGRES_PASSWORD")
-postgres_url = os.environ.get("DB_URL")
-cc_schema = os.environ.get("ALEMBIC_SCHEMA", "domain-cc")
-port = 5432
-env = os.environ.get("ENV", "localhost")
+DB_USERNAME = os.environ.get("DOMAIN_CC_USER") or os.environ.get("POSTGRES_USER")
+DB_PASSWORD = os.environ.get("DOMAIN_CC_PW") or os.environ.get("POSTGRES_PASSWORD")
+DB_URL = os.environ.get("DB_URL")
+CC_SCHEMA = os.environ.get("ALEMBIC_SCHEMA")
+DB_PORT = 5432
+ENV = os.environ.get("ENV", "localhost")
 
-alembic_user = f"postgresql://{username}:{password}@{env}:{port}/vro"
-print(f"databaseURL: {alembic_user}")
+alembic_user = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{ENV}:{DB_PORT}/vro"
--- domain-cc/cc-app/alembic/db_models.py
+++ domain-cc/cc-app/alembic/db_models.py
@@ -1,18 +1,19 @@
+from database_config import CC_SCHEMA
 from sqlalchemy import Column, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+if CC_SCHEMA:
+    Base.__table_args__ = {"schema": CC_SCHEMA}
 
 
 class Claims(Base):
     __tablename__ = "claims"
-    # __table_args__ = {'schema': 'domain-cc'}
 
     vbms_claim_id = Column(Integer, primary_key=True)
 
 
 class Contentions(Base):
     __tablename__ = "contentions"
-    # __table_args__ = {'schema': 'domain-cc'}
 
     id = Column(Integer, primary_key=True)
