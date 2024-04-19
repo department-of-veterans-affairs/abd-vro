b4decccd9b2cd0d9f2c183b72458e2203bec13e9
Cheng <89034346+chengjie8@users.noreply.github.com>
2024-04-18 23:39:53 -0400

    add keystore reference back (#2876)

--- svc-bie-kafka/docker-entryprep.sh
+++ svc-bie-kafka/docker-entryprep.sh
@@ -12,6 +12,13 @@ for ENV_VAR in \
   fi
 done
 
+export KEYSTORE_FILE="$PWD/keystore.p12"
+echo "$BIE_KAFKA_KEYSTORE_INBASE64" | base64 -d > "$KEYSTORE_FILE"
+echo -e "\nVerifying keystore ($KEYSTORE_FILE) and its password..."
+if ! keytool -list -v -keystore "$KEYSTORE_FILE" -storepass "$BIE_KAFKA_KEYSTORE_PASSWORD" | grep "Alias name:"; then
+  >&2 echo "ERROR: with keystore"
+fi
+
 export TRUSTSTORE_FILE="$PWD/truststore.p12"
 echo "$BIE_KAFKA_TRUSTSTORE_INBASE64" | base64 -d > "$TRUSTSTORE_FILE"
 echo -e "\nVerifying truststore ($TRUSTSTORE_FILE) and its password..."
