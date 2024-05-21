5e0119080ef71e26d7049c2f8f8cd3f0eb67d50d
Luke Short <lukeshortemail@gmail.com>
2024-05-21 14:55:19 -0700

    add pydantic models for use w/ multi-contention classification

--- domain-cc/cc-app/src/python_src/pydantic_models.py
+++ domain-cc/cc-app/src/python_src/pydantic_models.py
@@ -1,6 +1,8 @@
 from typing import Optional
 
+from fastapi import HTTPException
 from pydantic import BaseModel, model_validator
+from pydantic.types import conlist
 
 
 class Claim(BaseModel):
@@ -39,3 +41,40 @@ class ClaimLinkInfo(BaseModel):
 
     va_gov_claim_id: int
     vbms_claim_id: int
+
+
+class Contention(BaseModel):
+    contention_text: str
+    contention_type: str  # "disabilityActionType" in the VA.gov API
+    diagnostic_code: Optional[int]  # only required for contention_type: "claim_for_increase"
+
+    @model_validator(mode="before")
+    def check_dc_for_cfi(cls, values):
+        contention_type = values.get("contention_type")
+        diagnostic_code = values.get("diagnostic_code")
+
+        if contention_type == "claim_for_increase" and not diagnostic_code:
+            raise HTTPException(
+                422, "diagnostic_code is required for contention_type claim_for_increase"
+            )
+
+
+class VaGovClaim(BaseModel):
+    claim_id: int
+    form526_submission_id: int
+    contentions: conlist(Contention, min_items=1)
+
+
+class ClassifiedContention(BaseModel):
+    classification: Optional[PredictedClassification]
+    diagnostic_code: Optional[int]  # only required for contention_type: "claim_for_increase"
+    contention_type: str  # "disabilityActionType" in the VA.gov API
+
+
+class ClassifierResponse(BaseModel):
+    contentions: conlist(ClassifiedContention, min_items=1)
+    claim_id: int
+    form526_submission_id: int
+    is_fully_classified: bool
+    num_processed_contentions: int
+    num_classified_contentions: int
