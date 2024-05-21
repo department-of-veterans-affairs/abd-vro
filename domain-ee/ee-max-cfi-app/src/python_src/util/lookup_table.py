4d8bbad968533af8d14e266ee02f29070f7b7da7
dfitchett <135860892+dfitchett@users.noreply.github.com>
2024-05-21 16:13:12 -0700

    Add mypy to max-ratings API

--- domain-ee/ee-max-cfi-app/src/python_src/util/lookup_table.py
+++ domain-ee/ee-max-cfi-app/src/python_src/util/lookup_table.py
@@ -6,9 +6,9 @@ from .table_version import TABLE_VERSION
 TABLE_NAME = f'Max Ratings DC Lookup Table {TABLE_VERSION}.csv'
 
 
-def get_max_ratings_by_code():
+def get_max_ratings_by_code() -> dict[int, int]:
     filename = os.path.join(os.path.dirname(__file__), 'data', TABLE_NAME)
-    diagnostic_code_to_max_rating = {}
+    diagnostic_code_to_max_rating: dict[int, int] = {}
     with open(filename, 'r') as file:
         csv_reader = csv.reader(file)
         for index, csv_line in enumerate(csv_reader):
@@ -18,13 +18,13 @@ def get_max_ratings_by_code():
             if len(csv_line) != 7:
                 raise ValueError(f'Invalid CSV line at index {index}')
 
-            diagnostic_code, rated_issue_name, max_rating, body_system, category, subcategory, cfr_ref = csv_line
-            if not diagnostic_code or not max_rating:
+            diagnostic_code_str, rated_issue_name, max_rating_str, body_system, category, subcategory, cfr_ref = csv_line
+            if not diagnostic_code_str or not max_rating_str:
                 continue
 
             try:
-                diagnostic_code = int(diagnostic_code)
-                max_rating = int(max_rating)
+                diagnostic_code = int(diagnostic_code_str)
+                max_rating = int(max_rating_str)
             except ValueError:
                 raise ValueError(f'Invalid diagnostic code or max rating at index {index}: \n{csv_line}')
 
@@ -35,5 +35,5 @@ def get_max_ratings_by_code():
 MAX_RATINGS_BY_CODE = get_max_ratings_by_code()
 
 
-def get_max_rating(classification_code):
-    return MAX_RATINGS_BY_CODE.get(classification_code)
+def get_max_rating(diagnostic_code: int) -> int | None:
+    return MAX_RATINGS_BY_CODE.get(diagnostic_code)
