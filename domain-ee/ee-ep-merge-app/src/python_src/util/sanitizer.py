4bda6a7865bfb2dbf436ce61335ccf675a81f563
dfitchett <135860892+dfitchett@users.noreply.github.com>
2024-05-24 08:37:43 -0700

    Add mypy to ep-merge

--- domain-ee/ee-ep-merge-app/src/python_src/util/sanitizer.py
+++ domain-ee/ee-ep-merge-app/src/python_src/util/sanitizer.py
@@ -1,4 +1,7 @@
-def sanitize(obj) -> str:
+from typing import Any
+
+
+def sanitize(obj: Any) -> str:
     if isinstance(obj, list):
         return str([sanitize(item) for item in obj])
     return str(obj).replace('\r\n', '').replace('\n', '')
