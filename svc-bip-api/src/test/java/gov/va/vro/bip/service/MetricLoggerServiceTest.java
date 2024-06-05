cadacd08284e825be4cd6bab644c85b558187441
Lisa Chung <943046+lisac@users.noreply.github.com>
2024-06-05 12:21:08 -0400

    add unit tests

---
+++ svc-bip-api/src/test/java/gov/va/vro/bip/service/MetricLoggerServiceTest.java
@@ -0,0 +1,76 @@
+package gov.va.vro.bip.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.datadog.api.client.v1.model.DistributionPointsPayload;
+import com.datadog.api.client.v1.model.MetricsPayload;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+import java.util.Objects;
+
+@ActiveProfiles("test")
+public class MetricLoggerServiceTest {
+
+  private MetricLoggerService mls = new MetricLoggerService();
+
+  @Test
+  void testGetFullMetricString() {
+    assertEquals(
+        "vro_bip.request",
+        MetricLoggerService.getFullMetricString(MetricLoggerService.METRIC.REQUEST));
+    assertEquals(
+        "vro_bip.request_duration",
+        MetricLoggerService.getFullMetricString(MetricLoggerService.METRIC.REQUEST_DURATION));
+    assertEquals(
+        "vro_bip.response_complete",
+        MetricLoggerService.getFullMetricString(MetricLoggerService.METRIC.RESPONSE_COMPLETE));
+    assertEquals(
+        "vro_bip.response_error",
+        MetricLoggerService.getFullMetricString(MetricLoggerService.METRIC.RESPONSE_ERROR));
+  }
+
+  @Test
+  void getTagsForSubmission() {
+    List<String> tags =
+        mls.getTagsForSubmission(new String[] {"source:integration-test", "version:2.1"});
+    assertTrue(tags.contains("source:integration-test"));
+    assertTrue(tags.contains("version:2.1"));
+    assertTrue(tags.contains("service:vro-svc-bip-api"));
+    assertTrue(tags.contains(String.format("environment:%s", System.getenv("env"))));
+    assertEquals(tags.size(), 4);
+  }
+
+  @Test
+  void getTagsForSubmissionNoCustomTags() {
+    List<String> tags = mls.getTagsForSubmission(null);
+    assertTrue(tags.contains("service:vro-svc-bip-api"));
+    assertTrue(tags.contains(String.format("environment:%s", System.getenv("env"))));
+    assertEquals(2, tags.size());
+  }
+
+  @Test
+  void testCreateMetricsPayload() {
+    MetricsPayload mp =
+        mls.createMetricsPayload(
+            MetricLoggerService.METRIC.RESPONSE_COMPLETE, 14.0, new String[] {"zone:purple"});
+    assertEquals(1, mp.getSeries().size());
+    assertEquals("vro_bip.response_complete", mp.getSeries().get(0).getMetric());
+    assertEquals(14.0, mp.getSeries().get(0).getPoints().get(0).get(0));
+    assertTrue(Objects.requireNonNull(mp.getSeries().get(0).getTags()).contains("zone:purple"));
+  }
+
+  @Test
+  void testCreateDistributionPointsPayload() {
+    DistributionPointsPayload dpl =
+        mls.createDistributionPointsPayload(
+            MetricLoggerService.METRIC.REQUEST_DURATION, 1523, new String[] {"food:pizza"});
+    assertEquals(1, dpl.getSeries().size());
+    assertEquals("vro_bip.request_duration", dpl.getSeries().get(0).getMetric());
+    assertEquals(
+        Double.valueOf(1523), dpl.getSeries().get(0).getPoints().get(0).get(0).getDouble());
+    assertTrue(Objects.requireNonNull(dpl.getSeries().get(0).getTags()).contains("food:pizza"));
+  }
+}
