1b98c90b7d78e0f2d6d17affb1f5c47a09ba6f67
Cheng <89034346+chengjie8@users.noreply.github.com>
2024-06-13 11:24:35 -0400

    small change (#3083)
    test

--- db-init/docker-entrypoint.sh
+++ db-init/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 # Poll to see if DB instance is ready
 until pg_isready -d $POSTGRES_DB -h $POSTGRES_URL -p 5432 -U $POSTGRES_FLYWAY_USER; do
  echo 'Waiting for Postgres DB to be available'
- sleep 5
+ sleep 6
 done
 
 # Perform flyway migrations
