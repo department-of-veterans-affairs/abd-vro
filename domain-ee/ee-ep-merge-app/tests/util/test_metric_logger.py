31115b9dcf92e8628ad973a0b076c19785943fe5
Derek Fitchett <135860892+dfitchett@users.noreply.github.com>
2024-06-20 08:59:19 -0700

    EP Merge: Fix memory leak (#3097)
    * Fix memory leak by closing event loop on make_request in ep_merge_machine.py
    * Revert "Chengjie8/update versions (#3080)" - This reverts commit e3b8a589538976165e34f7440af32f0424e7f206.

---
+++ domain-ee/ee-ep-merge-app/tests/util/test_metric_logger.py
@@ -0,0 +1,89 @@
+import pytest
+from datadog_api_client.v2.model.metric_intake_type import MetricIntakeType
+
+from src.python_src.util.metric_logger import (
+    CountMetric,
+    DistributionMetric,
+    distribution,
+    increment,
+)
+
+
+@pytest.fixture()
+def count_metrics_api(mocker):
+    return mocker.patch('src.python_src.util.metric_logger.count_metrics_api')
+
+
+@pytest.fixture()
+def distribution_metrics_api(mocker):
+    return mocker.patch('src.python_src.util.metric_logger.distribution_metrics_api')
+
+
+def test_increment_with_one_metric_to_increment(count_metrics_api):
+    metrics = [CountMetric(name='test_metric', value=1)]
+
+    increment(metrics)
+
+    body = count_metrics_api.submit_metrics.call_args[1]['body']
+
+    assert len(body.series) == 1
+    assert body.series[0].metric == 'ep_merge.test_metric'
+    assert body.series[0].type == MetricIntakeType.COUNT
+    assert len(body.series[0].points) == 1
+    assert body.series[0].points[0].value == 1
+
+
+def test_increment_with_multiple_metrics_to_increment(count_metrics_api):
+    metrics = [CountMetric(name='test_metric_1', value=1), CountMetric(name='test_metric_2', value=2)]
+
+    increment(metrics)
+
+    body = count_metrics_api.submit_metrics.call_args[1]['body']
+
+    assert len(body.series) == 2
+    assert all(s.type == MetricIntakeType.COUNT for s in body.series)
+
+    assert body.series[0].metric == 'ep_merge.test_metric_1'
+    assert len(body.series[0].points) == 1
+    assert body.series[0].points[0].value == 1
+
+    assert body.series[1].metric == 'ep_merge.test_metric_2'
+    assert len(body.series[1].points) == 1
+    assert body.series[1].points[0].value == 2
+
+
+def test_increment_with_one_metric_to_distribution(distribution_metrics_api):
+    metrics = [DistributionMetric(name='test_metric', value=1.5)]
+
+    distribution(metrics)
+
+    body = distribution_metrics_api.submit_distribution_points.call_args[1]['body']
+
+    assert len(body.series) == 1
+    assert body.series[0].metric == 'ep_merge.test_metric.distribution'
+    assert len(body.series[0].points) == 1
+    points = body.series[0].points[0].value[1]
+    assert len(points) == 1
+    assert points[0] == 1.5
+
+
+def test_increment_with_multiple_metrics_to_distribution(distribution_metrics_api):
+    metrics = [DistributionMetric(name='test_metric_1', value=1.1), DistributionMetric(name='test_metric_2', value=2.2)]
+
+    distribution(metrics)
+
+    body = distribution_metrics_api.submit_distribution_points.call_args[1]['body']
+
+    assert len(body.series) == 2
+
+    assert body.series[0].metric == 'ep_merge.test_metric_1.distribution'
+    assert len(body.series[0].points) == 1
+    points = body.series[0].points[0].value[1]
+    assert len(points) == 1
+    assert points[0] == 1.1
+
+    assert body.series[1].metric == 'ep_merge.test_metric_2.distribution'
+    assert len(body.series[1].points) == 1
+    points = body.series[1].points[0].value[1]
+    assert len(points) == 1
+    assert points[0] == 2.2
