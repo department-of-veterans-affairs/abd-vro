9362e01e407d8016f9080ec0995640237450c2ea
Derek Fitchett <135860892+dfitchett@users.noreply.github.com>
2024-07-12 08:29:02 -0700

    lib-hoppy: Update to include per message TTL parameter for AsyncHoppyClient. (#3180)
    Updated hoppy lib to include request message TTL parameter for AsyncHoppyClient.

--- shared/lib-hoppy/hoppy/async_hoppy_client.py
+++ shared/lib-hoppy/hoppy/async_hoppy_client.py
@@ -17,6 +17,9 @@ UNEXPECTED_PUBLISH_ERROR = "Unexpected Error Caught While Publishing"
 
 
 class AsyncHoppyClient:
+    """
+    Asynchronous client that performs requests and handles responses over RabbitMQ.
+    """
 
     def __init__(self,
                  name: str,
@@ -27,8 +30,26 @@ class AsyncHoppyClient:
                  reply_queue_properties: QueueProperties,
                  request_routing_key: str,
                  reply_routing_key: str,
-                 max_latency: int = 30,
+                 request_message_ttl: int = 0,
+                 response_max_latency: int = 30,
                  response_reject_and_requeue_attempts: int = 3):
+        """
+        Initialize the AsyncHoppyClient.
+
+        Args:
+            name (str): The name of the client.
+            app_id (str): The application ID of the client.
+            config (dict): Configuration dictionary for the client.
+            exchange_properties (ExchangeProperties): Properties of the exchange.
+            request_queue_properties (QueueProperties): Properties of the request queue.
+            reply_queue_properties (QueueProperties): Properties of the reply queue.
+            request_routing_key (str): Routing key for requests.
+            reply_routing_key (str): Routing key for replies.
+            request_message_ttl (int, optional): Time to live (in milliseconds) for request messages. Defaults to 0ms.
+            response_max_latency (int, optional): Maximum latency (in seconds) for responses. Defaults to 30s.
+            response_reject_and_requeue_attempts (int, optional): Number of attempts to reject and requeue responses. Defaults to 3.
+        """
+
         self.name = name
         self.app_id = app_id
         self.config = config
@@ -37,7 +58,9 @@ class AsyncHoppyClient:
         self.request_queue_properties = request_queue_properties
         self.reply_queue_properties = reply_queue_properties
 
-        self.max_latency = max_latency
+        self.request_message_ttl = request_message_ttl
+
+        self.response_max_latency = response_max_latency
         self.response_reject_and_requeue_attempts = response_reject_and_requeue_attempts
 
         self.async_publisher = AsyncPublisher(config=self.config,
@@ -86,9 +109,7 @@ class AsyncHoppyClient:
             if response is not None:
                 return self.handle_response(request_id, correlation_id, response)
             else:
-                self.check_for_timeout(correlation_id,
-                                       request_id,
-                                       wait_for_response_time)
+                self.check_for_timeout(correlation_id, request_id, wait_for_response_time)
             await asyncio.sleep(0)
 
     def publish(self, body, correlation_id, request_id):
@@ -96,8 +117,9 @@ class AsyncHoppyClient:
             self.async_publisher.publish_message(body,
                                                  BasicProperties(app_id=self.app_id,
                                                                  content_type="application/json",
-                                                                 reply_to=self.reply_queue_properties.name,
-                                                                 correlation_id=correlation_id))
+                                                                 correlation_id=correlation_id,
+                                                                 expiration=str(self.request_message_ttl),
+                                                                 reply_to=self.reply_queue_properties.name))
         except Exception as e:
             logging.warning(f"event=requestError "
                             f"client={self.name} "
@@ -127,7 +149,7 @@ class AsyncHoppyClient:
 
     def check_for_timeout(self, correlation_id, request_id, wait_for_response_time):
         current_time = time.time()
-        if current_time - wait_for_response_time >= self.max_latency:
+        if current_time - wait_for_response_time >= self.response_max_latency:
             logging.warning(f"event=requestError "
                             f"client={self.name} "
                             f"id={request_id} "
@@ -184,6 +206,13 @@ class AsyncHoppyClient:
 
 
 class RetryableAsyncHoppyClient(AsyncHoppyClient):
+    """
+    Retryable AsyncHoppyClient that retries requests a specified number of times.
+
+    Attributes:
+        max_retries (int): Maximum number of retries.
+    """
+
     def __init__(self,
                  name: str,
                  app_id: str,
@@ -193,12 +222,14 @@ class RetryableAsyncHoppyClient(AsyncHoppyClient):
                  reply_queue_properties: QueueProperties,
                  request_routing_key: str,
                  reply_routing_key: str,
-                 max_latency: int = 30,
+                 request_message_ttl: int = 1000,
+                 response_max_latency: int = 30,
                  response_reject_and_requeue_attempts: int = 3,
                  max_retries=3):
         self.max_retries = max_retries
+        # add super constructor call with keyword arguments
         super().__init__(name, app_id, config, exchange_properties, request_queue_properties, reply_queue_properties,
-                         request_routing_key, reply_routing_key, max_latency,
+                         request_routing_key, reply_routing_key, request_message_ttl, response_max_latency,
                          response_reject_and_requeue_attempts)
 
     async def make_request(self, request_id, body):
