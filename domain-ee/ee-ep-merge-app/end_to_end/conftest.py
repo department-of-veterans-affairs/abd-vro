4f396adc3c615ca5a27c2f56fc50c09523f2fde8
Derek Fitchett <135860892+dfitchett@users.noreply.github.com>
2024-07-19 15:07:23 -0700

    EP Merge : Make JOB_RUNNER wait for jobs to finish before allowing new jobs to be submitted (#3222)
    * Make JOB_RUNNER wait for jobs to finish before allowing new jobs to be submitted.

--- domain-ee/ee-ep-merge-app/end_to_end/conftest.py
+++ domain-ee/ee-ep-merge-app/end_to_end/conftest.py
@@ -1,10 +1,11 @@
 import pytest_asyncio
 
-from src.python_src.api import on_shut_down, start_hoppy
+from src.python_src.api import on_shut_down, start_hoppy, start_job_runner
 
 
 @pytest_asyncio.fixture(autouse=True, scope='session')
 async def endpoint_lifecycle():
     await start_hoppy()
+    await start_job_runner()
     yield
     await on_shut_down()
