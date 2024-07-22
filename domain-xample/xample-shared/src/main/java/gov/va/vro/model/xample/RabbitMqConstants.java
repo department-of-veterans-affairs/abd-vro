f7b0a94dc5a64af7e97e19a0d6ce432a3a7e0f78
Cheng <89034346+chengjie8@users.noreply.github.com>
2024-07-22 11:29:02 -0600

    removed camel library references, update package names and dependencies (#3171)
    * removed camel library references, update package names and dependencies
    
    * remove commented out library reference
    
    * added unit tests

--- domain-xample/xample-shared/src/main/java/gov/va/vro/model/xample/RabbitMqConstants.java
+++ domain-xample/xample-shared/src/main/java/gov/va/vro/model/xample/RabbitMqConstants.java
@@ -1,6 +1,6 @@
 package gov.va.vro.model.xample;
 
-public class CamelConstants {
+public class RabbitMqConstants {
   public static final String V3_EXCHANGE = "v3";
   public static final String POST_RESOURCE_QUEUE = "postResource";
   public static final String GET_RESOURCE_QUEUE = "getResource";
