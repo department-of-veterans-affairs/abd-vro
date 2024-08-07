b38dc9707344d640a4d0d14818970c88b8a8a149
Lisa Chung <943046+lisac@users.noreply.github.com>
2024-08-07 11:21:39 -0400

    add unit test to verify metrics are submitted

--- svc-bie-kafka/src/main/java/gov/va/vro/services/bie/service/kafka/KafkaConsumer.java
+++ svc-bie-kafka/src/main/java/gov/va/vro/services/bie/service/kafka/KafkaConsumer.java
@@ -25,7 +25,9 @@ import org.springframework.stereotype.Component;
 public class KafkaConsumer {
   private final AmqpMessageSender amqpMessageSender;
   private final BieProperties bieProperties;
-  final IMetricLoggerService metricLogger;
+
+  private IMetricLoggerService metricLogger;
+
   final String[] metricTagsSendToQueue = new String[] {"type:sendRecordToMq", "source:svcBieKafka"};
 
   @KafkaListener(topics = "#{bieProperties.topicNames()}")
@@ -60,7 +62,7 @@ public class KafkaConsumer {
           MetricLoggerService.METRIC.RESPONSE_COMPLETE, metricTagsWithTopicName);
     } catch (Exception e) {
       log.error("Exception occurred while processing message: " + e.getMessage());
-      metricLogger.submitCount(MetricLoggerService.METRIC.RESPONSE_COMPLETE, metricTagsSendToQueue);
+      metricLogger.submitCount(MetricLoggerService.METRIC.RESPONSE_ERROR, metricTagsSendToQueue);
     }
   }
 
@@ -86,5 +88,9 @@ public class KafkaConsumer {
     payload.setNotifiedAt(record.timestamp());
 
     return payload;
+  }
+
+  public void setMetricLogger(IMetricLoggerService metricLogger) {
+    this.metricLogger = metricLogger;
   }
 }
--- svc-bie-kafka/src/test/java/gov/va/vro/services/bie/config/KafkaConsumerTest.java
+++ svc-bie-kafka/src/test/java/gov/va/vro/services/bie/config/KafkaConsumerTest.java
@@ -1,9 +1,16 @@
 package gov.va.vro.services.bie.config;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import gov.va.vro.metricslogging.MetricLoggerService;
 import gov.va.vro.services.bie.service.AmqpMessageSender;
 import gov.va.vro.services.bie.service.kafka.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,5 +31,32 @@ class KafkaConsumerTest {
   @BeforeEach
   void setUp() {
     bieProperties = new BieProperties();
+  }
+
+  @Test
+  void testMetricsSubmitted() {
+    MetricLoggerService metricLoggerService = mock(MetricLoggerService.class);
+    kafkaConsumer.setMetricLogger(metricLoggerService);
+    kafkaConsumer.consume(new ConsumerRecord<String, Object>("testTopic", 0, 1, "lorem", "ipsum"));
+
+    verify(metricLoggerService)
+        .submitCount(MetricLoggerService.METRIC.REQUEST_START, ArgumentMatchers.any());
+    verify(metricLoggerService)
+        .submitRequestDuration(
+            ArgumentMatchers.any(long.class),
+            ArgumentMatchers.any(long.class),
+            ArgumentMatchers.any());
+    verify(metricLoggerService)
+        .submitCount(MetricLoggerService.METRIC.RESPONSE_COMPLETE, ArgumentMatchers.any());
+  }
+
+  @Test
+  void testErrorMetricSubmitted() {
+    MetricLoggerService metricLoggerService = mock(MetricLoggerService.class);
+    kafkaConsumer.setMetricLogger(metricLoggerService);
+    kafkaConsumer.consume(new ConsumerRecord<String, Object>("testTopic", 0, 1, null, null));
+
+    verify(metricLoggerService)
+        .submitCount(MetricLoggerService.METRIC.RESPONSE_ERROR, ArgumentMatchers.any());
   }
 }
