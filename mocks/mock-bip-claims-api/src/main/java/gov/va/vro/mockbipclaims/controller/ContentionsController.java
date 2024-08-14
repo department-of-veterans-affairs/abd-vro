10391aff39e6b360092aa82b213bbfb4c3b44e95
Erik Nelsestuen <erik.nelsestuen@va.com>
2024-08-14 04:30:36 -0600

    spotlessApply fix for mock bip contentions controller

--- mocks/mock-bip-claims-api/src/main/java/gov/va/vro/mockbipclaims/controller/ContentionsController.java
+++ mocks/mock-bip-claims-api/src/main/java/gov/va/vro/mockbipclaims/controller/ContentionsController.java
@@ -143,7 +143,10 @@ public class ContentionsController extends BaseController implements Contentions
     return new ResponseEntity<>(response, HttpStatus.OK);
   }
 
-  /** Responds with the json in file: mocks/mock-bip-claims-api/src/main/resources/mock-special-issues.json*/
+  /**
+   * Responds with the json in file:
+   * mocks/mock-bip-claims-api/src/main/resources/mock-special-issues.json
+   */
   @Override
   public ResponseEntity<SpecialIssueTypesResponse> getSpecialIssueTypes() {
     log.info("Returning mock special issue types...");
