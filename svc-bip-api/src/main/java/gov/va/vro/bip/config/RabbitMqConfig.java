b3ff3320b403987cd54cf01059a457982e589a90
dfitchett <135860892+dfitchett@users.noreply.github.com>
2024-08-14 15:25:58 -0700

    Set DL queue declaration to false on BIP startup.

--- svc-bip-api/src/main/java/gov/va/vro/bip/config/RabbitMqConfig.java
+++ svc-bip-api/src/main/java/gov/va/vro/bip/config/RabbitMqConfig.java
@@ -85,10 +85,9 @@ public class RabbitMqConfig implements RabbitListenerConfigurer {
   @Bean
   Queue deadLetterQueue() {
     log.info(
-        "Creating dead letter queue with name={} and properties={}",
-        props.getDeadLetterQueueName(),
-        props.getDeadLetterQueueArgs());
-    return QueueBuilder.durable(props.getDeadLetterQueueName()).autoDelete().build();
+        "Creating dead letter queue with name={}",
+        props.getDeadLetterQueueName());
+    return QueueBuilder.durable(props.getDeadLetterQueueName()).build();
   }
 
   @Bean
