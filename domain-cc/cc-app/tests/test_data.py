12e5bac28747ca14f568a1dba6a132f46c449442
tyler-spangler6 <142833783+tyler-spangler6@users.noreply.github.com>
2024-09-10 14:57:24 -0600

    Update Lookup Table Versions (#3432)
    * updated lookup tables, tests
    
    * adds unit tests for new versions and dc lut build
    
    ---------
    
    Co-authored-by: Tyler Spangler <tylerspangler@Tyler-Spangler.local>

--- domain-cc/cc-app/tests/test_data.py
+++ domain-cc/cc-app/tests/test_data.py
@@ -1,8 +1,17 @@
-from src.python_src.util.lookup_table import ContentionTextLookupTable
+from src.python_src.util.lookup_table import (
+    ContentionTextLookupTable,
+    DiagnosticCodeLookupTable,
+)
 
-DROPDOWN_LUT_SIZE = 1050
+CONTENTION_DROPDOWN_LUT_SIZE = 1056
+DIAGNOSTIC_CODE_LUT_SIZE = 755
 
 
 def test_build_dropdown_lookup_table():
     lookup_table = ContentionTextLookupTable()
-    assert len(lookup_table) == DROPDOWN_LUT_SIZE
+    assert len(lookup_table) == CONTENTION_DROPDOWN_LUT_SIZE
+
+
+def test_build_dc_lut():
+    dc_lookup_table = DiagnosticCodeLookupTable()
+    assert len(dc_lookup_table) == DIAGNOSTIC_CODE_LUT_SIZE
