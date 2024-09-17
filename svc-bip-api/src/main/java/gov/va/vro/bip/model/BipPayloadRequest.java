464b58a0ee158ac6c52878feaacac0da9860450f
Derek Fitchett <135860892+dfitchett@users.noreply.github.com>
2024-09-17 10:30:44 -0700

    svc-bip-api: Add externalKey and externalUserId to JWT (#3469)
    * Add externalKey and externalUserId to request models from RabbitMQ and use to populate claims in JWT.

--- svc-bip-api/src/main/java/gov/va/vro/bip/model/BipPayloadRequest.java
+++ svc-bip-api/src/main/java/gov/va/vro/bip/model/BipPayloadRequest.java
@@ -1,3 +1,17 @@
 package gov.va.vro.bip.model;
 
-public interface BipPayloadRequest {}
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder(toBuilder = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public abstract class BipPayloadRequest {
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+  private String externalUserId;
+
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+  private String externalKey;
+}
